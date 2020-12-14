2c5077b129b8f1aed8b3174cdd922ec3e4c707a9
mozerpol <mozerpol@protonmail.com>
2020-12-15 00:44:57 +0100

    Add some lines to lui.asm

--- instructions/LUI/lui.asm
+++ instructions/LUI/lui.asm
@@ -1,1 +1,7 @@
-lui		x1, 0xFFFFF
+lui     x1, 0xFFFFF # x1 is equal 0xfffff000
+lui     x1, 0xFFFF  # x1 is equal 0x0ffff000
+lui     x1, 0xFFF   # x1 is equal 0x00fff000
+lui     x1, 0xFF    # x1 is equal 0x000ff000
+lui     x1, 0xF     # x1 is equal 0x0000f000
+
+
