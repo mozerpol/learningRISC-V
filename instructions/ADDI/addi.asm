e3405406e550dd79e4cb5356d520f4318339d5e5
mozerpol <mozerpol@protonmail.com>
2020-12-15 16:54:20 +0100

    Add third line to asm with substract

--- instructions/ADDI/addi.asm
+++ instructions/ADDI/addi.asm
@@ -1,2 +1,3 @@
 li		x1, 0x1		# load number 1 to x1 reg. Reg x1 = 0x00000001
 addi 	x2, x1, 0x3	# add x1 and 0x3 numb and save in x2. Rex x2 = 0x00000004
+addi 	x2, x2, -3	# add x2 and -3 numb and save in x2. Rex x2 = 0x00000001
