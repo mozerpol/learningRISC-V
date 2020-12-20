ccec9c6a67f7dc7d86d14fe2c50736f9b9987d60
mozerpol <mozerpol@protonmail.com>
2020-12-21 00:14:29 +0100

    Add code to slti.asm

---
+++ instructions/SLTI/slti.asm
@@ -0,0 +1,3 @@
+addi	x1, x0, 3 # set number 3 in x1
+slti	x2, x1, 4 # compare number 4 with x1. 4 is greater than 3 so set 1 in x2
+slti	x2, x1, 1 # compare number 1 with x1. 1 is less than 3 so set 0 in x2
