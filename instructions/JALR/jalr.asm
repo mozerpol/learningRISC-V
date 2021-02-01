55cfe6ace1f84a774b751716a796a37601f8c1ee
mozerpol <mozerpol@protonmail.com>
2021-02-01 23:39:44 +0100

    Finish jalr code

--- instructions/JALR/jalr.asm
+++ instructions/JALR/jalr.asm
@@ -1,4 +1,5 @@
-addi	x2, x0, 0
-addi	x2, x0, 0
-addi	x2, x0, 2
-jalr	x1, x2, 2
+addi	x2, x0, 0 # reset x2 register
+addi	x2, x0, 0 # reset x2 register
+addi	x2, x0, 2 # save the value 2 in the register x2 
+jalr	x1, x2, 2 # add value from x2 with 0x2 and jump to instruction number
+                  # 0x4 - it's second line
