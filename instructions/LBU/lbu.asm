c0cad78911d73922f540b11c6308b6de1fb319d6
mozerpol <mozerpol@protonmail.com>
2021-02-13 23:48:42 +0100

    Add comments to lbu.asm

--- instructions/LBU/lbu.asm
+++ instructions/LBU/lbu.asm
@@ -1,7 +1,7 @@
-la 		x1, array
-lbu 	x2, 0(x1)
-lbu 	x3, 1(x1)
+la 		x1, array # load PC adress array label to x1 register 
+lbu 	x2, 0(x1) # read first byte from label saved in x1 and save result in x2
+lbu 	x3, 1(x1) # read second byte from label saved in x1 and save result in x2
 
-.data
+.data # assembly directive
 array:
-	.asciiz "AB"
+	.asciiz "AB" # save "AB" string in memory
