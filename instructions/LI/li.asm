bce3eb5282cb00d365bb42b70acda28577989864
mozerpol <mozerpol@protonmail.com>
2021-04-11 14:59:51 +0200

    Add code and comments to li.asm

---
+++ instructions/LI/li.asm
@@ -0,0 +1,4 @@
+li		x1, 0xff # save in x1 0xff, just ADDI
+li		x2, 0xfff # save in x2 0xfff, break down LI in LUI and ADDI
+li		x3, -0xff # save in x3 -0xff, just ADDI
+li		x4, -0xfff # save in x4 -0xfff, break down LI in LUI and ADDI
