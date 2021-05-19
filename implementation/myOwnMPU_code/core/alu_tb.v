f95e0529e39b85a8d155ffee74d8ec6b586f7bfe
mozerpol <mozerpol@protonmail.com>
2021-05-19 14:05:55 +0200

    Almost working alu_tb

--- implementation/myOwnMPU_code/core/alu_tb.v
+++ implementation/myOwnMPU_code/core/alu_tb.v
@@ -15,17 +15,43 @@ module alu_tb;
     .alu_out(alu_out_tb)
   );
 
+
+  reg [31:0] val_for_in1 [0:4]; // 4 x 32-bit
+  reg [31:0] val_for_in2 [0:4];
+  integer i;
+  reg [0:3] a;
+
   initial begin
     $dumpfile("tb.vcd"); 
     $dumpvars;
 
-    alu_in1_tb = 32'b01;
-    alu_in2_tb = 32'b10;
-    alu_op_tb = 4'b0000;
+    val_for_in1[0] = 32'd10;
+    val_for_in1[1] = 32'd3;
+    val_for_in1[2] = -32'd4;
+    val_for_in1[3] = 32'd4;
+    val_for_in1[4] = -32'd16;
 
-    #50
-    alu_in1_tb = 32'b0;
-    alu_in2_tb = 32'b0;
+    val_for_in2[0] = 32'd2;
+    val_for_in2[1] = 32'd10;
+    val_for_in2[2] = 32'd4;
+    val_for_in2[3] = -32'd4;
+    val_for_in2[4] = 32'd2;
+
+
+//     $display ("val_for_in1: %d", val_for_in1[2]);
+//     $display ("val_for_in2: %d", val_for_in2[2]);
+
+    for(a = 0; a < 10; a++)
+      begin
+        $display ("i: %b", a);
+
+        for(i = 0; i < 5; i++)
+          begin
+            $display ("val_for_in1: %d", val_for_in1[i]);
+            $display ("val_for_in2: %d", val_for_in2[i]);
+          end
+      end
+
     #50 $finish;
   end
 
