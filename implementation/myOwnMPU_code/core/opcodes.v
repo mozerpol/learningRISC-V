66d241d5ec2c7bd6f4b7c73767feed94991e3aff
mozerpol <mozerpol@protonmail.com>
2021-05-23 15:31:00 +0200

    Change idea how to resolve problem with opcodes

--- implementation/myOwnMPU_code/core/opcodes.v
+++ implementation/myOwnMPU_code/core/opcodes.v
@@ -1,27 +1,30 @@
 `timescale 100ns / 10ns
 
-'define LOAD =		5'b00000
-`define LOAD_FP =	5'b00001
-`define CUSTOM_0 =	5'b00010
-`define MISC_MEM =	5'b00011
-`define OP_IMM =	5'b00100
-
 module opcode(
   input wire [5:0] opcode_in,
   output wire [5:0] opcode_out
 );
 
+  localparam LOAD =		5'b00000;
+  localparam LOAD_FP =	5'b00001;
+  localparam CUSTOM_0 =	5'b00010;
+  localparam MISC_MEM =	5'b00011;
+  localparam OP_IMM =	5'b00100;
+
   reg [5:0] o;
   assign opcode_out = o;
-  
-  always@(*)
+
+  always@(opcode_in)
     begin
       case(opcode_in)
-        0 : o = 'LOAD;
-        1 : o = 5'b00001;
+        0 : o =	LOAD;
+        1 : o = LOAD_FP;
+        2 : o = CUSTOM_0;
+        3 : o = MISC_MEM;
+        4 : o = OP_IMM;
       endcase
     end
+
 endmodule
 
 
-// https://stackoverflow.com/questions/65767011/using-define-inside-case-statement-not-working
