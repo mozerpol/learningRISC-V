c55b7a1102748f357bf2ab81f348925a80705b8e
mozerpol <mozerpol@protonmail.com>
2021-05-25 13:41:31 +0200

    Add test and comment for B-type

--- implementation/myOwnMPU_code/core/decode_tb.v
+++ implementation/myOwnMPU_code/core/decode_tb.v
@@ -44,11 +44,26 @@ module decode_tb();
       /* 
       	LUI x1 0x23 is equal 000230B7 in machine code
         000230B7 in hex = 00000000000000100011 00001 0110111 in bin, where:
-        0 -   6:  0110111              - opcode  =  37 in hex
-        7 -  11:  00001                - rd      =  1  in hex
+        0  -  6:  0110111              - opcode  =  37 in hex
+        7  - 11:  00001                - rd      =  1  in hex
         12 - 31:  00000000000000100011 - imm     =  23 in hex
       */
 
+      #1 inst_tb <= 32'hfe111ce3;
+      // Test for B-type, BNE instruction:
+      /*
+      	BNE x2, x1, loop is equal in fe111ce3 machine code
+        fe111ce3 in hex = 1 111111 00001 00010 001 1100 1 1100011 in bin, where:
+        0  -  6:  1100011  - opcode    =  63 in hex
+        7  -  7:  1        - imm[11]   =  1  in hex
+        8  - 11:  1100     - imm[4:1]  =  C  in hex
+        12 - 14:  001      - funct3    =  1  in hex
+        15 - 19:  00010    - rs1       =  2  in hex
+        20 - 24:  00001    - rs2       =  1  in hex
+        25 - 30:  111111   - imm[10:5] =  3F in hex
+        31 - 31:  1        - imm[12]   =  1 in hex
+      */
+      
 
       #1 $finish;
 
