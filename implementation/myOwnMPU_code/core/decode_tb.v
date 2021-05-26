bd3675c0937d5f2d481688d14b24d02088e31981
mozerpol <mozerpol@protonmail.com>
2021-05-26 14:06:46 +0200

    Add comments and a few new tests

--- implementation/myOwnMPU_code/core/decode_tb.v
+++ implementation/myOwnMPU_code/core/decode_tb.v
@@ -1,3 +1,11 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2019 Rafal Kozik
+ * All rights reserved.
+ * Mozerpol added new tests
+*/
+
 `timescale 100ns / 10ns
 
 module decode_tb();
@@ -76,9 +84,44 @@ module decode_tb();
         21 - 30:  1111111100  - imm[10:1]  =  3FC in hex
         31 - 31:  1           - imm[20]    =  1   in hex
       */
+
+      #1 inst_tb <= 32'h000230B7; 
+      /* 
+        32'h000230B7 it's an "lui x1 0x23" instruction:
+        000230B7 (hex) = 00000000000000100011 00001 0110111 (bin)
+
+        0 -   6: 0110111              - opcode = 37 in hex
+        7 -  11: 00001                - rd     = 1 in hex
+        12 - 31: 00000000000000100011 - imm    = 23 hex
+      */
+      #1 inst_tb <= 32'h05408113; 
+      /*
+      	32'h05408113 it's an "addi x2 x1 0x54" instruction:
+        05408113 (hex) = 0000001010100 00001 000 00010 0010011 (bin)
+
+        0 -   6: 0010011       - opcode = 13 in hex
+        7 -  11: 00010         - rd     = 2 in hex
+        12 - 14: 000           - funct3 = 0 in hex
+        15 - 19: 00001         - rs1    = 1 in hex
+        20 - 31: 0000001010100 - imm    = 54 in hex
+      */
+
+      #1 inst_tb <= 32'h001101B3;
+      // Test for R-type, ADD instruction:
+      /*
+      	ADD x3, x2, x1 is equal 001101B3 in machine code
+        001101B3 in hex = 0000000 00001 00010 000 00011 0110011 in bin, where:
+        0  -  6:  0110011 - opcode  =  33 in hex
+        7  - 11:  00011   - rd      =  3  in hex
+        12 - 14:  000     - funct3  =  0  in hex
+        15 - 19:  00010   - rs1     =  1  in hex
+        20 - 24:  00001   - rs2     =  2  in hex
+        25 - 31:  0000000 - funct7  =  0  in hex
+      */
+
+      #1 inst_tb <= 32'h40110233; // SUB x4 x2 x1
       
       #1 $finish;
-
     end
 
 endmodule
