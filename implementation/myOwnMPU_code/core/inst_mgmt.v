807a8a3c64bc77a4b2df6cbeb61c7a8806a5895c
mozerpol <mozerpol@protonmail.com>
2021-05-31 16:16:52 +0200

    Add comments

--- implementation/myOwnMPU_code/core/inst_mgmt.v
+++ implementation/myOwnMPU_code/core/inst_mgmt.v
@@ -12,20 +12,20 @@ module inst_mgmt(
   input wire [`REG_LEN-1:0] rdata,
   output wire [`REG_LEN-1:0] inst
 );
-  
+
   parameter NOP = 32'h00000013;
   reg [`REG_LEN-1:0] o;
   assign inst = o;
 
   always @(posedge clk) begin
     case(inst_sel)
-      `INST_OLD : o <= inst;
-      `INST_NOP : o <= NOP;
-      `INST_MEM : o <= rdata;
-      default : o <= NOP;
+      `INST_OLD : o <= inst; // Previous instruction on output.
+      `INST_NOP : o <= NOP; // NOP on output.
+      `INST_MEM : o <= rdata; // Instruction from RDATA on output. Just transfer in to out.
+      default : o <= NOP; // Tkanks to this line, when we run the processor on our
+      // output, we get NOP.
     endcase
-    if(rst)
-      o <= NOP;
+    if(rst) o <= NOP;
   end
 
 endmodule
--- implementation/myOwnMPU_code/core/inst_mgmt_tb.v
+++ implementation/myOwnMPU_code/core/inst_mgmt_tb.v
@@ -24,31 +24,40 @@ module inst_mgmt_tb;
     $dumpfile("tb.vcd"); 
     $dumpvars;
 
-    rst_tb = 0;
-	
-    //''''''//
-    //		//
-    //,,,,,,//
+    rst_tb = 0;	
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
     #10 inst_sel_tb = `INST_MEM;
-    rdata_tb = 32'd10;
+    rdata_tb = 32'd11;
     #10 rdata_tb = 32'd3;
     #10 rdata_tb = -32'd4;
     #10 rdata_tb = 32'd4;
     #10 rdata_tb = -32'd16;
-    //''''''//
-    //		//
-    //,,,,,,//
+    //'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                                Test 2:                                //
+    // We have previous instruction on output. In this case will be -32'd16  //
+    // which is equal in signed 2's complement notation fffffff0.            //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
     #10 inst_sel_tb = `INST_OLD;
-    //''''''//
-    //		//
-    //,,,,,,//
-    #10 inst_sel_tb = `INST_NOP;
-    #10 inst_sel_tb = `INST_NOP;
-    //''''''//
-    //		//
-    //,,,,,,//
+    //'''''''''''''''''''''''''''''''''''''''''''//
+    //                  Test 3:                  //
+    // NOP on output. NOP is equal 32'h00000013. //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 inst_sel_tb = `INST_NOP; // Fistr clock cycle
+    #10 inst_sel_tb = `INST_NOP; // Second clock cycle
+    //'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                                Test 3:                                    //
+    // Move instructions from output to input (instruction comes from RDATA      //
+    // reg). Look at the core structure. In the middle reset the processor.      //
+    // During reset on output should be NOP. After the end of reset, instruction //
+    // from RDATA reg will appear in next two cycles, because first clock cycle  //
+    // is for exits from the conditional statement: if(rst) o <= NOP;            //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
     #10 inst_sel_tb = `INST_MEM;
-    rdata_tb = 32'd10;
+    rdata_tb = 32'd11;
     #10 rdata_tb = 32'd3;
     #10 rst_tb = 0;
     #10 rst_tb = 1;
@@ -63,4 +72,3 @@ module inst_mgmt_tb;
   always #5 clk_tb = ~clk_tb;
 
 endmodule
-
