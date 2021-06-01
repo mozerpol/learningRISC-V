53c4212ea5db83b13d8fb33629bae388d472a5dd
mozerpol <mozerpol@protonmail.com>
2021-06-01 13:33:13 +0200

    Add new tests and code to module

--- implementation/myOwnMPU_code/core/mem_addr_sel.v
+++ implementation/myOwnMPU_code/core/mem_addr_sel.v
@@ -1,51 +1,46 @@
 `include "rysy_pkg.vh"
 `timescale 100ns / 10ns
 
-// module pc_sel_module(
-//   input wire [1:0] pc_sel_in,
-//   output wire [1:0] pc_sel_out
-// );
-//   assign pc_sel_out = pc_sel_in;
-// endmodule
-
-// module mem_sel_module(
-//   input wire mem_sel_in,
-//   input wire mem_sel_out
-// );
-//   assign mem_sel_out = mem_sel_in;
-// endmodule
-
 `define PC_ALU 2'b00
 `define PC_P4  2'b01 // Plus
 `define PC_M4  2'b10 // Minus
 `define PC_OLD 2'b11
+`define MEM_PC 1'b0
+`define MEM_ALU 1'b1
 
 module mem_addr_sel(
   input wire clk,
   input wire rst,
-  input wire [1:0] pc_sel,
-  input wire mem_sel,
-  input wire [`REG_LEN-1:0]alu_out,
-  output wire [`REG_LEN-1:0]addr,
-  output wire [`REG_LEN-1:0]pc
+  input wire [1:0] pc_sel, 			// mux for PC
+  input wire mem_sel, 				// mux, instruction from PC or ALU?
+  input wire [`REG_LEN-1:0]alu_out, // value to PC from ALU?
+  output wire [`REG_LEN-1:0]addr, 	// for ADDR module
+  output wire [`REG_LEN-1:0]pc 		// for PC module
 );
 
-  wire [`REG_LEN-1:0] pc_reg;
+  reg [`REG_LEN-1:0] pc_reg;
   assign pc = pc_reg;
-  reg [`REG_LEN-1:0] o;
-  assign pc_reg = o;
 
   always@(posedge clk)
     begin
       case(pc_sel)
-        `PC_ALU : o <= alu_out;
-        `PC_P4  : o <= pc_reg + 4;
-        `PC_OLD : o <= pc_reg;
-        `PC_M4  : o <= pc_reg - 4;
-        default: o <= pc_reg;
+        `PC_ALU : pc_reg <= alu_out; 	// value to PC from alu
+        `PC_P4  : pc_reg <= pc_reg + 4; // Increment actual value in PC
+        `PC_M4  : pc_reg <= pc_reg - 4;
+        `PC_OLD : pc_reg <= pc_reg; 	// Do nothing
+        default: pc_reg <= pc_reg;
       endcase
       if (rst)
-        o <= 0;
+        pc_reg <= 0;
     end
+
+  reg [`REG_LEN-1:0] addr_reg;
+  assign addr = addr_reg;
+
+  always@(*)
+    case(mem_sel)
+      `MEM_PC  : addr_reg = pc_reg;
+      `MEM_ALU : addr_reg = alu_out;
+    endcase
 
 endmodule
--- implementation/myOwnMPU_code/core/mem_addr_sel_tb.v
+++ implementation/myOwnMPU_code/core/mem_addr_sel_tb.v
@@ -8,7 +8,7 @@ module mem_addr_sel_tb;
   reg mem_sel_tb;
   reg [31:0] alu_out_tb;
   wire [31:0] addr_tb;
-  
+
   mem_addr_sel uut(
     .clk(clk_tb),
     .rst(rst_tb),
@@ -17,17 +17,85 @@ module mem_addr_sel_tb;
     .addr(addr_tb),
     .mem_sel(mem_sel_tb)
   );
-  
+
   initial begin
     $dumpfile("tb.vcd"); 
     $dumpvars;
-    
+
     rst_tb = 0;
-    #5 pc_sel_tb = 2'b01;
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 pc_sel_tb = 2'b00; // Value to the PC from ALU
+    alu_out_tb = 32'd10;
+    #10 alu_out_tb = 32'd3;
+    #10 alu_out_tb = -32'd4;
+    #10 alu_out_tb = 32'd4;
+    #10 alu_out_tb = -32'd16;
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 pc_sel_tb = 2'b01; // Increment actual value in PC
+    #50; 
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 pc_sel_tb = 2'b10; // Decrement actual value in PC
+    #100;
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 pc_sel_tb = 2'b00; // Value to the PC from ALU
+    alu_out_tb = 32'd10;
+    #10 alu_out_tb = 32'd3;
+    #10 rst_tb = 1;
+    #30 rst_tb = 0;
+    #10 alu_out_tb = -32'd4;
+    #10 alu_out_tb = 32'd4;
+    #10 alu_out_tb = -32'd16;
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 pc_sel_tb = 2'b11;
+    #50;
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 mem_sel_tb = 1'b0;
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//    
+    #20 alu_out_tb = 32'd10;
+    mem_sel_tb = 1'b1;
+    //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''//
+    //                          Test 1:                               //
+    // Move instructions from output to input (instruction comes from //
+    // RDATA reg). Look at the core structure                         //
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,//
+    #10 alu_out_tb = 32'd3;
+    #10 rst_tb = 1;
+    #30 rst_tb = 0;
+    #10 alu_out_tb = -32'd4;
+    #10 alu_out_tb = 32'd4;
+    #10 alu_out_tb = -32'd16;
     
-    #50 $finish;
+    #20 $finish;
   end
 
   always #5 clk_tb = ~clk_tb;
-  
+
 endmodule
