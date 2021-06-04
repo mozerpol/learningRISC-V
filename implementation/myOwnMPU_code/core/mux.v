638760fcbe97a899a3c35f353ff7a4d1d13009df
mozerpol <mozerpol@protonmail.com>
2021-06-04 13:02:19 +0200

    Add clog2 function

--- implementation/myOwnMPU_code/core/mux.v
+++ implementation/myOwnMPU_code/core/mux.v
@@ -1,7 +1,16 @@
+function integer clog2;
+  input integer value;
+  begin
+    value = value-1;
+    for(clog2=0; value>0; clog2=clog2+1)
+      value = value>>1;
+  end
+endfunction
+
 module mux(inputs, addr, out);  
   parameter DATA_WIDTH = 32,  
   INPUTS = 2,
-  ADDR_WIDTH= $clog2(INPUTS);
+  ADDR_WIDTH= clog2(INPUTS);
 
   input inputs;
   input addr;
