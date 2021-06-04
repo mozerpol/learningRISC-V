101b965408bbb49dfd207b3d0ebc58da10061e6e
mozerpol <mozerpol@protonmail.com>
2021-06-04 15:57:00 +0200

    Add comment to mux

--- implementation/myOwnMPU_code/core/mux.v
+++ implementation/myOwnMPU_code/core/mux.v
@@ -1,3 +1,14 @@
+/*
+	Theoretically it's not necessary include "mux" file to project. I hope... I compiled project
+    without this file and everything worked correct, so I suppose that it's not necessary. Moreover
+    there is one problem... This code refuses to compile under the old Verilog version :D
+    The prblem is with this line: "wire [DATA_WIDTH-1:0] inputs[INPUTS-1:0];". Unpacked
+    arrays are supported since SystemVerilog (or Verilog 2005, but I'm not sure). Anyway
+    it looks this code is not necessary.
+*/
+
+// Below function act as system function $clog2. Below function is necessary, because
+// in old v. of Verilog you can't use this system function. 
 function integer clog2;
   input integer value;
   begin
