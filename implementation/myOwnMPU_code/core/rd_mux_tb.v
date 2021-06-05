2f0a43b6fbccf3571b29c072d7a0a10f795e8202
mozerpol <mozerpol@protonmail.com>
2021-06-05 14:12:25 +0200

    Complete testbench

--- implementation/myOwnMPU_code/core/rd_mux_tb.v
+++ implementation/myOwnMPU_code/core/rd_mux_tb.v
@@ -2,7 +2,7 @@
  * Mozerpol 2021
  */
 
-`timescale 100ns / 10ns
+`timescale 100ns/10ns
 
 module rd_mux_tb;
 
@@ -21,36 +21,42 @@ module rd_mux_tb;
     .rd_mem(rd_mem_tb),
     .clk(clk_tb),
     .rd_sel(rd_sel_tb),
-    .rd_d(rd_d_tb)
+    .rd_d(rd_d_tb) // output from module
   );
 
   integer i, j;
-  reg [31:0] val_for_imm [0:4];
+  reg [31:0] machineWord [0:4];
 
   initial
     begin
       $dumpfile("dump.vcd"); 
       $dumpvars;
 
-      val_for_imm[0] = 32'd10;
-      val_for_imm[1] = 32'd3;
-      val_for_imm[2] = -32'd4;
-      val_for_imm[3] = 32'd4;
-      val_for_imm[4] = -32'd16;
+      machineWord[0] = 32'd10;
+      machineWord[1] = 32'd3;
+      machineWord[2] = -32'd4;
+      machineWord[3] = 32'd4;
+      machineWord[4] = -32'd16;
 
       for(i = 0; i < 4; i=i+1)
         begin
           #5 rd_sel_tb = i; 
-          
+
           for(j = 0; j < 5; j=j+1)
             begin
-              imm_tb = val_for_imm[j]; 
               #5;
+              case(i)
+                0 : imm_tb = machineWord[j]; 
+                1 : begin pc_tb = machineWord[j]; #5; end
+                2 : alu_out_tb = machineWord[j];
+                3 : rd_mem_tb = machineWord[j];
+              endcase
+
             end
         end
-
       #10 $finish;
     end
 
-  always #5 clk_tb = ~clk_tb;  
+  always #5 clk_tb = ~clk_tb;
+
 endmodule
