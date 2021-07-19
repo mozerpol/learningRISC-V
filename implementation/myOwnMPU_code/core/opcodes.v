15a661f5b29c126c8fc32f780a5c0c218dd02321
mozerpol <mozerpol@protonmail.com>
2021-07-19 14:50:51 +0200

    Delete equality sign from opcodes part

--- implementation/myOwnMPU_code/core/opcodes.v
+++ implementation/myOwnMPU_code/core/opcodes.v
@@ -1,38 +1,38 @@
 //''''''''''''//
 //   opcode   //
 //,,,,,,,,,,,,//
-`define	LOAD =		5'b00000
-`define	LOAD_FP =	5'b00001
-`define	CUSTOM_0 =	5'b00010
-`define	MISC_MEM =	5'b00011
-`define OP_IMM =	5'b00100
-`define AUIPC =		5'b00101
-`define OP_IMM_32 =	5'b00110
-`define LEN_48_0 =	5'b00111
-`define STORE =		5'b01000
-`define STORE_FP = 	5'b01001
-`define CUSTOM_1 =	5'b01010
-`define AMO = 		5'b01011
-`define OP =		5'b01100
-`define LUI =		5'b01101
-`define OP_32 =		5'b01110
-`define LEN_64 =	5'b01111
-`define MADD =		5'b10000
-`define MSUB =		5'b10001
-`define NMSUB =		5'b10010
-`define NMADD =		5'b10011
-`define OP_FP =		5'b10100
-`define RESERVED_0 = 5'b10101
-`define CUSTOM_2 =	5'b10110
-`define LEN_48_1 =	5'b10111
-`define BRANCH =	5'b11000
-`define JALR =		5'b11001
-`define RESERVED_1 = 5'b11010
-`define JAL =		5'b11011
-`define SYSTEM =	5'b11100
-`define RESERVED_2 = 5'b11101
-`define CUSTOM_3 =	5'b11110
-`define LEN_80 =	5'b11111
+`define	LOAD 		5'b00000
+`define	LOAD_FP 	5'b00001
+`define	CUSTOM_0 	5'b00010
+`define	MISC_MEM 	5'b00011
+`define OP_IMM 		5'b00100
+`define AUIPC 		5'b00101
+`define OP_IMM_32 	5'b00110
+`define LEN_48_0 	5'b00111
+`define STORE 		5'b01000
+`define STORE_FP  	5'b01001
+`define CUSTOM_1 	5'b01010
+`define AMO  		5'b01011
+`define OP 			5'b01100
+`define LUI 		5'b01101
+`define OP_32 		5'b01110
+`define LEN_64 		5'b01111
+`define MADD 		5'b10000
+`define MSUB 		5'b10001
+`define NMSUB 		5'b10010
+`define NMADD 		5'b10011
+`define OP_FP 		5'b10100
+`define RESERVED_0  5'b10101
+`define CUSTOM_2 	5'b10110
+`define LEN_48_1 	5'b10111
+`define BRANCH 		5'b11000
+`define JALR 		5'b11001
+`define RESERVED_1  5'b11010
+`define JAL 		5'b11011
+`define SYSTEM 		5'b11100
+`define RESERVED_2  5'b11101
+`define CUSTOM_3 	5'b11110
+`define LEN_80 		5'b11111
 
 //''''''''''''''''''//
 //   FUNC3_BRANCH   //
