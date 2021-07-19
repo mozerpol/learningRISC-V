ad01c656582cb9f77d8b7c42ee9736540f61a3d6
mozerpol <mozerpol@protonmail.com>
2021-07-19 11:35:00 +0200

    Include files for ctrl.v

---
+++ implementation/myOwnMPU_code/core/ctrl.v
@@ -0,0 +1,20 @@
+/*
+	By Mozerpol
+*/
+
+`timescale 100ns / 10ns
+
+`include "rysy_pkg.vh"
+`include "opcodes.vh"
+`include "inst_mgmt.v"
+`include "imm_mux.v"
+`include "alu.v"
+`include "cmp.v"
+`include "mem_addr_sel.v"
+`include "rd_mux.v"
+`include "alu1_mux.v"
+`include "alu2_mux.v"
+`include "select_pkg.v"
+
+module ctrl();
+endmodule
