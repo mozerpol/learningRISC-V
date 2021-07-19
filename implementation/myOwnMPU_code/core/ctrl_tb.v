df5d2f1f0995c8f4306b66d465f1aaaa08cd41ae
mozerpol <mozerpol@protonmail.com>
2021-07-19 15:09:40 +0200

    Add test bench for alu2_mux part

--- implementation/myOwnMPU_code/core/ctrl_tb.v
+++ implementation/myOwnMPU_code/core/ctrl_tb.v
@@ -35,7 +35,7 @@ module ctrl_tb;
   reg [2:0] func3_tb;
   reg [6:0] func7_tb;
   reg b_tb;
-  
+
   ctrl dut(
     .clk(clk_tb),
     .rst(rst_tb),
@@ -44,7 +44,7 @@ module ctrl_tb;
     .func7(func7_tb),
     .b(b_tb)
   );
-  
+
   initial begin
     $dumpfile("dump.vcd"); 
     $dumpvars;
@@ -56,17 +56,28 @@ module ctrl_tb;
     opcode_tb = `LUI;		#5;	// imm_type should return IMM_U 3'b001
     opcode_tb = `OP_IMM; 	#5;	// imm_type should return IMM_I 3'b100
     opcode_tb = `STORE; 	#5;	// imm_type should return IMM_S 3'b011
-    
+
     //'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
     //		Test for alu1_nux
     //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-  	
+
     opcode_tb = `JAL; 		#5;	// alu1_sel should return 1
     opcode_tb = `LOAD;	 	#5; // alu1_sel should return 0
-    
+
+    //'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+    //		Test for alu2_mux
+    //		We have only one case when alu2_sel return 0, it's
+    //		for opcode_tb = `OP
+    //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+    opcode_tb = `OP;		#5;	// alu2_sel should return 0
+    opcode_tb = 5'b10101;	#5;	// default value, alu2_sel should return 1
+    opcode_tb = `OP_IMM;	#5;	// alu2_sel should return 1
+    opcode_tb = `OP;		#5;	// alu2_sel should return 0
+
     $finish;
   end
-  
+
   always #5 clk_tb = ~clk_tb;
-  
+
 endmodule
