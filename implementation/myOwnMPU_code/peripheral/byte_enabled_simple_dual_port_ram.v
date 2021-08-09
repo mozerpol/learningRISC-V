58120e1a6a49441d7d472ede1ce3b1c13a2faf34
mozerpol <mozerpol@protonmail.com>
2021-08-09 17:16:19 +0200

    Change dimension of arrays
    Eliminate problem with dimension of arrays which was used for
    implementing RAM.

--- implementation/myOwnMPU_code/peripheral/byte_enabled_simple_dual_port_ram.v
+++ implementation/myOwnMPU_code/peripheral/byte_enabled_simple_dual_port_ram.v
@@ -22,12 +22,11 @@ module byte_enabled_simple_dual_port_ram
     output reg [WIDTH-1:0] q
   );
 
-
   localparam WORDS = 1 << ADDR_WIDTH;
 
   // use a multi-dimensional packed array to model individual bytes within the word
-  // reg [BYTE_WIDTH-1:0] ram[BYTES-1:0][0:WORDS-1];
-  reg [BYTE_WIDTH-1:0] ram[0:ADDR_WIDTH-1];
+
+  reg [WIDTH-1:0] ram[0:WORDS-1];
 
   initial
     begin
@@ -38,17 +37,15 @@ module byte_enabled_simple_dual_port_ram
     begin
       if(we) begin
         // edit this code if using other than four bytes per word
-        if(be[0]) ram[waddr][0] <= wdata[0];
-        if(be[1]) ram[waddr][1] <= wdata[1];
-        if(be[2]) ram[waddr][2] <= wdata[2];
-        if(be[3]) ram[waddr][3] <= wdata[3];
+        if(be[0]) ram[waddr][0] <= wdata[WIDTH-1:WIDTH-9];
+        if(be[1]) ram[waddr][1] <= wdata[23:16];
+        if(be[2]) ram[waddr][2] <= wdata[15:8];
+        if(be[3]) ram[waddr][3] <= wdata[7:0];
       end
       q <= ram[raddr];
     end
 
 endmodule
 /*
-poomocne pliki: reg_file, mux
-linki:
-https://www.chipverify.com/systemverilog/systemverilog-packed-arrays - moze jakos konkatenacja pomoze
+	Nice link about arrays in verilog: http://www.pepedocs.com/img/fpga-0.png
 */
