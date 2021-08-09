d77711cdf79594ecef01f16b41d14b0c92a869f2
mozerpol <mozerpol@protonmail.com>
2021-08-09 13:25:19 +0200

    Add comments

--- implementation/rysyCore_code/peripheral/byte_enabled_simple_dual_port_ram.sv
+++ implementation/rysyCore_code/peripheral/byte_enabled_simple_dual_port_ram.sv
@@ -1,9 +1,9 @@
 /*-
- * SPDX-License-Identifier: BSD-3-Clause
- *
- * Copyright (c) 2019 Rafal Kozik
- * All rights reserved.
- */
+   * SPDX-License-Identifier: BSD-3-Clause
+   *
+   * Copyright (c) 2019 Rafal Kozik
+   * All rights reserved.
+   */
 
 // Quartus Prime SystemVerilog Template
 //
@@ -11,40 +11,58 @@
 // and with a control for writing single bytes into the memory word; byte enable
 
 module byte_enabled_simple_dual_port_ram
-	#(parameter
-		CODE = "code/regop.mem",
-		int ADDR_WIDTH = 8,
-		int BYTE_WIDTH = 8,
-		int BYTES = 4,
-		int WIDTH = BYTES * BYTE_WIDTH
-)
-( 
-	input [ADDR_WIDTH-1:0] waddr,
-	input [ADDR_WIDTH-1:0] raddr,
-	input [BYTES-1:0] be,
-	input [BYTES-1:0][BYTE_WIDTH-1:0] wdata, 
-	input we, clk,
-	output reg [WIDTH - 1:0] q
-);
-	localparam int WORDS = 1 << ADDR_WIDTH;
+  #(parameter
+    CODE = "regop.mem",
+    int ADDR_WIDTH = 8,
+    int BYTE_WIDTH = 8,
+    int BYTES = 4,
+    int WIDTH = BYTES * BYTE_WIDTH
+   )
+  ( 
+    input [ADDR_WIDTH-1:0] waddr, // 8 width one vector: [7, 6, 5, 4, 3, 2, 1, 0]
+    input [ADDR_WIDTH-1:0] raddr,
+    input [BYTES-1:0] be,
+    input [BYTES-1:0][BYTE_WIDTH-1:0] wdata, // 4x8 vector:
+    /*
+          [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0]
+      */
+    input we, clk,
+    output reg [WIDTH-1:0] q
+  );
+  localparam int WORDS = 1 << ADDR_WIDTH;
 
-	// use a multi-dimensional packed array to model individual bytes within the word
-	logic [BYTES-1:0][BYTE_WIDTH-1:0] ram[0:WORDS-1];
-	
-	initial
-	begin
-		$readmemh(CODE, ram);
-	end
+  // use a multi-dimensional packed array to model individual bytes within the word
+  logic [BYTES-1:0][BYTE_WIDTH-1:0] ram[0:WORDS-1]; // eight 4x8 vectors:
+  /*
+    0:
+		[7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0]
+	1:
+    	[7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0]
+	.
+    .
+    .
 
-	always_ff@(posedge clk)
-	begin
-		if(we) begin
-		// edit this code if using other than four bytes per word
-			if(be[0]) ram[waddr][0] <= wdata[0];
-			if(be[1]) ram[waddr][1] <= wdata[1];
-			if(be[2]) ram[waddr][2] <= wdata[2];
-			if(be[3]) ram[waddr][3] <= wdata[3];
-		end
-		q <= ram[raddr];
-	end
+    7:
+    	[7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0]
+
+	*/
+
+  initial
+    begin
+      $readmemh(CODE, ram);
+    end
+
+  always_ff@(posedge clk)
+    begin
+      if(we) begin
+        // edit this code if using other than four bytes per word
+        if(be[0]) ram[waddr][0] <= wdata[0];
+        if(be[1]) ram[waddr][1] <= wdata[1];
+        if(be[2]) ram[waddr][2] <= wdata[2];
+        if(be[3]) ram[waddr][3] <= wdata[3];
+      end
+      q <= ram[raddr];
+    end
 endmodule : byte_enabled_simple_dual_port_ram
+
+// Nice link about arrays: http://www.pepedocs.com/notes?tid=fpga&nid=fpga
