9c92c5c39b2f790f8700c6697604181e38e74ecf
mozerpol <mozerpol@protonmail.com>
2021-08-19 14:00:36 +0200

    Add more comments

--- implementation/rysyCore_code/peripheral/byte_enabled_simple_dual_port_ram.sv
+++ implementation/rysyCore_code/peripheral/byte_enabled_simple_dual_port_ram.sv
@@ -35,7 +35,7 @@ module byte_enabled_simple_dual_port_ram
   // use a multi-dimensional packed array to model individual bytes within the word
   logic [BYTES-1:0][BYTE_WIDTH-1:0] ram[0:WORDS-1]; // One hundred 4x8 vectors:
   /*
-    0:
+    0:         fourth byte               third		              second
 		[7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0]
 	1:
     	[7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0] [7, 6, 5, 4, 3, 2, 1, 0]
@@ -48,7 +48,6 @@ module byte_enabled_simple_dual_port_ram
 
     So we have in one row 4x8 bits, so it's four bytes. So... one row has four bytes and we have 256 rows,
     when we'll multiply 256 rows x 4 bytes = 256x4 = 1024 bytes = 1 kB. So our memory has 1 kB.
-
 	*/
 
   initial
@@ -61,7 +60,8 @@ module byte_enabled_simple_dual_port_ram
     begin
       if(we) begin
         // edit this code if using other than four bytes per word
-        if(be[0]) ram[waddr][0] <= wdata[0];
+        if(be[0]) ram[waddr][0] <= wdata[0]; // First octet from Wdata save in ram
+        // under first byte address from waddr
         if(be[1]) ram[waddr][1] <= wdata[1];
         if(be[2]) ram[waddr][2] <= wdata[2];
         if(be[3]) ram[waddr][3] <= wdata[3];
