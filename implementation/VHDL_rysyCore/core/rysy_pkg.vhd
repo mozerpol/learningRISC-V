971417c44823090454fb715e1031f59a2f75940f
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-03 19:25:55 +0100

    Add  rysy_pkg with constants

---
+++ implementation/VHDL_rysyCore/core/rysy_pkg.vhd
@@ -0,0 +1,14 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.std_logic_unsigned.all;
+
+package rysyPkg is
+   constant REG_LEN     : integer := 32;
+   constant REG_NUM     : integer := 32;
+   constant ADDR_LEN    : integer := 5;
+   constant NOP         : integer := 32x"00000013";
+end;
+
+package body rysyPkg is
+
+end package body;
