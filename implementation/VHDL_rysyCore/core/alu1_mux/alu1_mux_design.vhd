ad3f162179dfd94ce4984f5395476e2bc5c86d77
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-04 14:25:11 +0100

    Add architecture and fix inputs in entity

--- implementation/VHDL_rysyCore/core/alu1_mux/alu1_mux_design.vhd
+++ implementation/VHDL_rysyCore/core/alu1_mux/alu1_mux_design.vhd
@@ -1,12 +1,19 @@
 library ieee;
    use ieee.std_logic_1164.all;
+   use ieee.std_logic_unsigned.all;
+library rysy_pkg;
+   use rysy_pkg.rysyPkg.all;
 
 
 entity alu1_mux_design is
    port (
-      input rs1_d : std_logic
+      rs1_d : in std_logic_vector(REG_LEN-1 downto 0);
+      pc    : in std_logic_vector(REG_LEN-1 downto 0)
+
    );
 end entity alu1_mux_design;
 
+architecture rtl of alu1_mux_design is
+begin
 
-
+end architecture rtl;
