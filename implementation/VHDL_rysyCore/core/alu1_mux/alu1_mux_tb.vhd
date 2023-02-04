da88b33a369fb98009bdfb3f087bfbc35c2a9abb
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-04 14:31:46 +0100

    Add tb draft

---
+++ implementation/VHDL_rysyCore/core/alu1_mux/alu1_mux_tb.vhd
@@ -0,0 +1,14 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
+   use ieee.std_logic_unsigned.all;
+library rysy_pkg;
+   use rysy_pkg.rysyPkg.all;
+
+entity alu1_mux_tb is
+end alu1_mux_tb;
+
+architecture tb of alu1_mux_tb is
+begin
+
+end architecture tb;
