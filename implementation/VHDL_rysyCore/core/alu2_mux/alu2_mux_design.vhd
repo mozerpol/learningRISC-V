9b546739c0115da1f865300a9cb7f08437391f53
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-19 20:54:54 +0100

    Add all design body

--- implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_design.vhd
+++ implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_design.vhd
@@ -1,37 +1,36 @@
--- Multiplexer can take two values:
--- 1. From the PC register to pass a value to the ALU, which next calculate a
---    new address, for example to perform jump. But before passing value from PC
---    to ALU must wait two clock cycles to empty pipeline. Two lines of code do
---    this:
---    old_pc(1) <= pc;
---    old_pc(0) <= old_pc(1);
--- 2. Operand rs1_d which comes from reg_file. This operand can be simple number
---    for ALU like 12 or 3.
-
 library ieee;
    use ieee.std_logic_1164.all;
    use ieee.std_logic_unsigned.all;
 library rysy_pkg;
    use rysy_pkg.rysyPkg.all;
-
+library alu2_mux_lib;
+   use alu2_mux_lib.all;
+   use alu2_mux_lib.alu2_mux_pkg.all;
 
 entity alu2_mux_design is
    port (
+      signal rs2_d    : in std_logic_vector(REG_LEN-1 downto 0);
+      signal imm      : in std_logic_vector(REG_LEN-1 downto 0);
+      signal alu2_sel : in std_logic;
+      signal alu2_in2 : out std_logic_vector(REG_LEN-1 downto 0)
    );
 end entity alu2_mux_design;
 
 architecture rtl of alu2_mux_design is
 
+   signal o    : std_logic_vector(REG_LEN-1 downto 0);
+
 begin
 
-   p_alu2_pc : process(clk)
-   begin
-      if (clk'event and clk = '1') then
-      end if;
-   end process p_alu2_pc;
+   alu2_in2 <= o;
 
    p_alu2_sel : process(all)
    begin
+      case alu2_sel is
+         when ALU2_RS   => o <= rs2_d;
+         when ALU2_IMM  => o <= imm;
+         when others    => NULL;
+      end case;
    end process p_alu2_sel;
 
 end architecture rtl;
