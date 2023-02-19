0be19eb4684a022edb2681288e6c22c768db9b8b
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-19 20:55:23 +0100

    Bind test with design

--- implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_tb.vhd
+++ implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_tb.vhd
@@ -14,12 +14,25 @@ architecture tb of alu2_mux_tb is
 
    component alu2_mux_design is
    port (
+      signal rs2_d    : in std_logic_vector(REG_LEN-1 downto 0);
+      signal imm      : in std_logic_vector(REG_LEN-1 downto 0); 
+      signal alu2_sel : in std_logic;
+      signal alu2_in2  : out std_logic_vector(REG_LEN-1 downto 0)
    );
    end component alu2_mux_design;
+
+   signal rs2_d_tb      : std_logic_vector(REG_LEN-1 downto 0);   
+   signal imm_tb        : std_logic_vector(REG_LEN-1 downto 0); 
+   signal alu2_sel_tb   : std_logic;
+   signal alu2_in2_tb    : std_logic_vector(REG_LEN-1 downto 0); 
 
 begin
    inst_alu2_mux : component alu2_mux_design 
    port map (
+      rs2_d       => rs2_d_tb,
+      imm         => imm_tb,
+      alu2_sel    => alu2_sel_tb,
+      alu2_in2    => alu2_in2_tb
    );
 
    p_tb : process
@@ -27,13 +40,5 @@ begin
       wait for 25 ns;
       stop(2); 
    end process p_tb;
-
-   p_clock_gen : process
-   begin
-      clk_tb <= '0';
-      wait for 5 ns;
-      clk_tb <= '1';
-      wait for 5 ns;
-   end process p_clock_gen;
 
 end architecture tb;
