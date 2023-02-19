4382c8e3679dab2150ab9fb826f9ce110492ada0
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-19 20:55:07 +0100

    Add consts

--- implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_pkg.vhd
+++ implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_pkg.vhd
@@ -5,6 +5,9 @@ library ieee;
   
  package alu2_mux_pkg is
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
+   constant ALU2_RS    : std_logic := '0';
+   constant ALU2_IMM   : std_logic := '1';
+
  end;
  
  package body alu2_mux_pkg is
