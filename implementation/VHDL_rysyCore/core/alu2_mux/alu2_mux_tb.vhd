7ecc73fc5a11513e0e507dc4e5fe2995173273d1
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-20 19:27:06 +0100

    Add tb

--- implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_tb.vhd
+++ implementation/VHDL_rysyCore/core/alu2_mux/alu2_mux_tb.vhd
@@ -24,7 +24,11 @@ architecture tb of alu2_mux_tb is
    signal rs2_d_tb      : std_logic_vector(REG_LEN-1 downto 0);   
    signal imm_tb        : std_logic_vector(REG_LEN-1 downto 0); 
    signal alu2_sel_tb   : std_logic;
-   signal alu2_in2_tb    : std_logic_vector(REG_LEN-1 downto 0); 
+   signal alu2_in2_tb   : std_logic_vector(REG_LEN-1 downto 0); 
+   type t_val_for_rs2_d is array(0 to 4) of std_logic_vector(31 downto 0);
+   signal val_for_rs2_d : t_val_for_rs2_d;
+   type t_val_for_imm   is array(0 to 5) of std_logic_vector(31 downto 0);
+   signal val_for_imm   : t_val_for_imm;
 
 begin
    inst_alu2_mux : component alu2_mux_design 
@@ -37,6 +41,30 @@ begin
 
    p_tb : process
    begin
+      val_for_rs2_d(0)   <= 32x"10";
+      val_for_rs2_d(1)   <= 32x"3";
+      val_for_rs2_d(2)   <= 32sx"FFF0"; -- -4
+      val_for_rs2_d(3)   <= 32x"4";
+      val_for_rs2_d(4)   <= 32sx"FFFC"; -- -16
+      val_for_imm(0)     <= 32x"0";
+      val_for_imm(1)     <= 32x"1";
+      val_for_imm(2)     <= 32x"2";
+      val_for_imm(3)     <= 32x"10";
+      val_for_imm(4)     <= 32sx"FFFC";  -- -16
+      val_for_imm(5)     <= 32sx"FFF0"; -- -4
+
+      alu2_sel_tb        <= '0';
+      for i in 0 to 4 loop
+         wait for 10 ns;
+         rs2_d_tb       <= val_for_rs2_d(i);
+      end loop;
+
+      wait for 10 ns;
+      alu2_sel_tb       <= '1';
+      for i in 0 to 5 loop
+         imm_tb         <= val_for_imm(i);
+      end loop;
+
       wait for 25 ns;
       stop(2); 
    end process p_tb;
