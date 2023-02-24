60f1c776ba656cf7eb504a0509acf76a90f77d1a
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-24 22:50:56 +0100

    Add ALU process

--- implementation/VHDL_rysyCore/core/alu/alu_design.vhd
+++ implementation/VHDL_rysyCore/core/alu/alu_design.vhd
@@ -1,6 +1,7 @@
 library ieee;
    use ieee.std_logic_1164.all;
    use ieee.std_logic_unsigned.all;
+   use ieee.numeric_std.all;
 library rysy_pkg;
    use rysy_pkg.rysyPkg.all;
 library alu_lib;
@@ -19,18 +20,29 @@ end entity alu;
 architecture rtl of alu is
 
    signal o          : std_logic_vector(REG_LEN-1 downto 0);
-   signal alu_in1_s  : std_logic_vector(REG_LEN-1 downto 0);
-   signal alu_in2_s  : std_logic_vector(REG_LEN-1 downto 0);
 
 begin
 
-   --alu_out     <= o;
-   alu_in1_s   <= alu_in1;
-   alu_in2_s   <= alu_in2;
+   alu_out     <= o;
 
    p_alu : process(all)
    begin
-      alu_out <= (others => '1');
+      case alu_op is
+         when ADD    => o <= alu_in1 + alu_in2;
+         when "0001" => o <= alu_in1 - alu_in2; -- SUB
+         when "0010" => o <= alu_in1 xor alu_in2; -- XOR
+         when "0011" => o <= alu_in1 or alu_in2; -- OR
+         when "0100" => o <= alu_in1 and alu_in2; -- AND
+         when "0101" => o <= std_logic_vector(unsigned(alu_in1) sll 
+            to_integer(unsigned(alu_in2))); -- SLL
+         when "0110" => o <= std_logic_vector(unsigned(alu_in1) srl 
+            to_integer(unsigned(alu_in2))); -- SRL
+         when "0111" => o <= std_logic_vector(unsigned(alu_in1) sra
+            to_integer(unsigned(alu_in2))); -- SRA
+         when SLT    => o <= alu_in1 when alu_in1 > alu_in2 else alu_in2;
+         when SLTU   => o <= alu_in1 when alu_in1 > alu_in2 else alu_in2; 
+         when others => o <= (others => '0');
+      end case;
    end process p_alu;
 
 end architecture rtl;
