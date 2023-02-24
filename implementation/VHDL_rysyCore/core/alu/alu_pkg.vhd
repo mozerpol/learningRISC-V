2870a8b3155760413f425ce56c2a1f147faee944
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-24 22:54:08 +0100

    Clear package, delete unused constants

--- implementation/VHDL_rysyCore/core/alu/alu_pkg.vhd
+++ implementation/VHDL_rysyCore/core/alu/alu_pkg.vhd
@@ -6,13 +6,6 @@ library ieee;
  package alu_pkg is
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
     constant ADD  : std_logic_vector(3 downto 0) := "0000";
-    -- constant SUB  : std_logic_vector(3 downto 0) := "0001";
-    -- constant XOR : std_logic_vector(3 downto 0) := "0010";
-    -- constant OR : std_logic_vector(3 downto 0) :=  "0011";
-    -- constant AND : std_logic_vector(3 downto 0) := "0100";
-    -- constant SLL : std_logic_vector(3 downto 0) := "0101";
-    -- constant SRL : std_logic_vector(3 downto 0) := "0110";
-    -- constant SRA : std_logic_vector(3 downto 0) := "0111";
     constant SLT  : std_logic_vector(3 downto 0) := "1000";
     constant SLTU : std_logic_vector(3 downto 0) :="1001";
 
