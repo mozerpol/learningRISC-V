c854cf63804098dea537ea8675847bce581627a1
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-25 19:19:38 +0100

    Add consts to package

--- implementation/VHDL_rysyCore/core/cmp/cmp_pkg.vhd
+++ implementation/VHDL_rysyCore/core/cmp/cmp_pkg.vhd
@@ -5,6 +5,12 @@ library ieee;
   
  package cmp_pkg is
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
+    constant EQ   : std_logic_vector(2 downto 0) := "000";
+    constant NE   : std_logic_vector(2 downto 0) := "001";
+    constant LT   : std_logic_vector(2 downto 0) := "010";
+    constant GE   : std_logic_vector(2 downto 0) := "011";
+    constant LTU  : std_logic_vector(2 downto 0) := "100";
+    constant GEU  : std_logic_vector(2 downto 0) := "101";
     
  end;
  
