98b1dc9d482d4af610eb8306f5fd37676a1a542f
mozerpol <krzmozer@student.pg.edu.pl>
2023-02-25 20:01:15 +0100

    Add and bind ports in design and test

--- implementation/VHDL_rysyCore/core/cmp/cmp_design.vhd
+++ implementation/VHDL_rysyCore/core/cmp/cmp_design.vhd
@@ -10,15 +10,30 @@ library cmp_lib;
 
 entity cmp is
    port (
+      rs1_d    : in std_logic_vector(REG_LEN-1 downto 0);
+      rs2_d    : in std_logic_vector(REG_LEN-1 downto 0);
+      cmp_op   : in std_logic_vector(2 downto 0);
+      b        : out std_logic
    );
 end entity cmp;
 
 architecture rtl of cmp is
 
+   signal rs1_d_s    : std_logic_vector(REG_LEN-1 downto 0);
+   signal rs2_d_s    : std_logic_vector(REG_LEN-1 downto 0);
+   signal o          : std_logic;
+
 begin
+
+   b        <= o;
+   rs1_d_s  <= rs1_d;
+   rs2_d_s  <= rs2_d;
 
    p_cmp : process(all)
    begin
+      -- case (cmp_op) is
+         --when EQ => o <= (rs1_d == rs2_d);
+
    end process p_cmp;
 
 end architecture rtl;
--- implementation/VHDL_rysyCore/core/cmp/cmp_tb.vhd
+++ implementation/VHDL_rysyCore/core/cmp/cmp_tb.vhd
@@ -14,20 +14,33 @@ architecture tb of cmp_tb is
 
    component cmp is
    port (
+      rs1_d    : in std_logic_vector(REG_LEN-1 downto 0);
+      rs2_d    : in std_logic_vector(REG_LEN-1 downto 0);
+      cmp_op   : in std_logic_vector(2 downto 0);
+      b        : out std_logic
    );
    end component cmp;
+
+   signal rs1_d_tb   : std_logic_vector(REG_LEN-1 downto 0);
+   signal rs2_d_tb   : std_logic_vector(REG_LEN-1 downto 0); 
+   signal cmp_op_tb  : std_logic_vector(2 downto 0);
+   signal b_tb       : std_logic;
  
 begin
 
    inst_cmp : component cmp 
    port map (
+      rs1_d    => rs1_d_tb,
+      rs2_d    => rs2_d_tb,
+      cmp_op   => cmp_op_tb,
+      b        => b_tb
    );
 
-   p_tb : process
+   cmp_tb : process
    begin
        
       wait for 25 ns;
       stop(2); 
-   end process p_tb;
+   end process cmp_tb;
 
 end architecture tb;
