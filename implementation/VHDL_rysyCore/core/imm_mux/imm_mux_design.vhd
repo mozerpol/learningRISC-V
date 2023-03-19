3a5d08a787c7d14ce00ec005eadc783ce886f54c
mozerpol <krzmozer@student.pg.edu.pl>
2023-03-19 21:21:26 +0100

    Add all code to design

--- implementation/VHDL_rysyCore/core/imm_mux/imm_mux_design.vhd
+++ implementation/VHDL_rysyCore/core/imm_mux/imm_mux_design.vhd
@@ -10,6 +10,13 @@ library imm_mux_lib;
 
 entity imm_mux is
    port (
+      imm_type    : in std_logic_vector(2 downto 0);
+      imm_J       : in std_logic_vector(REG_LEN-1 downto 0);
+      imm_U       : in std_logic_vector(REG_LEN-1 downto 0);
+      imm_B       : in std_logic_vector(REG_LEN-1 downto 0);
+      imm_S       : in std_logic_vector(REG_LEN-1 downto 0);
+      imm_I       : in std_logic_vector(REG_LEN-1 downto 0);
+      imm         : out std_logic_vector(REG_LEN-1 downto 0)
    );
 end entity imm_mux;
 
@@ -19,6 +26,14 @@ begin
 
    p_imm_mux : process(all)
    begin
+      case (imm_type) is
+         when C_IMM_J => imm    <= imm_J;
+         when C_IMM_U => imm    <= imm_U;
+         when C_IMM_B => imm    <= imm_B;
+         when C_IMM_S => imm    <= imm_S;
+         when C_IMM_I => imm    <= imm_I;
+         when others => imm   <= (others => '0');
+      end case;
    end process p_imm_mux;
 
 end architecture rtl;
