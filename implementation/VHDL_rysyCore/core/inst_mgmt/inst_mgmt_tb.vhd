27190be43a3f44ed0bcb2232879282fa0ec627d7
mozerpol <krzmozer@student.pg.edu.pl>
2023-03-20 21:53:13 +0100

    Bind ports, add tests

--- implementation/VHDL_rysyCore/core/inst_mgmt/inst_mgmt_tb.vhd
+++ implementation/VHDL_rysyCore/core/inst_mgmt/inst_mgmt_tb.vhd
@@ -6,6 +6,9 @@ library rysy_pkg;
    use rysy_pkg.rysyPkg.all;
 library std;
   use std.env.all;
+library inst_mgmt_lib;
+   use inst_mgmt_lib.all;
+   use inst_mgmt_lib.inst_mgmt_pkg.all;
 
 entity inst_mgmt_tb is
 end inst_mgmt_tb;
@@ -14,17 +17,75 @@ architecture tb of inst_mgmt_tb is
 
    component inst_mgmt is
    port (
+      signal clk        : in std_logic;
+      signal rst        : in std_logic;
+      signal inst_sel   : in std_logic_vector(1 downto 0);
+      signal rdata      : in std_logic_vector(REG_LEN-1 downto 0);
+      signal inst       : out std_logic_vector(REG_LEN-1 downto 0)
    );
    end component inst_mgmt;
- 
+   
+   signal clk_tb        : std_logic;
+   signal rst_tb        : std_logic;
+   signal inst_sel_tb   : std_logic_vector(1 downto 0);
+   signal rdata_tb      : std_logic_vector(REG_LEN-1 downto 0);
+   signal inst_tb       : std_logic_vector(REG_LEN-1 downto 0);
+
 begin
 
    inst_inst_mgmt : component inst_mgmt 
    port map (
+      clk      => clk_tb,
+      rst      => rst_tb,
+      inst_sel => inst_sel_tb,
+      rdata    => rdata_tb,
+      inst     => inst_tb
    );
+   
+   inst_clk_gen : process
+   begin
+      clk_tb <= '0';
+      wait for 1 ns;
+      clk_tb <= '1';
+      wait for 1 ns;
+   end process inst_clk_gen;
 
    inst_mgmt_tb : process
    begin
+      rst_tb       <= '1';
+      wait for 10 ns;
+      rst_tb       <= '0';
+      wait for 10 ns;
+      inst_sel_tb  <= INST_MEM;
+      rdata_tb     <= 32d"11";
+      wait for 10 ns;
+      rdata_tb     <= 32d"3";
+      wait for 10 ns;
+      rdata_tb     <= 32d"4";
+      wait for 10 ns;
+      rdata_tb     <= 32d"16";
+      wait for 10 ns;
+
+      inst_sel_tb <= INST_OLD;
+      wait for 10 ns;
+      inst_sel_tb <= INST_NOP;
+      wait for 10 ns;
+      inst_sel_tb <= INST_NOP;
+      wait for 10 ns;
+      inst_sel_tb <= INST_MEM;
+      rdata_tb    <= 32d"11";
+      wait for 10 ns;
+      rdata_tb    <= 32d"3";
+      wait for 10 ns;
+      rdata_tb    <= 32d"0";
+      wait for 10 ns;
+      rdata_tb    <= 32d"1";
+      wait for 10 ns;
+      rdata_tb    <= 32d"0";
+      wait for 10 ns;
+      rdata_tb    <= 32d"4";
+      wait for 10 ns;
+      rdata_tb    <= 32d"16";
 
       wait for 25 ns;
       stop(2); 
