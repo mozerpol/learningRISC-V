72e95b05973cff5d2c7de1c6b803b4f41b5201cb
mozerpol <krzmozer@student.pg.edu.pl>
2023-03-21 18:53:52 +0100

    Add constants

--- implementation/VHDL_rysyCore/core/inst_mgmt/inst_mgmt_pkg.vhd
+++ implementation/VHDL_rysyCore/core/inst_mgmt/inst_mgmt_pkg.vhd
@@ -5,7 +5,10 @@ library ieee;
   
  package inst_mgmt_pkg is
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
-    
+    constant C_NOP      : std_logic_vector(31 downto 0) := 32x"00000013";
+    constant INST_OLD   : std_logic_vector(1 downto 0)  := "00";
+    constant INST_NOP   : std_logic_vector(1 downto 0)  := "01"; 
+    constant INST_MEM   : std_logic_vector(1 downto 0)  := "10";
  end;
  
  package body inst_mgmt_pkg is
