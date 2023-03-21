ba17fb9f406e7269003f04e45cfdac878efda6ea
mozerpol <krzmozer@student.pg.edu.pl>
2023-03-21 19:45:14 +0100

    Add constants

--- implementation/VHDL_rysyCore/core/mem_addr_sel/mem_addr_sel_pkg.vhd
+++ implementation/VHDL_rysyCore/core/mem_addr_sel/mem_addr_sel_pkg.vhd
@@ -5,7 +5,13 @@ library ieee;
   
  package mem_addr_sel_pkg is
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
-    
+    constant PC_ALU     : std_logic_vector(1 downto 0) := "00";
+    constant PC_P4      : std_logic_vector(1 downto 0) := "01";
+    constant PC_M4      : std_logic_vector(1 downto 0) := "10";
+    constant PC_OLD     : std_logic_vector(1 downto 0) := "11";
+    constant MEM_PC     : std_logic                    := '0';
+    constant MEM_ALU    : std_logic                    := '1';
+   
  end;
  
  package body mem_addr_sel_pkg is
