4643790a4f41fa654360bb611214cbd47adf9430
mozerpol <krzmozer@student.pg.edu.pl>
2023-03-21 19:44:59 +0100

    Add all code to design

--- implementation/VHDL_rysyCore/core/mem_addr_sel/mem_addr_sel_design.vhd
+++ implementation/VHDL_rysyCore/core/mem_addr_sel/mem_addr_sel_design.vhd
@@ -10,6 +10,13 @@ library mem_addr_sel_lib;
 
 entity mem_addr_sel is
    port (
+      clk      : in std_logic;
+      rst      : in std_logic;
+      pc_sel   : in std_logic_vector(1 downto 0);
+      mem_sel  : in std_logic;
+      alu_out  : in std_logic_vector(REG_LEN-1 downto 0);
+      pc       : out std_logic_vector(REG_LEN-1 downto 0);
+      addr     : out std_logic_vector(REG_LEN-1 downto 0)
    );
 end entity mem_addr_sel;
 
@@ -17,8 +24,22 @@ architecture rtl of mem_addr_sel is
 
 begin
 
-   p_mem_addr_sel : process(all)
+   addr <= pc      when mem_sel = MEM_PC else
+           alu_out when mem_sel = MEM_ALU;
+   
+   p_mem_addr_sel : process(clk, rst)
    begin
+      if (rst = '1') then
+         pc    <= (others => '0');
+      elsif (clk'event and clk = '1') then
+         case (pc_sel) is
+            when PC_ALU => pc <= alu_out;
+            when PC_P4  => pc <= pc + 4;
+            when PC_M4  => pc <= pc - 4;
+            when PC_OLD => pc <= pc;
+            when others => pc <= pc;
+         end case;
+      end if;
    end process p_mem_addr_sel;
 
 end architecture rtl;
