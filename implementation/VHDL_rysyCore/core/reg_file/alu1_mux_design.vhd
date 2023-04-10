e162f632e96092f9795e1fe99f35891b85812f09
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-10 14:11:30 +0200

    Create reg_file part, design, test, package and modify scripts

---
+++ implementation/VHDL_rysyCore/core/reg_file/alu1_mux_design.vhd
@@ -0,0 +1,17 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.std_logic_unsigned.all;
+library rysy_pkg;
+   use rysy_pkg.rysyPkg.all;
+
+
+entity reg_file_design is
+   port (
+   );
+end entity reg_file_design;
+
+architecture rtl of reg_file_design is
+
+begin
+
+end architecture rtl;
