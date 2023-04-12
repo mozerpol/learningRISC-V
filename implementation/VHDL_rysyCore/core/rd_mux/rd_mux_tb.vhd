acb98cf4d285247deb6378d39f1d073771519a52
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-12 22:58:31 +0200

    Add test

--- implementation/VHDL_rysyCore/core/rd_mux/rd_mux_tb.vhd
+++ implementation/VHDL_rysyCore/core/rd_mux/rd_mux_tb.vhd
@@ -24,13 +24,15 @@ architecture tb of rd_mux_tb is
    );
    end component rd_mux_design;
 
-   signal imm_tb     : std_logic_vector(REG_LEN-1 downto 0);
-   signal pc_tb      : std_logic_vector(REG_LEN-1 downto 0);
-   signal alu_out_tb : std_logic_vector(REG_LEN-1 downto 0);
-   signal rd_mem_tb  : std_logic_vector(REG_LEN-1 downto 0);
-   signal clk_tb     : std_logic;
-   signal rd_sel_tb  : std_logic_vector(1 downto 0);
-   signal rd_d_tb    : std_logic_vector(REG_LEN-1 downto 0);
+   signal imm_tb        : std_logic_vector(REG_LEN-1 downto 0);
+   signal pc_tb         : std_logic_vector(REG_LEN-1 downto 0);
+   signal alu_out_tb    : std_logic_vector(REG_LEN-1 downto 0);
+   signal rd_mem_tb     : std_logic_vector(REG_LEN-1 downto 0);
+   signal clk_tb        : std_logic;
+   signal rd_sel_tb     : std_logic_vector(1 downto 0);
+   signal rd_d_tb       : std_logic_vector(REG_LEN-1 downto 0);
+   type t_machineWord   is array(0 to 4) of std_logic_vector(31 downto 0);
+   signal machineWord   : t_machineWord;
 
 begin
    inst_rd_mux : component rd_mux_design 
@@ -46,9 +48,38 @@ begin
 
    p_tb : process
    begin
-   
+      machineWord(0) <= 32d"10";
+      machineWord(1) <= 32d"3";
+      machineWord(2) <= 32d"4";
+      machineWord(3) <= 32d"16";
+      machineWord(4) <= 32d"5";
+
+      for i in 0 to 4 loop
+         rd_sel_tb <= std_logic_vector(to_unsigned(i,2));
+         wait for 10 ns;
+         for j in 0 to 4 loop
+            case (i) is
+               when 0 => imm_tb        <= machineWord(j);
+               when 1 => pc_tb         <= machineWord(j);
+               when 2 => alu_out_tb    <= machineWord(j);
+               when 3 => rd_mem_tb     <= machineWord(j);
+               when others => NULL;
+            end case;
+               wait for 10 ns;
+
+         end loop;
+      end loop;
+
       wait for 25 ns;
       stop(2); 
    end process p_tb;
+
+   p_clk : process
+   begin
+      clk_tb <= '1';
+      wait for 1 ns;
+      clk_tb <= '0';
+      wait for 1 ns;
+   end process p_clk;
 
 end architecture tb;
