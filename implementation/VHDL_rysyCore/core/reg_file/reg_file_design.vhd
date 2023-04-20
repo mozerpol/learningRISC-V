97ba134eeaab7eef94a8606be9175f179169820a
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-20 20:28:43 +0200

    Add design

--- implementation/VHDL_rysyCore/core/reg_file/reg_file_design.vhd
+++ implementation/VHDL_rysyCore/core/reg_file/reg_file_design.vhd
@@ -1,6 +1,7 @@
 library ieee;
    use ieee.std_logic_1164.all;
    use ieee.std_logic_unsigned.all;
+   use ieee.numeric_std.all;
 library rysy_pkg;
    use rysy_pkg.rysyPkg.all;
 
@@ -23,6 +24,22 @@ end entity reg_file_design;
 
 architecture rtl of reg_file_design is
 
+   type t_x  is array(REG_LEN downto 0) of std_logic_vector(REG_LEN-1 downto 0);
+   signal x : t_x;
+
 begin
+
+
+   rs1_d <= 32x"0" when rs1 = 5b"0" else x(to_integer(signed(rs1)));
+   rs2_d <= 32x"0" when rs2 = 5b"0" else x(to_integer(signed(rs2)));
+
+   p_reg : process(clk)
+   begin
+      if (clk'event and clk = '1') then
+         if(reg_wr) then
+            x(to_integer(signed(rd))) <= rd_d;
+         end if;
+      end if;
+   end process p_reg;
 
 end architecture rtl;
