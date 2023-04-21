63dbe52b41318294e0e8a6a19f5e01ebf0e0eb04
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-21 18:18:55 +0200

    Add select_pkg with constants

---
+++ implementation/VHDL_rysyCore/core/select_pkg.vhd
@@ -0,0 +1,16 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.std_logic_unsigned.all;
+
+package selectPkg is
+   -- SB, SH, ... are S-type instructions, storage instructions.
+   constant SB     : std_logic_vector(2 downto 0) := "000;
+   constant SH     : std_logic_vector(2 downto 0) := "001;
+   constant SW     : std_logic_vector(2 downto 0) := "010;
+   constant SBU    : std_logic_vector(2 downto 0) := "011;
+   constant SHU    : std_logic_vector(2 downto 0) := "100;
+end;
+
+package body selectPkg is
+
+end package body;
