981ee496c1568c9ff0b80bdbec848c561ad1bb5f
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-22 13:43:56 +0200

    Add empty case

--- implementation/VHDL_rysyCore/core/select_rd/select_rd_design.vhd
+++ implementation/VHDL_rysyCore/core/select_rd/select_rd_design.vhd
@@ -4,6 +4,9 @@ library ieee;
    use ieee.numeric_std.all;
 library rysy_pkg;
    use rysy_pkg.rysyPkg.all;
+library select_rd_lib;
+   use select_rd_lib.all;
+   use select_rd_lib.select_rd_pkg.all;
 
 
 entity select_rd_design is
@@ -18,5 +21,26 @@ end entity select_rd_design;
 architecture rtl of select_rd_design is
 
 begin
+
+   p_select_rd : process(all)
+   begin
+      case (sel_type) is
+         when SB => 
+            case (sel_addr_old) is
+               when "00" => rd_mem(REG_LEN-1 downto 0) <= (rdata(7 downto 0) & 
+                  (24x"ffffff" when rdata(7) = '1' else
+                   24x"000000"));
+               when "01" => NULL;
+               when "10" => NULL;
+               when "11" => NULL;
+               when others => NULL;
+            end case;
+         when SH => NULL;
+         when SW => NULL;
+         when SBU => NULL;
+         when SHU => NULL;
+         when others => NULL;
+      end case;
+   end process p_select_rd;
 
 end architecture rtl;
