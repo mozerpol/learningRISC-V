87b325883285b079c4128a6490e47c4bc647f173
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-24 23:04:16 +0200

    Add processes to design

--- implementation/VHDL_rysyCore/core/select_wr/select_wr_design.vhd
+++ implementation/VHDL_rysyCore/core/select_wr/select_wr_design.vhd
@@ -25,6 +25,34 @@ begin
 
    p_select_wr : process(all)
    begin
+      case (sel_type) is
+         when SB => 
+            case (sel_addr) is
+               when "00" => wdata <= 24x"000000" & rs2_d(7 downto 0);
+               when "01" => wdata <= 16x"0000" & rs2_d(7 downto 0) & 8x"00";
+               when "10" => wdata <= 8x"00" & rs2_d(7 downto 0) & 16x"0000";
+               when "11" => wdata <= rs2_d(7 downto 0) & 24x"000000";
+               when others => wdata <= 24x"000000" & rs2_d(7 downto 0);
+            end case;
+         when SH =>
+            case (sel_addr) is
+               when "00" => wdata <= 16x"0000" & rs2_d(15 downto 0);
+               when "01" => wdata <= rs2_d(15 downto 0) & 16x"0000";
+               when others => wdata <= 16x"0000" & rs2_d(15 downto 0);
+            end case;
+         when SW => wdata <= rs2_d;
+         when others => wdata <= rs2_d;
+      end case;
    end process p_select_wr;
+
+   p_be : process(all)
+   begin
+      case (sel_type) is
+         when SB => be <= "0001" sll to_integer(unsigned(sel_addr));
+         when SH => be <= "0011" sll to_integer(unsigned(sel_addr));
+         when SW => be <= 4b"1111";
+         when others => be <= 4b"1111";
+      end case;
+   end process p_be;
 
 end architecture rtl;
