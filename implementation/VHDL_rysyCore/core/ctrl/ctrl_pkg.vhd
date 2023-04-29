9c4e746f6b57ae61424f9c9482fc97cca5542e33
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-29 15:34:26 +0200

    Remove all constants, all are in opcodes.vhd

--- implementation/VHDL_rysyCore/core/ctrl/ctrl_pkg.vhd
+++ implementation/VHDL_rysyCore/core/ctrl/ctrl_pkg.vhd
@@ -5,30 +5,6 @@ library ieee;
   
  package ctrl_pkg is
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
-    
-   constant OP		               : std_logic_vector(4 downto 0) := "01100";
-   constant STORE	               : std_logic_vector(4 downto 0) := "01000";
-   constant JALR	               : std_logic_vector(4 downto 0) := "11001";
-   constant	OP_IMM	            : std_logic_vector(4 downto 0) := "00100";
-   constant LUI		            : std_logic_vector(4 downto 0) := "01101";
-   constant JAL		            : std_logic_vector(4 downto 0) := "11011";
-   constant BRANCH	            : std_logic_vector(4 downto 0) := "11000";
-   constant LOAD                 : std_logic_vector(4 downto 0) := "00000";
-   constant FUNC3_ADD_SUB	      : std_logic_vector(2 downto 0) := "000";
-   constant FUNC3_SLT		      : std_logic_vector(2 downto 0) := "010";
-   constant FUNC3_XOR		      : std_logic_vector(2 downto 0) := "100";
-   constant FUNC3_SLL		      : std_logic_vector(2 downto 0) := "001";
-   constant FUNC3_SR		         : std_logic_vector(2 downto 0) := "101";
-   constant FUNC3_BRANCH_BEQ	   : std_logic_vector(2 downto 0) := "000";
-   constant FUNC3_BRANCH_BGE 	   : std_logic_vector(2 downto 0) := "101";
-   constant FUNC3_BRANCH_BLTU	   : std_logic_vector(2 downto 0) := "110";
-   constant FUNC3_SH		         : std_logic_vector(2 downto 0) := "001";
-   constant FUNC3_SBU		      : std_logic_vector(2 downto 0) := "100";
-   constant FUNC3_SHU		      : std_logic_vector(2 downto 0) := "101";
-   constant FUNC7_SR_SRL		   : std_logic_vector(6 downto 0) := "0000000";
-   constant FUNC7_SR_SRA		   : std_logic_vector(6 downto 0) := "0100000";
-   constant FUNC7_ADD_SUB_SUB	   : std_logic_vector(6 downto 0) := "0100000";
-   constant FUNC7_ADD_SUB_ADD	   : std_logic_vector(6 downto 0) := "0000000";
 
  end;
  
