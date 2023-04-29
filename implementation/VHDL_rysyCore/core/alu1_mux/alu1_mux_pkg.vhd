1789adeb0875f2e47bef7b7023512556b63a78a0
mozerpol <krzmozer@student.pg.edu.pl>
2023-04-29 13:53:54 +0200

    Add constants

--- implementation/VHDL_rysyCore/core/alu1_mux/alu1_mux_pkg.vhd
+++ implementation/VHDL_rysyCore/core/alu1_mux/alu1_mux_pkg.vhd
@@ -5,6 +5,8 @@ library ieee;
   
  package alu1_mux_pkg is
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
+    constant ALU1_RS   : std_logic := '0';
+    constant ALU1_PC   : std_logic := '1';
  end;
  
  package body alu1_mux_pkg is
