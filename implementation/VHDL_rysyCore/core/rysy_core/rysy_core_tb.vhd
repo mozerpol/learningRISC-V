ae8b2579de9708962d1485bd4565f443e962edb2
mozerpol <krzmozer@student.pg.edu.pl>
2023-05-02 10:50:08 +0200

    Add tests

--- implementation/VHDL_rysyCore/core/rysy_core/rysy_core_tb.vhd
+++ implementation/VHDL_rysyCore/core/rysy_core/rysy_core_tb.vhd
@@ -46,6 +46,97 @@ begin
 
    p_tb : process
    begin
+
+      wait for 10 ns;
+      -- ....:::::rst_tb = 1;:::::....
+      -- next_nop	   in	ctrl		   =	1
+      -- load_phase	in	ctrl		   =	0
+      -- inst_sel	   in	ctrl		   = 	01
+      -- inst		   in	inst_mgmt	=	32'b10011
+      -- pc_reg	   in	mem_addr_sel=	0
+      rst_tb <= '1';
+      wait for 20 ns;
+      -- ....:::::rst_tb = 0;:::::....
+      -- next_nop	   in	ctrl		   =	0
+      -- load_phase	in	ctrl		   =	0
+      -- inst_sel	   in	ctrl		   = 	10
+      -- inst		   in	inst_mgmt	=	32'b10011
+      -- pc_reg		in	mem_addr_sel=	100 (4 in dec, next clk will is 8...)
+      rst_tb <= '0';
+      wait for 20 ns;
+      -- ....:::::rdata_tb = 32'b000000000101_00001_000_00010_0010011;:::::....
+      -- This instruction means: addi x2, x1, 0x5
+      -- inst 		in	inst_mgmt		=	rdata
+      -- inst_sel	in	inst_mgmt		=	10 (INST_MEM)
+      -- inst_sel	in	ctrl			   =	10 (INST_MEM)
+      -- opcode	in	decode			=	00100 (4 in dec) (OP_IMM in opcodes)
+      -- func3		in	decode			=	000
+      -- rd			in	decode			=	00010
+      -- rs1		in	decode			=	1
+      -- rs2		in decode			=	101
+      -- imm_type	in	ctrl/imm_mux	=	00100 (4 in dec)
+      -- imm		in	imm_mux			=	101
+      -- reg_wr	in	ctrl/reg_file	=	1
+      rdata_tb <= 32b"000000000101_00001_000_00010_0010011";
+      wait for 20 ns;
+      rst_tb <= '1';
+      wait for 20 ns;
+      rst_tb <= '0';
+      wait for 20 ns;
+      rdata_tb <= 32b"10101010101_00000_111_00001_0010011"; -- andi x1, x0, 0b010101010101
+      wait for 20 ns;
+      rdata_tb <= 32b"000000000101_00001_000_00010_0010011"; -- addi x2, x1, 0x5
+      wait for 20 ns;
+      rdata_tb <= 32b"0000000_00010_00001_000_00011_0110011"; -- ADD x3, x1, x2
+      wait for 20 ns;
+      rdata_tb <= 32b"0000000000100_00001_010_00010_0010011"; -- slti x2, x1, 4
+      wait for 20 ns;
+      rdata_tb <= 32b"100000000000_00001_011_00010_0010011"; -- sltiu x2, x1, -2048
+      wait for 20 ns;
+      rst_tb <= '1';
+      wait for 20 ns;
+      rst_tb <= '0';
+      wait for 20 ns; 
+      rdata_tb <= 32b"0000000_00010_00001_000_00011_0110011"; -- ADD x3, x1, x2
+      wait for 20 ns; 
+      rdata_tb <= 32b"11111111111111111111_00101_0010111"; -- auipc x5, 0xfffff
+      wait for 20 ns;
+      rdata_tb <= 32b"1111111_00001_00010_001_11001_1100011"; -- bne x2, x1, loop - loop is label
+      wait for 20 ns;
+      rdata_tb <= 32b"11111111100111111111_00100_1101111"; -- jal x4, loop
+      wait for 20 ns;
+      rst_tb <= '1';
+      wait for 20 ns;
+      rst_tb <= '0';
+      wait for 20 ns;
+      rdata_tb <= 32b"11111111111111111111_00001_0110111"; -- lui x1, 0xFFFFF
+      wait for 20 ns;
+      rdata_tb <= 32b"010101010101_00000_110_00001_0010011"; -- ori x1, x0, 0b10101010101
+      wait for 20 ns;
+      rdata_tb <= 32b"0000000_00010_00001_000_00011_0110011"; -- ADD x3, x1, x2
+      wait for 20 ns;
+      rdata_tb <= 32b"0000000000100_00001_010_00010_0010011"; -- slti x2, x1, 4
+      wait for 20 ns;
+      rdata_tb <= 32b"100000000000_00001_011_00010_0010011"; -- sltiu x2, x1, -2048
+      wait for 20 ns;
+      rdata_tb <= 32b"000000000101_00001_000_00010_0010011"; -- addi x2, x1, 0x5
+      wait for 20 ns;
+      rdata_tb <= 32b"010101010101_00000_111_00001_0010011"; -- andi x1, x0, 0b010101010101
+      wait for 20 ns;
+      rdata_tb <= 32b"000000000101_00001_000_00010_0010011"; -- addi x2, x1, 0x5
+      wait for 20 ns;
+      rdata_tb <= 32b"010101010101_00000_111_00001_0010011"; -- andi x1, x0, 0b010101010101
+      wait for 20 ns;
+      rdata_tb <= 32b"000000000101_00001_000_00010_0010011"; -- addi x2, x1, 0x5
+      wait for 20 ns;
+      rdata_tb <= 32b"010101010101_00000_111_00001_0010011"; -- andi x1, x0, 0b010101010101
+      wait for 20 ns;
+      rdata_tb <= 32b"000000000101_00001_000_00010_0010011"; -- addi x2, x1, 0x5
+      wait for 20 ns; 
+      rdata_tb <= 32b"11111111111111111111_00101_0010111"; -- auipc x5, 0xfffff
+      wait for 20 ns;
+      rdata_tb <= 32b"1111111_00001_00010_001_11001_1100011"; -- bne x2, x1, loop - loop is label
+
       wait for 25 ns;
       stop(2); 
    end process;
