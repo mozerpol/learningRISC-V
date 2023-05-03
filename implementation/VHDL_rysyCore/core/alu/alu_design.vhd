b133040cc2d3ed9b51591c33303952e75b1eccda
mozerpol <krzmozer@student.pg.edu.pl>
2023-05-03 12:55:13 +0200

    Change: o => alu_out

--- implementation/VHDL_rysyCore/core/alu/alu_design.vhd
+++ implementation/VHDL_rysyCore/core/alu/alu_design.vhd
@@ -19,29 +19,25 @@ end entity alu;
 
 architecture rtl of alu is
 
-   signal o          : std_logic_vector(REG_LEN-1 downto 0);
-
 begin
-
-   alu_out     <= o;
 
    p_alu : process(all)
    begin
       case alu_op is
-         when ADD    => o <= alu_in1 + alu_in2;
-         when "0001" => o <= alu_in1 - alu_in2; -- SUB
-         when "0010" => o <= alu_in1 xor alu_in2; -- XOR
-         when "0011" => o <= alu_in1 or alu_in2; -- OR
-         when "0100" => o <= alu_in1 and alu_in2; -- AND
-         when "0101" => o <= std_logic_vector(unsigned(alu_in1) sll 
-            to_integer(unsigned(alu_in2))); -- SLL
-         when "0110" => o <= std_logic_vector(unsigned(alu_in1) srl 
-            to_integer(unsigned(alu_in2))); -- SRL
-         when "0111" => o <= std_logic_vector(unsigned(alu_in1) sra
-            to_integer(unsigned(alu_in2))); -- SRA
-         when SLT    => o <= alu_in1 when alu_in1 > alu_in2 else alu_in2;
-         when SLTU   => o <= alu_in1 when alu_in1 > alu_in2 else alu_in2; 
-         when others => o <= (others => '0');
+         when ADD    => alu_out <= alu_in1 + alu_in2;
+         when "0001" => alu_out <= alu_in1 - alu_in2;   -- SUB
+         when "0010" => alu_out <= alu_in1 xor alu_in2; -- XOR
+         when "0011" => alu_out <= alu_in1 or alu_in2;  -- OR
+         when "0100" => alu_out <= alu_in1 and alu_in2; -- AND
+         when "0101" => alu_out <= std_logic_vector(unsigned(alu_in1) sll 
+            to_integer(unsigned(alu_in2)));             -- SLL
+         when "0110" => alu_out <= std_logic_vector(unsigned(alu_in1) srl 
+            to_integer(unsigned(alu_in2)));             -- SRL
+         when "0111" => alu_out <= std_logic_vector(unsigned(alu_in1) sra
+            to_integer(unsigned(alu_in2)));             -- SRA
+         when SLT    => alu_out <= alu_in1 when alu_in1 > alu_in2 else alu_in2;
+         when SLTU   => alu_out <= alu_in1 when alu_in1 > alu_in2 else alu_in2; 
+         when others => alu_out <= (others => '0');
       end case;
    end process p_alu;
 
