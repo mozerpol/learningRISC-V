f387f3d7a92c1f657257d93949c6c1ba4d8b7be0
mozerpol <krzmozer@student.pg.edu.pl>
2023-05-12 14:37:44 +0200

    Change rd type, array length

--- implementation/VHDL_rysyCore/core/reg_file/reg_file_design.vhd
+++ implementation/VHDL_rysyCore/core/reg_file/reg_file_design.vhd
@@ -24,20 +24,20 @@ end entity reg_file_design;
 
 architecture rtl of reg_file_design is
 
-   type t_x  is array(REG_LEN downto 0) of std_logic_vector(REG_LEN-1 downto 0);
+   type t_x  is array(0 to 31) of std_logic_vector(REG_LEN-1 downto 0);
    signal x : t_x;
 
 begin
 
 
-   rs1_d <= 32x"00000000" when rs1 = 5b"00000" else x(to_integer(unsigned(rs1)));
-   rs2_d <= 32x"00000000" when rs2 = 5b"00000" else x(to_integer(unsigned(rs2)));
+   rs1_d <= (others => '0') when rs1 = 5b"00000" else x(to_integer(unsigned(rs1)));
+   rs2_d <= (others => '0') when rs2 = 5b"00000" else x(to_integer(unsigned(rs2)));
 
    p_reg : process(clk)
    begin
       if (clk'event and clk = '1') then
          if(reg_wr = '1') then
-            x(to_integer(signed(rd))) <= rd_d;
+            x(to_integer(unsigned(rd))) <= rd_d;
          end if;
       end if;
    end process p_reg;
