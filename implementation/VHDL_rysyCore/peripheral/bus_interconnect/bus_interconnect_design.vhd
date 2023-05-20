7e3490af128e7a9deb2e274ed10a250d05e9677a
mozerpol <krzmozer@student.pg.edu.pl>
2023-05-20 11:24:05 +0200

    Add bus_interconnect with project draft

---
+++ implementation/VHDL_rysyCore/peripheral/bus_interconnect/bus_interconnect_design.vhd
@@ -0,0 +1,16 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.std_logic_unsigned.all;
+
+
+entity bus_interconnect_design is
+   port (
+   );
+end entity bus_interconnect_design;
+
+architecture rtl of bus_interconnect_design is
+
+begin
+
+
+end architecture rtl;
---
+++ implementation/VHDL_rysyCore/peripheral/bus_interconnect/bus_interconnect_tb.vhd
@@ -0,0 +1,37 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
+   use ieee.std_logic_unsigned.all;
+library std;
+  use std.env.all;
+
+entity bus_interconnect_tb is
+end bus_interconnect_tb;
+
+architecture tb of bus_interconnect_tb is
+
+   component bus_interconnect_design is
+   port (
+   );
+   end component bus_interconnect_design;
+
+begin
+   inst_bus_interconnect : component bus_interconnect_design 
+   port map (
+   );
+
+   p_tb : process
+   begin
+      wait for 25 ns;
+      stop(2); 
+   end process p_tb;
+
+   p_clock_gen : process
+   begin
+      clk_tb <= '0';
+      wait for 5 ns;
+      clk_tb <= '1';
+      wait for 5 ns;
+   end process p_clock_gen;
+
+end architecture tb;
