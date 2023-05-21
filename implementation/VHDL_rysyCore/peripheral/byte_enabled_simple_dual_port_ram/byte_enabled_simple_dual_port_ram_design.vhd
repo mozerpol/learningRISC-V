a6236a6e08a89511b2a960038fd208f4adcbfd0a
mozerpol <krzmozer@student.pg.edu.pl>
2023-05-21 19:53:11 +0200

    Init RAM with data

--- implementation/VHDL_rysyCore/peripheral/byte_enabled_simple_dual_port_ram/byte_enabled_simple_dual_port_ram_design.vhd
+++ implementation/VHDL_rysyCore/peripheral/byte_enabled_simple_dual_port_ram/byte_enabled_simple_dual_port_ram_design.vhd
@@ -1,6 +1,7 @@
 library ieee;
    use ieee.std_logic_1164.all;
    use ieee.std_logic_unsigned.all;
+   use std.textio.all;
 library byte_enabled_simple_dual_port_ram_lib;
    use byte_enabled_simple_dual_port_ram_lib.all;
    use byte_enabled_simple_dual_port_ram_lib.byte_enabled_simple_dual_port_ram_pkg.all;
@@ -8,6 +9,7 @@ library byte_enabled_simple_dual_port_ram_lib;
 
 entity byte_enabled_simple_dual_port_ram is
    generic (
+      CODE        : string  := "ram_content_hex.txt";
       ADDR_WIDTH  : integer := 8;
       BYTE_WIDTH  : integer := 8;
       BYTES       : integer := 4;
@@ -25,6 +27,24 @@ entity byte_enabled_simple_dual_port_ram is
 end entity byte_enabled_simple_dual_port_ram;
 
 architecture rtl of byte_enabled_simple_dual_port_ram is
+
+   constant WORDS : integer := 256; -- Number of rows
+   type t_array is array (WORDS-1 downto 0) of 
+      std_logic_vector((ADDR_WIDTH*BYTES)-1 downto 0);
+
+   impure function init_ram_hex return t_array is
+      file text_file       : text open read_mode is CODE;
+      variable text_line   : line;
+      variable ram_content : t_array;
+   begin
+      for i in 0 to 7 loop  ---------------------- UP TO NUMBER OF LINES !!!!!
+         readline(text_file, text_line);
+         hread(text_line, ram_content(i));
+      end loop;
+      return ram_content;
+   end function;
+
+   signal ram : t_array := init_ram_hex;
 
 begin
 
