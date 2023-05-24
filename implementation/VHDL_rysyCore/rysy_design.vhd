4ff8f717ad7370d63a2d0b91546d304ae7101734
mozerpol <krzmozer@student.pg.edu.pl>
2023-05-24 18:59:13 +0200

    Change name: rysy => rysy_design, add design draft

---
+++ implementation/VHDL_rysyCore/rysy_design.vhd
@@ -0,0 +1,22 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
+   use ieee.std_logic_unsigned.all;
+   
+entity rysy is
+   port (
+      clk      : in std_logic
+   );
+end entity rysy;
+
+architecture rtl of rysy is
+
+
+begin
+
+   p_rysy : process(clk)
+   begin
+   end process p_rysy;
+
+
+end architecture rtl;
