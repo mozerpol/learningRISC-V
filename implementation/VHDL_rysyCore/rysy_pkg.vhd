3dd7d2a02d9e0377d39e7532aee8eea724546e01
mozerpol <krzmozer@student.pg.edu.pl>
2023-05-24 19:00:07 +0200

    Add empty rysy package

---
+++ implementation/VHDL_rysyCore/rysy_pkg.vhd
@@ -0,0 +1,12 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
+   use ieee.std_logic_unsigned.all;
+   
+package rysy_pkg is
+   -- constant C_NAME : std_logic_vector(N downto M) := "X";
+end;
+ 
+package body rysy_pkg is
+ 
+end package body;
