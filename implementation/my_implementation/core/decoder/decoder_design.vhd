ee3cc4cb9069d949138d98533e3721a0c4b1979b
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-09 19:07:25 +0200

    Add decoder part with all necessary files

---
+++ implementation/my_implementation/core/decoder/decoder_design.vhd
@@ -0,0 +1,22 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.std_logic_unsigned.all;
+   use ieee.numeric_std.all;
+library decoder_lib;
+   use decoder_lib.all;
+   use decoder_lib.decoder_pkg.all;
+
+entity decoder is
+   port (
+   );
+end entity decoder;
+
+architecture rtl of decoder is
+
+begin
+
+   p_decoder : process(all)
+   begin
+   end process p_decoder;
+
+end architecture rtl;
---
+++ implementation/my_implementation/core/decoder/decoder_tb.vhd
@@ -0,0 +1,31 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+   use ieee.numeric_std.all;
+   use ieee.std_logic_unsigned.all;
+library std;
+  use std.env.all;
+
+entity decoder_tb is
+end decoder_tb;
+
+architecture tb of decoder_tb is
+
+   component decoder is
+   port (
+   );
+   end component decoder;
+
+begin
+
+   inst_decoder : component decoder 
+   port map (
+   );
+
+   p_tb : process
+   begin
+       
+      wait for 25 ns;
+      stop(2); 
+   end process p_tb;
+
+end architecture tb;
