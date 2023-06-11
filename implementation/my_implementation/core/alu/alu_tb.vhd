6ea166e01b0ddc7b17f9f58a6cf07419f9c37af6
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-11 23:54:44 +0200

    Add tests

--- implementation/my_implementation/core/alu/alu_tb.vhd
+++ implementation/my_implementation/core/alu/alu_tb.vhd
@@ -39,7 +39,28 @@ begin
 
    p_tb : process
    begin
-       
+
+      alu_operand_1_tb  <= (others => '0');
+      alu_operand_2_tb  <= (others => '0');
+      control_tb        <= (others => '0');
+      rst_tb            <= '1';
+      wait for 20 ns;
+      rst_tb            <= '0';
+      -- ADD instruction
+      control_tb        <= "00000";
+      alu_operand_1_tb  <= 32d"2";
+      alu_operand_2_tb  <= 32d"11";
+      wait for 20 ns;
+      alu_operand_1_tb  <= 32d"4";
+      alu_operand_2_tb  <= 32d"4";
+      wait for 20 ns;
+      alu_operand_1_tb  <= 32d"10";
+      alu_operand_2_tb  <= 32x"FFFFFFFC"; -- -4
+      wait for 20 ns;
+      alu_operand_1_tb  <= 32d"20";
+      alu_operand_2_tb  <= 32x"FFFFFFF0"; -- -16
+      wait for 20 ns;
+
       wait for 25 ns;
       stop(2); 
    end process p_tb;
