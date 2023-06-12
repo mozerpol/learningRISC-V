4b465c20811aba8f6022b29c64866e90243f3f5c
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-13 00:30:05 +0200

    Add design behaviour

--- implementation/my_implementation/core/alu_mux_2/alu_mux_2_design.vhd
+++ implementation/my_implementation/core/alu_mux_2/alu_mux_2_design.vhd
@@ -8,6 +8,11 @@ library alu_mux_2_lib;
 
 entity alu_mux_2 is
    port (
+      i_rst             : in std_logic;
+      i_alu_mux_2_ctrl  : in std_logic;
+      i_rs2_data        : in std_logic_vector(31 downto 0);    -- From reg_file
+      i_imm             : in std_logic_vector(31 downto 0);
+      o_alu_operand_2   : out std_logic_vector(31 downto 0)
    );
 end entity alu_mux_2;
 
@@ -17,6 +22,15 @@ begin
 
    p_alu_mux_2 : process(all)
    begin
+      if (i_rst = '1') then
+         o_alu_operand_2 <= (others => '0');
+      else
+         case i_alu_mux_2_ctrl is
+            when '0'    => o_alu_operand_2 <= i_rs2_data;
+            when '1'    => o_alu_operand_2 <= i_imm;
+            when others => o_alu_operand_2 <= (others => '0');
+         end case;
+      end if;
    end process p_alu_mux_2;
 
 end architecture rtl;
