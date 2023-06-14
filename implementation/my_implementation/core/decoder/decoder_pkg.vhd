0c0466f794ff1e772f0f4a53276111e9f94923dd
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-14 14:35:01 +0200

    Add opcodes to pkg

--- implementation/my_implementation/core/decoder/decoder_pkg.vhd
+++ implementation/my_implementation/core/decoder/decoder_pkg.vhd
@@ -4,10 +4,23 @@ library ieee;
   use IEEE.math_real.all;
   
  package decoder_pkg is
-    -- constant C_NAME : std_logic_vector(N downto M) := "X";
+
+   -- Opcodes
+   constant C_OPCODE_LOAD     : std_logic_vector(4 downto 0) := "00000";
+   constant C_OPCODE_STORE    : std_logic_vector(4 downto 0) := "01000";
+   constant C_OPCODE_MADD     : std_logic_vector(4 downto 0) := "10000";
+   constant C_OPCODE_BRANCH   : std_logic_vector(4 downto 0) := "11000";
+   constant C_OPCODE_JALR     : std_logic_vector(4 downto 0) := "11001";
+   constant C_OPCODE_JAL      : std_logic_vector(4 downto 0) := "11011";
+   constant C_OPCODE_SYSTEM   : std_logic_vector(4 downto 0) := "11100";
+   constant C_OPCODE_OP       : std_logic_vector(4 downto 0) := "01100";
+   constant C_OPCODE_OPIMM    : std_logic_vector(4 downto 0) := "00100";
+   constant C_OPCODE_MISCMEM  : std_logic_vector(4 downto 0) := "00011";
+   constant C_OPCODE_AUIPC    : std_logic_vector(4 downto 0) := "00101";
+   constant C_OPCODE_LUI      : std_logic_vector(4 downto 0) := "01101";
 
  end;
- 
+ -- https://github.com/Domipheus/RPU
  package body decoder_pkg is
  
  end package body;
