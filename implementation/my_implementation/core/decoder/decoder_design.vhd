c652df9acf4a3e18905d45794317f075d4efbd55
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-14 22:12:16 +0200

    Add opcodes to case

--- implementation/my_implementation/core/decoder/decoder_design.vhd
+++ implementation/my_implementation/core/decoder/decoder_design.vhd
@@ -8,6 +8,15 @@ library decoder_lib;
 
 entity decoder is
    port (
+      i_rst          : in std_logic;
+      i_instruction  : in std_logic_vector(31 downto 0);
+      o_rd_data      : out std_logic_vector(4 downto 0);
+      o_rs1_data     : out std_logic_vector(4 downto 0);
+      o_rs2_data     : out std_logic_vector(4 downto 0);
+      o_imm          : out std_logic_vector(31 downto 0);
+      o_opcode       : out std_logic_vector(4 downto 0);
+      o_func3        : out std_logic_vector(2 downto 0);
+      o_func7        : out std_logic_vector(6 downto 0)
    );
 end entity decoder;
 
@@ -17,6 +26,24 @@ begin
 
    p_decoder : process(all)
    begin
+      if (i_rst) then
+      else
+         case i_instruction(6 downto 0) is
+            -- R type
+            when "0110011" =>
+            -- I type
+            when "0010011" || "0000011" =>
+            -- S type
+            when "0100011" =>
+            -- SB type
+            when "1100011" =>
+            -- U type
+            when "0010111" =>
+            -- UJ type
+            when "1101111" =>
+            when others => NULL;
+         end case;
+      end if;
    end process p_decoder;
 
 end architecture rtl;
