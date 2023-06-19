5b1670fa5885927ead0b2b3ba43129288b31f428
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-19 17:01:30 +0200

    Change opcodes order, add comments

--- implementation/my_implementation/core/decoder/decoder_pkg.vhd
+++ implementation/my_implementation/core/decoder/decoder_pkg.vhd
@@ -5,22 +5,30 @@ library ieee;
   
  package decoder_pkg is
 
-   -- Opcodes
-   constant C_OPCODE_LOAD     : std_logic_vector(4 downto 0) := "00000";
-   constant C_OPCODE_STORE    : std_logic_vector(4 downto 0) := "01000";
-   constant C_OPCODE_MADD     : std_logic_vector(4 downto 0) := "10000";
-   constant C_OPCODE_BRANCH   : std_logic_vector(4 downto 0) := "11000";
-   constant C_OPCODE_JALR     : std_logic_vector(4 downto 0) := "11001";
-   constant C_OPCODE_JAL      : std_logic_vector(4 downto 0) := "11011";
-   constant C_OPCODE_SYSTEM   : std_logic_vector(4 downto 0) := "11100";
-   constant C_OPCODE_OP       : std_logic_vector(4 downto 0) := "01100";
-   constant C_OPCODE_OPIMM    : std_logic_vector(4 downto 0) := "00100";
-   constant C_OPCODE_MISCMEM  : std_logic_vector(4 downto 0) := "00011";
-   constant C_OPCODE_AUIPC    : std_logic_vector(4 downto 0) := "00101";
+   -- Opcodes, all are in documentation in table on page 104
+   ---- U-type ----
    constant C_OPCODE_LUI      : std_logic_vector(4 downto 0) := "01101";
+   constant C_OPCODE_AUIPC    : std_logic_vector(4 downto 0) := "00101";
+   ---- J-type ----
+   constant C_OPCODE_JAL      : std_logic_vector(4 downto 0) := "11011";
+   ---- I-type ----
+   constant C_OPCODE_JALR     : std_logic_vector(4 downto 0) := "11001";
+   -- C_OPCODE_LOAD = LB, LH, LW, LBU, LHU, LD, LWU
+   constant C_OPCODE_LOAD     : std_logic_vector(4 downto 0) := "00000";
+   -- OP_IMM = ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI, SLLI, SRLI,
+   -- SRAI
+   constant C_OPCODE_OPIMM    : std_logic_vector(4 downto 0) := "00100";
+   ---- B-type ----
+   -- C_OPCODE_BRANCH = BEQ, BNE, BLT, BGE, BLTU, BGEU
+   constant C_OPCODE_BRANCH   : std_logic_vector(4 downto 0) := "11000";
+   ---- S-type ----
+   -- C_OPCODE_STORE = SB, SH, SW, SD
+   constant C_OPCODE_STORE    : std_logic_vector(4 downto 0) := "01000";
+   ---- R-type ----
+   -- C_OPCODE_OP = ADD, SUB, SLL, SLT, SLTU, XOR, SRL, SRA, OR, AND
+   constant C_OPCODE_OP       : std_logic_vector(4 downto 0) := "01100";
 
  end;
- -- https://github.com/Domipheus/RPU
  package body decoder_pkg is
  
  end package body;
