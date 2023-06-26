1b1b1e1928fbae879d08d6dd3acdd3d5d95bddbe
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-26 13:54:34 +0200

    Add ports and bind them

--- implementation/my_implementation/core/control/control_design.vhd
+++ implementation/my_implementation/core/control/control_design.vhd
@@ -8,6 +8,16 @@ library control_lib;
 
 entity control is
    port (
+      i_rst             : in std_logic;
+      i_opcode          : out std_logic_vector(6 downto 0);
+      i_func3           : out std_logic_vector(2 downto 0);
+      i_func7           : out std_logic_vector(6 downto 0);
+      o_rd_data         : out std_logic_vector(4 downto 0);
+      o_rs1_data        : out std_logic_vector(4 downto 0);
+      o_rs2_data        : out std_logic_vector(4 downto 0);
+      o_imm             : out std_logic_vector(31 downto 0);
+      o_alu_mux_1_ctrl  : out std_logic;
+      o_alu_mux_2_ctrl  : out std_logic
    );
 end entity control;
 
@@ -17,6 +27,9 @@ begin
 
    p_control : process(all)
    begin
+      if (i_rst = '1') then
+      else
+      end if;
    end process p_control;
 
 end architecture rtl;
--- implementation/my_implementation/core/control/control_tb.vhd
+++ implementation/my_implementation/core/control/control_tb.vhd
@@ -12,20 +12,50 @@ architecture tb of control_tb is
 
    component control is
    port (
+      i_rst             : in std_logic;
+      i_opcode          : out std_logic_vector(6 downto 0);
+      i_func3           : out std_logic_vector(2 downto 0);
+      i_func7           : out std_logic_vector(6 downto 0);
+      o_rd_data         : out std_logic_vector(4 downto 0);
+      o_rs1_data        : out std_logic_vector(4 downto 0);
+      o_rs2_data        : out std_logic_vector(4 downto 0);
+      o_imm             : out std_logic_vector(31 downto 0);
+      o_alu_mux_1_ctrl  : out std_logic;
+      o_alu_mux_2_ctrl  : out std_logic
    );
    end component control;
 
+   signal rst_tb             : std_logic;
+   signal opcode_tb          : std_logic_vector(6 downto 0);
+   signal func3_tb           : std_logic_vector(2 downto 0);
+   signal func7_tb           : std_logic_vector(6 downto 0);
+   signal rd_data_tb         : std_logic_vector(4 downto 0);
+   signal rs1_data_tb        : std_logic_vector(4 downto 0);
+   signal rs2_data_tb        : std_logic_vector(4 downto 0);
+   signal imm_tb             : std_logic_vector(31 downto 0);
+   signal alu_mux_1_ctrl_tb  : std_logic;
+   signal alu_mux_2_ctrl_tb  : std_logic;
 begin
 
-   inst_control : component control 
+   inst_control : component control
    port map (
+      i_rst             => rst_tb,
+      i_opcode          => opcode_tb,
+      i_func3           => func3_tb,
+      i_func7           => func7_tb,
+      o_rd_data         => rd_data_tb,
+      o_rs1_data        => rs1_data_tb,
+      o_rs2_data        => rs2_data_tb,
+      o_imm             => imm_tb,
+      o_alu_mux_1_ctrl  => alu_mux_1_ctrl_tb,
+      o_alu_mux_2_ctrl  => alu_mux_2_ctrl_tb
    );
 
    p_tb : process
    begin
-       
+
       wait for 25 ns;
-      stop(2); 
+      stop(2);
    end process p_tb;
 
 end architecture tb;
