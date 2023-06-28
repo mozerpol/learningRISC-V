e18317222c36f12c8bf70d5c239a3f6e79e3e229
mozerpol <krzmozer@student.pg.edu.pl>
2023-06-28 12:17:23 +0200

    Add a core for opcode, finc3 and func7

--- implementation/my_implementation/core/control/control_design.vhd
+++ implementation/my_implementation/core/control/control_design.vhd
@@ -35,6 +35,33 @@ begin
          o_alu_mux_1_ctrl  <= '0';
          o_alu_mux_2_ctrl  <= '0';
       else
+         case i_opcode is
+            when C_OPCODE_OP =>
+               case i_func3 is
+                  when C_FUNC3_ADD_SUB => 
+                     if (i_func7 = C_FUNC7_SUB) then
+                     else
+                     end if;
+                  when C_FUNC3_SLL     =>
+                  when C_FUNC3_SLT     =>
+                  when C_FUNC3_SLTU    =>
+                  when C_FUNC3_XOR     =>
+                  when C_FUNC3_SRL_SRA =>
+                     if (i_func7 = C_FUNC7_SRA) then
+                     else
+                     end if;
+                  when C_FUNC3_OR      =>
+                  when C_FUNC3_AND     =>
+                  when others => NULL;
+               end case;
+            when others => 
+               o_rd_data         <= (others => '0');
+               o_rs1_data        <= (others => '0');
+               o_rs2_data        <= (others => '0');
+               o_imm             <= (others => '0');
+               o_alu_mux_1_ctrl  <= '0';
+               o_alu_mux_2_ctrl  <= '0';
+         end case;
       end if;
    end process p_control;
 
