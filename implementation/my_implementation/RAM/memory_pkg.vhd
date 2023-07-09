1aca12097286b713cf35ace7aeaefd159c5a84a9
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-09 14:15:52 +0200

    Add t_ram and add code as a const

--- implementation/my_implementation/RAM/memory_pkg.vhd
+++ implementation/my_implementation/RAM/memory_pkg.vhd
@@ -4,8 +4,21 @@ library ieee;
   use IEEE.math_real.all;
   
  package memory_pkg is
+ 
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
+   type t_ram is array (0 to 7) of std_logic_vector(31 downto 0);
 
+   constant C_CODE : t_ram := (
+                              x"123450b7",
+                              x"67808093",
+                              x"fff0f113",
+                              x"00f0e193",
+                              x"fff02213",
+                              x"fff03293",
+                              x"002001b3",
+                              x"0000006f"
+                           );
+                               
  end;
  
  package body memory_pkg is
