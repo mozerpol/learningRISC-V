2edade1e204339bd3b4540ed521867df66a1daae
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-10 15:15:58 +0200

    Add wirting and reading from memory

--- implementation/my_implementation/RAM/memory_design.vhd
+++ implementation/my_implementation/RAM/memory_design.vhd
@@ -28,6 +28,19 @@ begin
    begin
       if (i_rst = '1') then
          ram   <= C_CODE;
+         o_q   <= (others => '0');
+      elsif (i_clk'event and i_clk = '1') then
+         if (i_wr_enable = '1') then
+            ram(to_integer(unsigned(i_wr_addr)))(7 downto 0)
+               <= i_wr_data(7 downto 0);
+            ram(to_integer(unsigned(i_wr_addr)))(15 downto 8)
+               <= i_wr_data(15 downto 8);
+            ram(to_integer(unsigned(i_wr_addr)))(23 downto 16)
+               <= i_wr_data(23 downto 16);
+            ram(to_integer(unsigned(i_wr_addr)))(31 downto 24)
+               <= i_wr_data(31 downto 24);
+         end if;
+         o_q <= ram(to_integer(unsigned(i_rd_addr)));
       end if;
    end process p_memory;
 
