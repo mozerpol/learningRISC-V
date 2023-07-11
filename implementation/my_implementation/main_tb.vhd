b9b316b90a58c7b890ef7f0c0d79e57e54b91bc6
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-11 15:32:58 +0200

    Add component and bind it

--- implementation/my_implementation/main_tb.vhd
+++ implementation/my_implementation/main_tb.vhd
@@ -12,17 +12,29 @@ architecture tb of main_tb is
 
    component main is
    port (
-      i_rst             : in std_logic
+      i_rst          : in std_logic;
+      i_clk          : in std_logic;
+      i_wr_data      : in std_logic_vector(31 downto 0);
+      i_wr_enable    : in std_logic;
+      o_rd_data      : out std_logic_vector(31 downto 0)
    );
    end component main;
 
-   signal rst_tb           : std_logic;
+   signal rst_tb        : std_logic;
+   signal clk_tb        : std_logic;
+   signal wr_data_tb    : std_logic_vector(31 downto 0);
+   signal wr_enable_tb  : std_logic;
+   signal rd_data_tb    : std_logic_vector(31 downto 0);
 
 begin
 
-   inst_main : component main 
+   inst_main : component main
    port map (
-      i_rst             => rst_tb
+      i_rst          => rst_tb,
+      i_clk          => clk_tb,
+      i_wr_data      => wr_data_tb,
+      i_wr_enable    => wr_enable_tb,
+      o_rd_data      => rd_data_tb
    );
 
    p_tb : process
@@ -33,7 +45,7 @@ begin
       rst_tb            <= '0';
 
       wait for 25 ns;
-      stop(2); 
+      stop(2);
    end process p_tb;
 
 end architecture tb;
