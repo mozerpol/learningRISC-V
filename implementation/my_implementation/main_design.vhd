e6243e1adeec728e6f11faf4f98cd57a29f96c7c
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-12 20:37:49 +0200

    Add signals

--- implementation/my_implementation/main_design.vhd
+++ implementation/my_implementation/main_design.vhd
@@ -29,7 +29,7 @@ architecture rtl of main is
          i_wr_enable : in std_logic;
          o_q         : out std_logic_vector(31 downto 0)
       );
-   end component memory;   
+   end component memory;
 
    component alu is
       port (
@@ -45,7 +45,7 @@ architecture rtl of main is
       port (
          i_rst             : in std_logic;
          i_alu_mux_1_ctrl  : in std_logic;
-         i_rs1_data        : in std_logic_vector(31 downto 0);    -- From reg_file
+         i_rs1_data        : in std_logic_vector(31 downto 0); -- From reg_file
          i_pc_addr         : in std_logic_vector(31 downto 0);
          o_alu_operand_1   : out std_logic_vector(31 downto 0)
       );
@@ -55,7 +55,7 @@ architecture rtl of main is
       port (
          i_rst             : in std_logic;
          i_alu_mux_2_ctrl  : in std_logic;
-         i_rs2_data        : in std_logic_vector(31 downto 0);    -- From reg_file
+         i_rs2_data        : in std_logic_vector(31 downto 0); -- From reg_file
          i_imm             : in std_logic_vector(31 downto 0);
          o_alu_operand_2   : out std_logic_vector(31 downto 0)
       );
@@ -101,6 +101,32 @@ architecture rtl of main is
          o_rs2_data     : out std_logic_vector(31 downto 0)
       );
    end component reg_file;
+
+   signal rst              : std_logic;
+   signal clk              : std_logic;
+   signal rd_addr          : std_logic_vector(7 downto 0);
+   signal wr_addr          : std_logic_vector(7 downto 0);
+   signal wr_data          : std_logic_vector(31 downto 0);
+   signal wr_enable        : std_logic;
+   signal q                : std_logic_vector(31 downto 0);
+   signal alu_operand_1    : std_logic_vector(31 downto 0);
+   signal alu_operand_2    : std_logic_vector(31 downto 0);
+   signal alu_out          : std_logic_vector(31 downto 0);
+   signal alu_mux_1_ctrl   : std_logic;
+   signal rs1_data         : std_logic_vector(31 downto 0);
+   signal pc_addr          : std_logic_vector(31 downto 0);
+   signal alu_mux_2_ctrl   : std_logic;
+   signal rs2_data         : std_logic_vector(31 downto 0);
+   signal imm              : std_logic_vector(31 downto 0);
+   signal opcode           : std_logic_vector(6 downto 0);
+   signal control_alu      : std_logic_vector(5 downto 0);
+   signal instruction      : std_logic_vector(31 downto 0);
+   signal rd_data          : std_logic_vector(4 downto 0);
+   signal func3            : std_logic_vector(2 downto 0);
+   signal func7            : std_logic_vector(6 downto 0);
+   signal rs1_addr         : std_logic_vector(4 downto 0);
+   signal rs2_addr         : std_logic_vector(4 downto 0);
+   signal reg_wr_ctrl      : std_logic;
 
 begin
 
