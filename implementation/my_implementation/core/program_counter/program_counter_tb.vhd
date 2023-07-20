d1407d4f50efa62894f01515c43f7a2f40d0eec9
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-20 17:03:51 +0200

    Add ports and bind them

--- implementation/my_implementation/core/program_counter/program_counter_tb.vhd
+++ implementation/my_implementation/core/program_counter/program_counter_tb.vhd
@@ -12,13 +12,29 @@ architecture tb of program_counter_tb is
 
    component program_counter is
    port (
+      i_rst          : in std_logic;
+      i_clk          : in std_logic;
+      i_alu_result   : in std_logic_vector(31 downto 0);
+      i_pc_ctrl      : in std_logic_vector(1 downto 0);
+      o_pc_addr      : out std_logic_vector(31 downto 0)
    );
    end component program_counter;
+
+   signal rst_tb        : std_logic;
+   signal clk_tb        : std_logic;
+   signal alu_result_tb : std_logic_vector(31 downto 0);
+   signal pc_ctrl_tb    : std_logic_vector(1 downto 0);
+   signal pc_addr_tb    : std_logic_vector(31 downto 0);
 
 begin
 
    inst_program_counter : component program_counter 
    port map (
+      i_rst         => rst_tb,
+      i_clk         => clk_tb,
+      i_alu_result  => alu_result_tb,
+      i_pc_ctrl     => pc_ctrl_tb,
+      o_pc_addr     => pc_addr_tb
    );
 
    p_tb : process
