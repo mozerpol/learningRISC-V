6ccb9898aaa58afde60a054858b0a4d243ba0876
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-20 13:32:05 +0200

    Change port names

--- implementation/my_implementation/core/memory_management/memory_management_design.vhd
+++ implementation/my_implementation/core/memory_management/memory_management_design.vhd
@@ -12,12 +12,12 @@ library alu_lib;
 entity memory_management is
    port (
       i_rst          : in std_logic;
-      i_alu_out      : in std_logic_vector(31 downto 0);
+      i_alu_result   : in std_logic_vector(31 downto 0);
       i_rs2_data     : in std_logic_vector(31 downto 0);
       i_alu_control  : in std_logic_vector(5 downto 0);
-      o_read_addr    : out std_logic_vector(7 downto 0);
-      o_write_addr   : out std_logic_vector(7 downto 0);
-      o_write_data   : out std_logic_vector(31 downto 0)
+      o_ram_read_addr    : out std_logic_vector(7 downto 0);
+      o_ram_write_addr   : out std_logic_vector(7 downto 0);
+      o_ram_write_data   : out std_logic_vector(31 downto 0)
    );
 end entity memory_management;
 
@@ -28,14 +28,14 @@ begin
    p_memory_management : process(all)
    begin
       if (i_rst = '1') then
-         o_read_addr    <= (others => '0');
-         o_write_addr   <= (others => '0');
-         o_write_data   <= (others => '0');
+         o_ram_read_addr    <= (others => '0');
+         o_ram_write_addr   <= (others => '0');
+         o_ram_write_data   <= (others => '0');
       else
          case i_alu_control is
             when C_SW   => 
-               o_write_addr <= i_alu_out(7 downto 0);
-               o_write_data <= i_rs2_data;
+               o_ram_write_addr <= i_alu_result(7 downto 0);
+               o_ram_write_data <= i_rs2_data;
             when others => 
          end case;
       end if;
