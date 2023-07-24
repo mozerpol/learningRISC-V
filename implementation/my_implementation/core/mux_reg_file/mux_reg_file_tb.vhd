510621e8fdcad1f35691e08a833b09b2ae6de391
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-24 18:48:13 +0200

    Add ports and bind them

--- implementation/my_implementation/core/mux_reg_file/mux_reg_file_tb.vhd
+++ implementation/my_implementation/core/mux_reg_file/mux_reg_file_tb.vhd
@@ -12,17 +12,32 @@ architecture tb of mux_reg_file_tb is
 
    component mux_reg_file is
    port (
-      i_rst             : in std_logic
+      i_rst                : in std_logic;
+      i_alu_result         : in std_logic_vector(31 downto 0);
+      i_instruction        : in std_logic_vector(31 downto 0);
+      i_mux_reg_file_ctrl  : in std_logic;
+      o_rd_data            : out std_logic_vector(31 downto 0);
+      o_instruction        : out std_logic_vector(31 downto 0)
    );
    end component mux_reg_file;
 
-   signal rst_tb              : std_logic;
+   signal rst_tb                 : std_logic;
+   signal alu_result_tb          : std_logic_vector(31 downto 0);
+   signal instruction_in_tb      : std_logic_vector(31 downto 0);
+   signal mux_reg_file_ctrl_tb   : std_logic;
+   signal rd_data_tb             : std_logic_vector(31 downto 0);
+   signal instruction_out_tb     : std_logic_vector(31 downto 0);
 
 begin
 
    inst_mux_reg_file : component mux_reg_file
    port map (
-      i_rst             => rst_tb
+      i_rst                => rst_tb,
+      i_alu_result         => alu_result_tb,
+      i_instruction        => instruction_in_tb,
+      i_mux_reg_file_ctrl  => mux_reg_file_ctrl_tb,
+      o_rd_data            => rd_data_tb,
+      o_instruction        => instruction_out_tb
    );
 
    p_tb : process
