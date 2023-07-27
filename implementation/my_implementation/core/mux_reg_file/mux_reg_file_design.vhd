98150a36718502e6f5b402ac744987ed22da897c
mozerpol <krzmozer@student.pg.edu.pl>
2023-07-27 23:24:52 +0200

    Add rd_data mux, shift instruction

--- implementation/my_implementation/core/mux_reg_file/mux_reg_file_design.vhd
+++ implementation/my_implementation/core/mux_reg_file/mux_reg_file_design.vhd
@@ -9,11 +9,12 @@ library mux_reg_file_lib;
 entity mux_reg_file is
    port (
       i_rst                : in std_logic;
+      i_clk                : in std_logic;
       i_alu_result         : in std_logic_vector(31 downto 0);
       i_instruction        : in std_logic_vector(31 downto 0);
       i_mux_reg_file_ctrl  : in std_logic;
       o_rd_data            : out std_logic_vector(31 downto 0);
-      o_instruction        : out std_logic_vector(31 downto 0)
+      o_instruction_dec    : out std_logic_vector(31 downto 0)
    );
 end entity mux_reg_file;
 
@@ -21,18 +22,15 @@ architecture rtl of mux_reg_file is
 
 begin
 
+
+    o_rd_data  <= i_alu_result when i_mux_reg_file_ctrl = '0' else i_instruction;
+
    p_mux : process(all)
    begin
       if (i_rst = '1') then
-         o_rd_data      <= (others => '0');
-         o_instruction  <= (others => '0');
-      else
-         if (i_mux_reg_file_ctrl = '1') then
-            o_rd_data      <= i_instruction;
-         else
-            -- TODO: add i_alu_result handler
-            o_instruction  <= i_instruction;
-         end if;
+         o_instruction_dec  <= (others => '0');
+      elsif (i_clk'event and i_clk = '1') then
+         o_instruction_dec <= i_instruction;
       end if;
    end process p_mux;
 
