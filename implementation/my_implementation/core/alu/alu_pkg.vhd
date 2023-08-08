fa4491b1bcb58ee105079ddc63935e6df607bf72
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-08 13:03:22 +0200

    Add store and load constants

--- implementation/my_implementation/core/alu/alu_pkg.vhd
+++ implementation/my_implementation/core/alu/alu_pkg.vhd
@@ -46,15 +46,6 @@ package alu_pkg is
    constant C_CSRRSI   : std_logic_vector(5 downto 0) := "101101";
    constant C_CSRRCI   : std_logic_vector(5 downto 0) := "101110";
    
-   constant C_LB       : std_logic_vector(2 downto 0) := "000";
-   constant C_LH       : std_logic_vector(2 downto 0) := "001";
-   constant C_LW       : std_logic_vector(2 downto 0) := "010";
-   constant C_LBU      : std_logic_vector(2 downto 0) := "011";
-   constant C_LHU      : std_logic_vector(2 downto 0) := "100";
-   constant C_SB       : std_logic_vector(2 downto 0) := "101";
-   constant C_SH       : std_logic_vector(2 downto 0) := "110";
-   constant C_SW       : std_logic_vector(2 downto 0) := "111";
-
 end;
 
 package body alu_pkg is
