359d85e022430e5f90ae088d24263a4a3c95ac51
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-11 13:03:57 +0200

    Change signal names, adapt for a new modules

--- implementation/my_implementation/main_design.vhd
+++ implementation/my_implementation/main_design.vhd
@@ -25,78 +25,73 @@ end entity main;
 architecture rtl of main is
 
 
+   component core is
+      port (
+      i_rst                : in std_logic;
+      i_clk                : in std_logic;
+      i_ram_data_read      : in std_logic_vector(31 downto 0);
+      o_ram_data_write     : out std_logic_vector(31 downto 0);
+      o_ram_addr           : out std_logic_vector(7 downto 0);
+      o_write_enable       : out std_logic
+      );
+   end component core;
+   
+
    component memory is
       port (
-         i_rst                : in std_logic;
-         i_clk                : in std_logic;
-         i_ram_read_addr      : in std_logic_vector(7 downto 0);
-         i_ram_write_addr     : in std_logic_vector(7 downto 0);
-         i_ram_write_data     : in std_logic_vector(31 downto 0);
-         i_ram_write_enable   : in std_logic;
-         o_instruction        : out std_logic_vector(31 downto 0)
+      i_rst                : in std_logic;
+      i_clk                : in std_logic;
+      i_ram_addr           : in std_logic_vector(7 downto 0);
+      i_write_enable       : in std_logic;
+      i_data               : in std_logic_vector(31 downto 0);
+      o_ram_data           : out std_logic_vector(31 downto 0)
       );
    end component memory;
 
-   component core is
-      port (
-          i_rst                : in std_logic;
-          i_clk                : in std_logic;
-          i_instruction_read   : in std_logic_vector(31 downto 0);
-          o_instruction_write  : out std_logic_vector(31 downto 0);
-          o_addr_read          : out std_logic_vector(7 downto 0);
-          o_addr_write         : out std_logic_vector(7 downto 0);
-          o_write_enable       : out std_logic
-      );
-   end component core;
    
    -- General
    signal rst               : std_logic;
    signal clk               : std_logic;
-   -- RAM
-   signal ram_read_addr     : std_logic_vector(7 downto 0);
-   signal ram_write_addr    : std_logic_vector(7 downto 0);
-   signal ram_write_data    : std_logic_vector(31 downto 0);
-   signal ram_write_enable  : std_logic;
-   signal instruction       : std_logic_vector(31 downto 0);
-   -- Core
-   signal instruction_read  : std_logic_vector(31 downto 0);
-   signal instruction_write : std_logic_vector(31 downto 0);
-   signal addr_read         : std_logic_vector(7 downto 0);
-   signal addr_write        : std_logic_vector(7 downto 0);
-   signal write_enable      : std_logic;
-   
+-- RAM and core
+signal ram_addr           : std_logic_vector(7 downto 0);
+signal write_enable       : std_logic;
+signal data               : std_logic_vector(31 downto 0);
+signal ram_data           : std_logic_vector(31 downto 0);
+signal ram_data_read      : std_logic_vector(31 downto 0);
+signal ram_data_write     : std_logic_vector(31 downto 0);
+
 
 begin
 
    inst_memory : component memory
    port map (
-      i_rst                => i_rst,
-      i_clk                => i_clk,
-      i_ram_read_addr      => ram_read_addr,
-      i_ram_write_addr     => ram_write_addr,
-      i_ram_write_data     => ram_write_data,
-      i_ram_write_enable   => ram_write_enable,
-      o_instruction        => instruction
+      i_rst                => rst,
+      i_clk                => clk,
+      i_ram_addr           => ram_addr,
+      i_write_enable       => write_enable,
+      i_data               => data,
+      o_ram_data           => ram_data
    );
 
    inst_core : component core 
    port map (
-      i_rst                => i_rst,
-      i_clk                => i_clk,
-      i_instruction_read   => instruction_read,
-      o_instruction_write  => instruction_write,
-      o_addr_read          => addr_read,
-      o_addr_write         => addr_write,
-      o_write_enable       => write_enable
+      i_rst                => rst,
+      i_clk                => clk,
+      i_ram_data_read      => ram_data_read,
+      o_ram_data_write     => ram_data_write, 
+      o_ram_addr           => ram_addr,
+      o_write_enable       => write_enable 
    );
-
     
-   ram_read_addr     <= addr_read; 
-   ram_write_addr    <= addr_write;
-   ram_write_data    <= instruction_write; 
-   ram_write_enable  <= write_enable;
-   --
-   instruction_read  <= instruction;
+rst <= i_rst;
+clk <= i_clk;
+-- RAM output
+ram_data_read <= ram_data;
+-- Core output 
+ram_data_write <= data;
+ram_addr    <= ram_addr;
+write_enable <= write_enable;
+      
 
    p_main : process(all)
    begin
