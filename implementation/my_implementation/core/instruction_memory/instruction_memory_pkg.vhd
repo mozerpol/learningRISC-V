3aadee4f3012e978dc7c685fecadad13c16676d6
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-13 13:02:59 +0200

    Swap places of SW with LW, change some values

--- implementation/my_implementation/core/instruction_memory/instruction_memory_pkg.vhd
+++ implementation/my_implementation/core/instruction_memory/instruction_memory_pkg.vhd
@@ -9,11 +9,11 @@ package instruction_memory_pkg is
    type t_instruction_memory is array (0 to 10) of std_logic_vector(31 downto 0);
 
    constant C_CODE : t_instruction_memory := (
-                              x"00200093", -- addi x1 x0 2           x1  =  x0  +  2     = 2
-                              x"00a00213", -- addi x4 x0 10          x4  =  x0  +  10    = 10
-                              x"001201b3", -- add x3, x4, x1         x3  =  10  +  2     = 12
-                              x"0020a103", -- lw x2, 2(x1)           x2  =  2   +  2     = 4          
-                              x"0021a123", -- sw x2, 2(x3)           RAM[2+12]           = 69 
+                              x"00200093", -- addi x1, x0, 2      x1  =  x0  +  2     = 2
+                              x"00a00213", -- addi x4, x0, 10     x4  =  x0  +  10    = 10
+                              x"001201b3", -- add x3, x4, x1      x3  =  10  +  2     = 12
+                              x"00322123", -- sw x3, 2(x4)        RAM[2+10]           = 12 -- load 32 bits from gpr(rs2) to RAM(rs1+offset)
+                              x"0001a103", -- lw x2, 0(x3)        x2  =  RAM[0+12]    = 12 -- load 32 bits from RAM(rs1+offset) to gpr(rd)
                               x"00000000",
                               x"00000000",
                               x"00000000",
