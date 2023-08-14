b59e1dfb31a0e605814b8cead333f68eb76855bf
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-14 13:02:36 +0200

    Create a new internal variables and assign them to modules

--- implementation/my_implementation/main_design.vhd
+++ implementation/my_implementation/main_design.vhd
@@ -52,53 +52,57 @@ architecture rtl of main is
    -- General
    signal rst               : std_logic;
    signal clk               : std_logic;
--- RAM and core
-signal ram_addr           : std_logic_vector(7 downto 0);
-signal write_enable       : std_logic;
-signal data               : std_logic_vector(31 downto 0);
-signal ram_data           : std_logic_vector(31 downto 0);
-signal ram_data_read      : std_logic_vector(31 downto 0);
-signal ram_data_write     : std_logic_vector(31 downto 0);
+-- core
+signal ram_data_read_core      : std_logic_vector(31 downto 0);
+signal ram_data_write_core     : std_logic_vector(31 downto 0);
+signal ram_addr_core           : std_logic_vector(7 downto 0);
+signal write_enable_core       : std_logic;
+-- RAM
+signal ram_addr_memory           : std_logic_vector(7 downto 0);
+signal write_enable_memory       : std_logic;
+signal data_memory               : std_logic_vector(31 downto 0);
+signal ram_data_memory           : std_logic_vector(31 downto 0);
 
 
 begin
-
-   inst_memory : component memory
-   port map (
-      i_rst                => rst,
-      i_clk                => clk,
-      i_ram_addr           => ram_addr,
-      i_write_enable       => write_enable,
-      i_data               => data,
-      o_ram_data           => ram_data
-   );
 
    inst_core : component core 
    port map (
       i_rst                => rst,
       i_clk                => clk,
-      i_ram_data_read      => ram_data_read,
-      o_ram_data_write     => ram_data_write, 
-      o_ram_addr           => ram_addr,
-      o_write_enable       => write_enable 
+      -- TODO: remove "ram" prefix, it's confusing, add sufix core instead
+      i_ram_data_read      => ram_data_read_core,
+      o_ram_data_write     => ram_data_write_core, 
+      o_ram_addr           => ram_addr_core,
+      o_write_enable       => write_enable_core 
    );
-    
+
+   inst_memory : component memory
+   port map (
+      i_rst                => rst,
+      i_clk                => clk,
+      i_ram_addr           => ram_addr_memory,
+      i_write_enable       => write_enable_memory,
+      i_data               => data_memory,
+      o_ram_data           => ram_data_memory
+   );
+
 rst <= i_rst;
 clk <= i_clk;
--- RAM output
-ram_data_read <= ram_data;
--- Core output 
-ram_data_write <= data;
-ram_addr    <= ram_addr;
-write_enable <= write_enable;
-      
+
+ram_addr_memory <= ram_addr_core;
+write_enable_memory <= write_enable_core;
+data_memory <= ram_data_write_core;
+ram_data_read_core <= ram_data_memory;
+
+
 
    p_main : process(all)
    begin
       if (i_rst) then
 
-      elsif (clk'event and clk = '1') then
-         
+      else
+
       end if;
    end process p_main;
 
