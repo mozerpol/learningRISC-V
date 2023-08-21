9b6ac59ca4c0dc3c64a7293588cf03c1befb6bce
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-21 13:05:12 +0200

    Change constans name, add writing result from pc to gpr

--- implementation/my_implementation/core/reg_file/reg_file_design.vhd
+++ implementation/my_implementation/core/reg_file/reg_file_design.vhd
@@ -13,10 +13,11 @@ entity reg_file is
       i_rs1_addr           : in std_logic_vector(4 downto 0);
       i_rs2_addr           : in std_logic_vector(4 downto 0);
       i_rd_addr            : in std_logic_vector(4 downto 0);
-      i_reg_file_inst_ctrl : in std_logic;
+      i_reg_file_inst_ctrl : in std_logic_vector(1 downto 0);
       i_reg_file_wr_ctrl   : in std_logic;
       i_rd_data            : in std_logic_vector(31 downto 0);
       i_alu_result         : in std_logic_vector(31 downto 0);
+      i_pc_addr            : in std_logic_vector(31 downto 0);
       o_rs1_data           : out std_logic_vector(31 downto 0);
       o_rs2_data           : out std_logic_vector(31 downto 0)
    );
@@ -40,10 +41,12 @@ begin
          gpr <= (others => (others => '0'));
       elsif (i_clk'event and i_clk = '1') then
          if (i_reg_file_wr_ctrl = C_WRITE_ENABLE) then
-            if (i_reg_file_inst_ctrl = C_DATA_REG_FILE) then
+            if (i_reg_file_inst_ctrl = C_WRITE_RD_DATA) then
                gpr(to_integer(unsigned(i_rd_addr))) <= i_rd_data;
-            elsif (i_reg_file_inst_ctrl = C_ALU_RESULT) then
+            elsif (i_reg_file_inst_ctrl = C_WRITE_ALU_RESULT) then
                gpr(to_integer(unsigned(i_rd_addr))) <= i_alu_result;
+            elsif (i_reg_file_inst_ctrl = C_WRITE_PC_ADDR) then
+               gpr(to_integer(unsigned(i_rd_addr))) <= i_pc_addr + 4;
             end if;
          end if;
       end if;
