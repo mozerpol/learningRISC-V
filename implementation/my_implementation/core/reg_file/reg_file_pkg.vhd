906d7f3b4a46a68cdf84d8c34639212b4d1faf89
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-21 13:06:15 +0200

    Change consts names, add third constant WRITE_PC_ADDR

--- implementation/my_implementation/core/reg_file/reg_file_pkg.vhd
+++ implementation/my_implementation/core/reg_file/reg_file_pkg.vhd
@@ -5,10 +5,11 @@ library ieee;
   
 package reg_file_pkg is
  
-   constant C_READ_ENABLE     : std_logic := '0';
-   constant C_WRITE_ENABLE    : std_logic := '1';
-   constant C_DATA_REG_FILE   : std_logic := '0';
-   constant C_ALU_RESULT      : std_logic := '1';
+   constant C_READ_ENABLE        : std_logic                    := '0';
+   constant C_WRITE_ENABLE       : std_logic                    := '1';
+   constant C_WRITE_RD_DATA      : std_logic_vector(1 downto 0) := "00";
+   constant C_WRITE_PC_ADDR      : std_logic_vector(1 downto 0) := "01";
+   constant C_WRITE_ALU_RESULT   : std_logic_vector(1 downto 0) := "10";
 
 end;
  
