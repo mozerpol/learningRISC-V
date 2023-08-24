2aa84eab1959d31d574bb1279d2ff530dc7c7311
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-24 21:41:02 +0200

    Add to pkg all branch instructions as a constants

--- implementation/my_implementation/core/branch_instructions/branch_instructions_pkg.vhd
+++ implementation/my_implementation/core/branch_instructions/branch_instructions_pkg.vhd
@@ -6,6 +6,12 @@ library ieee;
 package branch_instructions_pkg is
 
    -- constant C_NAME : std_logic_vector(N downto M) := "X";
+   constant C_BEQ    : std_logic_vector(2 downto 0) := "000";
+   constant C_BNE    : std_logic_vector(2 downto 0) := "001";
+   constant C_BLT    : std_logic_vector(2 downto 0) := "010";
+   constant C_BGE    : std_logic_vector(2 downto 0) := "011";
+   constant C_BLTU   : std_logic_vector(2 downto 0) := "100";
+   constant C_BGEU   : std_logic_vector(2 downto 0) := "101";
 
 end;
 
