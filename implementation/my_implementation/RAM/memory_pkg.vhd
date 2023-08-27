659b3ad7c9a196f36f2fd84c1f0eab2d56c1ea79
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-27 20:36:37 +0200

    Add c_ram_depth

--- implementation/my_implementation/RAM/memory_pkg.vhd
+++ implementation/my_implementation/RAM/memory_pkg.vhd
@@ -6,12 +6,13 @@ library ieee;
  package memory_pkg is
 
     -- constant C_NAME : std_logic_vector(N downto M) := "X";
-   type t_ram is array (0 to 127, 0 to 3) of std_logic_vector(7 downto 0);
+   constant C_RAM_DEPTH    : integer                        := 127;
    constant C_READ_ENABLE  : std_logic                      := '0';
    constant C_WRITE_ENABLE : std_logic                      := '1';   
    constant C_STORE_WORD   : std_logic_vector(1 downto 0)   := "00";
    constant C_STORE_HALF   : std_logic_vector(1 downto 0)   := "01";
    constant C_STORE_BYTE   : std_logic_vector(1 downto 0)   := "10";
+   type t_ram is array (0 to C_RAM_DEPTH-1, 0 to 3) of std_logic_vector(7 downto 0);
 
  end;
 
