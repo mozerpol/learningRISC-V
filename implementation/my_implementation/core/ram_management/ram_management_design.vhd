5e51fa3a2a6979881e470d5739e51db1deaad571
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-28 14:53:30 +0200

    Add all store instructions

--- implementation/my_implementation/core/ram_management/ram_management_design.vhd
+++ implementation/my_implementation/core/ram_management/ram_management_design.vhd
@@ -18,34 +18,68 @@ entity ram_management is
       i_imm                   : in std_logic_vector(31 downto 0);
       o_ram_addr              : out std_logic_vector(7 downto 0);
       o_write_enable          : out std_logic;
+      o_byte_number           : out std_logic_vector(3 downto 0);
       o_data                  : out std_logic_vector(31 downto 0)
    );
 end entity ram_management;
 
+-- LW / SW - address must be divisible by 4
+-- LH / SH - address must be divisible by 2
+-- LB / SB
+   
 architecture rtl of ram_management is
 
 begin
 
    p_ram_management : process(all)
+      variable v_address_row     : std_logic_vector(7 downto 0);
+      variable v_address_column  : std_logic_vector(7 downto 0);
    begin
       if (i_rst = '1') then
-         o_write_enable <= C_READ_ENABLE;
-         o_ram_addr     <= (others => '0');
-         o_data         <= (others => '0');
+         o_write_enable    <= C_READ_ENABLE;
+         o_ram_addr        <= (others => '0');
+         o_data            <= (others => '0');
+         o_byte_number     <= (others => '0');
+         v_address_row     := (others => '0');
+         v_address_column  := (others => '0');
       else
+         v_address_row     := i_rs1_data(7 downto 0) + i_imm(7 downto 0);
+         v_address_column  := v_address_row - (v_address_row(7 downto 2) & "00");
          case i_ram_management_ctrl is
             when C_SW   =>
-               o_ram_addr           <= i_rs1_data(7 downto 0) + i_imm(7 downto 0);
-               o_data               <= i_rs2_data;
                o_write_enable       <= C_WRITE_ENABLE;
+               o_ram_addr           <= v_address_row;
+               o_data               <= i_rs2_data;
+               o_byte_number        <= "1111";
             when C_SH   =>
-               o_write_enable       <= C_READ_ENABLE;
-          --     o_ram_addr           <= i_alu_result(7 downto 0);
-               o_data(15 downto 0)  <= i_rs2_data(15 downto 0);
+               o_write_enable       <= C_WRITE_ENABLE;
+               o_ram_addr           <= "00" & v_address_row(7 downto 2);
+               if (v_address_column(1 downto 0) = "00") then
+                  o_byte_number        <= "0011";
+                  o_data               <= i_rs2_data;
+               elsif (v_address_column(1 downto 0) = "01") then
+                  o_byte_number        <= "0110";
+                  o_data(23 downto 8)  <= i_rs2_data(15 downto 0);
+               elsif (v_address_column(1 downto 0) = "10") then
+                  o_byte_number        <= "1100";
+                  o_data(31 downto 16) <= i_rs2_data(15 downto 0);
+               end if;
             when C_SB   =>
-               o_write_enable       <= C_READ_ENABLE;
-            --   o_ram_addr           <= i_alu_result(7 downto 0);
-               o_data(7 downto 0)   <= i_rs2_data(7 downto 0);
+               o_write_enable       <= C_WRITE_ENABLE;
+               o_ram_addr           <= "00" & v_address_row(7 downto 2);
+               if (v_address_column(1 downto 0) = "00") then
+                  o_byte_number        <= "0001";
+                  o_data               <= i_rs2_data;
+               elsif (v_address_column(1 downto 0) = "01") then
+                  o_byte_number        <= "0010";
+                  o_data(15 downto 8)  <= i_rs2_data(7 downto 0);
+               elsif (v_address_column(1 downto 0) = "10") then
+                  o_byte_number        <= "0100";
+                  o_data(23 downto 16) <= i_rs2_data(7 downto 0);
+               elsif (v_address_column(1 downto 0) = "11") then
+                  o_byte_number        <= "1000";
+                  o_data(31 downto 24) <= i_rs2_data(7 downto 0);
+               end if;
             when C_LW   =>
                 o_write_enable      <= C_READ_ENABLE;
                 o_ram_addr          <= i_rs1_data(7 downto 0) + i_imm(7 downto 0);
