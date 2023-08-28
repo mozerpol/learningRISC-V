2899ac55e6d005842f1bd944cea3f7e538d9852c
mozerpol <krzmozer@student.pg.edu.pl>
2023-08-28 14:50:52 +0200

    Move test_instructions.asm and txt to ram_managment module

--- implementation/my_implementation/core/ram_management/test_instructions.asm
+++ implementation/my_implementation/core/ram_management/test_instructions.asm
@@ -1,5 +1,5 @@
 # Assign initial values
-addi x1, x0, 0x1
+addi ra, zero, 0x1
 addi x2, x0, 0x2
 addi x3, x0, 0x3
 lui  x4, 0x12345 
@@ -7,6 +7,14 @@ addi x4, x4, 0x678 # x4: 0x12345678
 addi x5, x0, 0x0
 addi x6, x0, 0x0
 addi x7, x0, 0x0
+
+sw   x0, 0(x0)
+sw   x0, 4(x0)
+sw   x0, 8(x0)
+sw   x0, 12(x0)
+sw   x0, 16(x0)
+sw   x0, 20(x0)
+
 # WRITE TO RAM
 # SW
 # RAM[0]: 78 56 34 12
@@ -18,23 +26,19 @@ sh   x4, 4(x0)
 # RAM[2]: 78 -- -- --
 sb   x4, 8(x0)
 # Check saving to different addresses
-# SW
-# RAM[3]: -- 78 56 34
-# RAM[4]: 12 -- -- --
-sw   x4, 12(x1)
 # SH
-# RAM[5]: -- -- 78 56
-sh   x4, 20(x2)
+# RAM[3]: -- -- 78 56
+sh   x4, 12(x2)
 # SB
-# RAM[6]: -- -- -- 78
-sb   x4, 24(x3)
+# RAM[4]: -- 78 -- --
+sb   x4, 16(x1)
 # READ FROM RAM
 # LW
-# x5: 0x56781234
-lw   x5, 1(x1)
+# x5: 0x12345678
+#lw   x5, 0(x0)
 # LH
-# x6: 0x00005678
-lh   x6, 2(x2)
+# x6: 0x00001234
+#lh   x6, 1(x1)
 # LB
 # x7: 0x00000012
-lb   x7, 0(x3)
+#lb   x7, 0(x3)
