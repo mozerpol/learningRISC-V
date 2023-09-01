4db4f7cdea6ccb95caadcf3ec3634def844dc679
mozerpol <krzmozer@student.pg.edu.pl>
2023-09-01 18:38:34 +0200

    Change name of bytes_number signal, add a small test

--- implementation/my_implementation/RAM/memory_tb.vhd
+++ implementation/my_implementation/RAM/memory_tb.vhd
@@ -20,7 +20,7 @@ architecture tb of memory_tb is
       i_ram_addr           : in std_logic_vector(7 downto 0);
       i_write_enable       : in std_logic;
       i_data               : in std_logic_vector(31 downto 0);
-      i_bytes_number       : in std_logic_vector(1 downto 0);
+      i_byte_number        : in std_logic_vector(3 downto 0);
       o_ram_data           : out std_logic_vector(31 downto 0)
    );
    end component memory;
@@ -30,7 +30,7 @@ architecture tb of memory_tb is
    signal ram_addr_tb         : std_logic_vector(7 downto 0);
    signal write_enable_tb     : std_logic;
    signal data_tb             : std_logic_vector(31 downto 0);
-   signal bytes_number_tb     : std_logic_vector(1 downto 0);
+   signal bytes_number_tb     : std_logic_vector(3 downto 0);
    signal ram_data_tb         : std_logic_vector(31 downto 0);
 
 begin
@@ -42,7 +42,7 @@ begin
       i_ram_addr           => ram_addr_tb,
       i_write_enable       => write_enable_tb,
       i_data               => data_tb,
-      i_bytes_number       => bytes_number_tb,
+      i_byte_number        => bytes_number_tb,
       o_ram_data           => ram_data_tb
    );
 
@@ -56,11 +56,11 @@ begin
 
    p_tb : process
    begin
-      
+
       ram_addr_tb       <= (others => '0');
       write_enable_tb   <= C_READ_ENABLE;
       data_tb           <= (others => '0');
-      bytes_number_tb   <= C_STORE_WORD;
+      bytes_number_tb   <= "0000";
       rst_tb            <= '1';
       wait for 10 ns;
       rst_tb            <= '0';
@@ -69,14 +69,46 @@ begin
       ---- WRITE TO RAM ----
       ----------------------
       ---- SW instruction ----
-      -- positive vmemorye
-      bytes_number_tb   <= C_STORE_WORD;
+      -- RAM[0]: 78 56 34 12
+      bytes_number_tb   <= "1111";
       ram_addr_tb       <= 8d"0";
       data_tb           <= 32x"12345678";
-      -- negative vmemorye
-      bytes_number_tb   <= C_STORE_WORD;
+      wait for 5 ns;
+      ---- SH instruction ----
+      -- RAM[1]: 78 56 -- --
+      bytes_number_tb   <= "0011";
+      ram_addr_tb       <= 8d"4";
       data_tb           <= 32x"12345678";
+      wait for 5 ns;
+      ---- SB instruction ----
+      -- RAM[2]: 78 -- -- --
+      bytes_number_tb   <= "0001";
+      data_tb           <= 32x"12345678";
+      ram_addr_tb       <= 8d"8";
+      wait for 5 ns;
+      --- Check saving to different addresses ---
+      ---- SH instruction ----
+      -- RAM[3]: -- -- 78 56
+      bytes_number_tb   <= "0011";
+      ram_addr_tb       <= 8d"14";
+      data_tb           <= 32x"12345678";
+      wait for 5 ns;
+      ---- SB instruction ----
+      -- RAM[4]: -- 78 -- --
+      bytes_number_tb   <= "0010";
+      ram_addr_tb       <= 8d"17";
+      data_tb           <= 32x"12345678";
+      wait for 5 ns;
 
+      -----------------------
+      ---- READ FROM RAM ----
+      -----------------------
+      ---- LW instruction ----
+      -- RAM[0]: 78 56 34 12
+   --   bytes_number_tb   <= C_STORE_WORD;
+   --   ram_addr_tb       <= 8d"0";
+  --    data_tb           <= 32x"12345678";
+  --    wait for 5 ns;
 
       wait for 25 ns;
       stop(2);
