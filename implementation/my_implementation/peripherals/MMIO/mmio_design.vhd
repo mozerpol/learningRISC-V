02c8e908fc74707a6e26d590044cbbaa33177649
mozerpol <krzmozer@student.pg.edu.pl>
2023-09-16 13:02:19 +0200

    Add ports

--- implementation/my_implementation/peripherals/MMIO/mmio_design.vhd
+++ implementation/my_implementation/peripherals/MMIO/mmio_design.vhd
@@ -7,12 +7,18 @@ library mmio_lib;
    use mmio_lib.mmio_pkg.all;
 
 entity mmio is
-port (
-);
+   port (
+      i_addr               : in integer range 0 to 63;
+      o_write_enable_ram   : out std_logic;
+      o_write_enable_gpio  : out std_logic 
+   );
 end mmio;
 
 architecture rtl of mmio is
 
 begin
+
+   o_write_enable_gpio <= '1' when i_addr = 63 else '0';
+   o_write_enable_ram  <= '1' when i_addr /= 63 else '0';
 
 end rtl;
