348e902259a5596a3a3ec986119aac0655d49835
mozerpol <krzmozer@student.pg.edu.pl>
2023-09-16 13:03:06 +0200

    Change i_addr type to vector, disable RAM

--- implementation/my_implementation/peripherals/MMIO/mmio_design.vhd
+++ implementation/my_implementation/peripherals/MMIO/mmio_design.vhd
@@ -8,7 +8,7 @@ library mmio_lib;
 
 entity mmio is
    port (
-      i_addr               : in integer range 0 to 63;
+      i_addr               : in std_logic_vector(5 downto 0);
       o_write_enable_ram   : out std_logic;
       o_write_enable_gpio  : out std_logic 
    );
@@ -18,7 +18,10 @@ architecture rtl of mmio is
 
 begin
 
-   o_write_enable_gpio <= '1' when i_addr = 63 else '0';
-   o_write_enable_ram  <= '1' when i_addr /= 63 else '0';
+   o_write_enable_gpio <= '1' when i_addr = "111111" else '0';
+   -- Jesli zapiszemy do RAM[63] dane, ktore maja sterowac GPIO, wtedy tez
+   -- mozemy odczytac co jest pod RAM[63] i wtedy wiemy jakie GPIO sa wysterowane.
+   -- W innym przypadku nie ma mozliwosci odczytu aktywnych GPIO
+   --o_write_enable_ram  <= '1' when i_addr /= "111111" else '0';
 
 end rtl;
