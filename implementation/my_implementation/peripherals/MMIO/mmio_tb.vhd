31135bda892d0d67cae34469435e4e712b8afdae
mozerpol <krzmozer@student.pg.edu.pl>
2023-09-16 13:02:59 +0200

    Add port signals, bind component

--- implementation/my_implementation/peripherals/MMIO/mmio_tb.vhd
+++ implementation/my_implementation/peripherals/MMIO/mmio_tb.vhd
@@ -15,19 +15,33 @@ architecture tb of mmio_tb is
 
    component mmio is
       port (
+         i_addr               : in integer range 0 to 63;
+         o_write_enable_ram   : out std_logic;
+         o_write_enable_gpio  : out std_logic
    );
    end component mmio;
 
+   signal addr_tb                : integer range 0 to 63;
+   signal write_enable_ram_tb    : std_logic;
+   signal write_enable_gpio_tb   : std_logic;
 
 begin
 
    inst_mmio : component mmio
    port map (
+      i_addr               => addr_tb,
+      o_write_enable_ram   => write_enable_ram_tb,
+      o_write_enable_gpio  => write_enable_gpio_tb
    );
 
 
    p_tb : process
    begin
+      addr_tb <= 0;
+      wait for 5 ns;
+      addr_tb <= 63;
+      wait for 5 ns;
+      addr_tb <= 60;
 
       wait for 25 ns;
       stop(2);
