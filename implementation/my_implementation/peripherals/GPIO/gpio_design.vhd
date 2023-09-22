c6046a390e67d52424009df85dd9ccc93716c264
mozerpol <krzmozer@student.pg.edu.pl>
2023-09-22 13:02:34 +0200

    Add signals to port, add gpio process

--- implementation/my_implementation/peripherals/GPIO/gpio_design.vhd
+++ implementation/my_implementation/peripherals/GPIO/gpio_design.vhd
@@ -7,7 +7,11 @@ library gpio_lib;
    use gpio_lib.gpio_pkg.all;
 
 entity gpio is
-port (
+   port (
+      i_clk    : in std_logic;
+      i_addr   : in std_logic_vector(31 downto 0);
+      i_wdata  : in std_logic_vector(31 downto 0);
+      o_gpio   : out std_logic_vector(3 downto 0)
 );
 end gpio;
 
@@ -16,5 +20,13 @@ architecture rtl of gpio is
 
 begin
 
+   p_gpio : process(i_clk)
+   begin
+      if (i_clk'event and i_clk = '1') then
+         if (i_addr = 6d"63") then
+            o_gpio <= i_wdata(3 downto 0);
+         end if;
+      end if;
+   end process p_gpio;
 
 end rtl;
