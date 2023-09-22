2a62c20ca9247261b5d38f239a5b09aaa6cdae74
mozerpol <krzmozer@student.pg.edu.pl>
2023-09-22 13:02:56 +0200

    Add port signals and bind them, add clk process

--- implementation/my_implementation/peripherals/GPIO/gpio_tb.vhd
+++ implementation/my_implementation/peripherals/GPIO/gpio_tb.vhd
@@ -15,16 +15,36 @@ architecture tb of gpio_tb is
 
    component gpio is
       port (
-   );
+         i_clk    : in std_logic;
+         i_addr   : in std_logic_vector(31 downto 0);
+         i_wdata  : in std_logic_vector(31 downto 0);
+         o_gpio   : out std_logic_vector(3 downto 0)
+      );
    end component gpio;
+
+   signal clk_tb     : std_logic;
+   signal addr_tb    : std_logic_vector(31 downto 0);
+   signal wdata_tb   : std_logic_vector(31 downto 0);
+   signal gpio_tb    : std_logic_vector(3 downto 0);
 
 
 begin
 
    inst_gpio : component gpio
    port map (
+      i_clk    => clk_tb,
+      i_addr   => addr_tb,
+      i_wdata  => wdata_tb,
+      o_gpio   => gpio_tb
    );
 
+   p_clk : process
+   begin
+      clk_tb <= '0';
+      wait for 1 ns;
+      clk_tb <= '1';
+      wait for 1 ns;
+   end process p_clk;
 
    p_tb : process
    begin
