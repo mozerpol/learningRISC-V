1b7a18902881ca0e944d2cac4643a48d55370758
mozerpol <krzmozer@student.pg.edu.pl>
2023-10-01 13:03:43 +0200

    Clean code

--- implementation/my_implementation/core/reg_file/reg_file_pkg.vhd
+++ implementation/my_implementation/core/reg_file/reg_file_pkg.vhd
@@ -2,9 +2,9 @@ library ieee;
    use ieee.std_logic_1164.all;
    use IEEE.std_logic_unsigned.all;
    use IEEE.math_real.all;
-  
+
 package reg_file_pkg is
- 
+
    constant C_READ_ENABLE        : std_logic                    := '0';
    constant C_WRITE_ENABLE       : std_logic                    := '1';
    constant C_WRITE_RD_DATA      : std_logic_vector(1 downto 0) := "00";
@@ -12,7 +12,7 @@ package reg_file_pkg is
    constant C_WRITE_ALU_RESULT   : std_logic_vector(1 downto 0) := "10";
 
 end;
- 
+
 package body reg_file_pkg is
- 
+
 end package body;
