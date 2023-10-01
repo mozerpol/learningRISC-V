15a3953c68a045389c96657635877429cb179014
mozerpol <krzmozer@student.pg.edu.pl>
2023-10-01 13:03:20 +0200

    Remove load and store instructions

--- implementation/my_implementation/core/reg_file/reg_file_design.vhd
+++ implementation/my_implementation/core/reg_file/reg_file_design.vhd
@@ -43,28 +43,7 @@ begin
       elsif (i_clk'event and i_clk = '1') then
          if (i_reg_file_wr_ctrl = C_WRITE_ENABLE) then
             if (i_reg_file_inst_ctrl = C_WRITE_RD_DATA) then
-            
-                gpr(to_integer(unsigned(i_rd_addr))) <= i_rd_data;
-            
-           --    if (i_load_instruction = "000") then    -- LB
-             --     if (i_rd_data(7) = '1') then
-              --       gpr(to_integer(unsigned(i_rd_addr))) <= 24x"ffffff" & i_rd_data(7 downto 0);
-             --     else 
-             --        gpr(to_integer(unsigned(i_rd_addr))) <= 24x"000000" & i_rd_data(7 downto 0);
-              --    end if;
-             --  elsif (i_load_instruction = "001") then -- LH
-             --     if (i_rd_data(15) = '1') then
-             --        gpr(to_integer(unsigned(i_rd_addr))) <= 16x"ffff" & i_rd_data(15 downto 0);
-             --     else 
-             --        gpr(to_integer(unsigned(i_rd_addr))) <= 16x"0000" & i_rd_data(15 downto 0);
-             --     end if; 
-           --    elsif (i_load_instruction = "010") then -- LW
-             --     gpr(to_integer(unsigned(i_rd_addr))) <= i_rd_data;                  
-            --   elsif (i_load_instruction = "011") then -- LBU
-            --      gpr(to_integer(unsigned(i_rd_addr))) <= 24x"000000" & i_rd_data(7 downto 0);
-           --    elsif (i_load_instruction = "100") then -- LHU
-            --      gpr(to_integer(unsigned(i_rd_addr))) <= 16x"0000" & i_rd_data(15 downto 0);
-            --   end if;
+               gpr(to_integer(unsigned(i_rd_addr))) <= i_rd_data;
             elsif (i_reg_file_inst_ctrl = C_WRITE_ALU_RESULT) then
                gpr(to_integer(unsigned(i_rd_addr))) <= i_alu_result;
             elsif (i_reg_file_inst_ctrl = C_WRITE_PC_ADDR) then
