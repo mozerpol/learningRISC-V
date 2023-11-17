add209bc1bb9417e5579db821f79b835b8fb36ae
mozerpol <krzmozer@student.pg.edu.pl>
2023-11-17 13:02:42 +0100

    Add clk to ports, reading from ROM on rising edge

--- implementation/my_implementation/core/instruction_memory/instruction_memory_design.vhd
+++ implementation/my_implementation/core/instruction_memory/instruction_memory_design.vhd
@@ -10,7 +10,8 @@ library instruction_memory_lib;
 entity instruction_memory is
    port (
       i_rst             : in std_logic;
-      i_ram_read_addr   : in std_logic_vector(31 downto 0);
+      i_clk             : in std_logic;
+      i_instruction_addr   : in std_logic_vector(31 downto 0);
       o_instruction     : out std_logic_vector(31 downto 0)
    );
 end entity instruction_memory;
@@ -22,20 +23,18 @@ architecture rtl of instruction_memory is
 begin
 
    p_instruction_memory : process(all)
+    variable v_asdf : std_logic;
    begin
       if (i_rst = '1') then
          o_instruction  <= (others => '0');
-         for i in 0 to C_INSTRUCTIONS_NUMBER-1 loop
-             rom(i*4)   <= C_CODE(i)(7 downto 0);
-             rom(i*4+1) <= C_CODE(i)(15 downto 8);
-             rom(i*4+2) <= C_CODE(i)(23 downto 16);
-             rom(i*4+3) <= C_CODE(i)(31 downto 24);
-         end loop;
-      else
-         o_instruction(7 downto 0)   <= rom(to_integer(unsigned(i_ram_read_addr)));
-         o_instruction(15 downto 8)  <= rom(to_integer(unsigned(i_ram_read_addr+1)));
-         o_instruction(23 downto 16) <= rom(to_integer(unsigned(i_ram_read_addr+2)));
-         o_instruction(31 downto 24) <= rom(to_integer(unsigned(i_ram_read_addr+3)));
+         v_asdf := '0';
+      elsif (i_clk = '1' and i_clk'event) then -- get rid of the ROM, read instructions directly from CONSTANT.
+     --    if (v_asdf = '1') then
+          o_instruction  <= C_CODE(to_integer(unsigned(i_instruction_addr(11 downto 2))));
+    --     else
+    --      v_asdf := '1';
+    --      o_instruction  <= (others => '0');
+    --     end if;
       end if;
    end process p_instruction_memory;
 
