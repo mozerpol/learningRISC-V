873e8cfe6e6263e792f9aba282bb2bf42c47cd30
mozerpol <krzmozer@student.pg.edu.pl>
2023-11-18 13:03:00 +0100

    Add consts

--- implementation/my_implementation/core/program_counter/program_counter_pkg.vhd
+++ implementation/my_implementation/core/program_counter/program_counter_pkg.vhd
@@ -9,6 +9,8 @@ package program_counter_pkg is
    constant C_DECREMENT_PC    : std_logic_vector(1 downto 0) := "01";
    constant C_LOAD_ALU_RESULT : std_logic_vector(1 downto 0) := "10";
    constant C_NOP             : std_logic_vector(1 downto 0) := "11";
+   constant C_INST_ADDR_PC    : std_logic := '0';
+   constant C_INST_ADDR_ALU   : std_logic := '1';
 
 end;
 
