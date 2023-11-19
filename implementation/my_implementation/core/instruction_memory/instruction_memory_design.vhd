de7d33680908b1d45f88a227b3e7ea0beac05981
mozerpol <krzmozer@student.pg.edu.pl>
2023-11-19 21:53:38 +0100

    Clean code

--- implementation/my_implementation/core/instruction_memory/instruction_memory_design.vhd
+++ implementation/my_implementation/core/instruction_memory/instruction_memory_design.vhd
@@ -9,32 +9,23 @@ library instruction_memory_lib;
 
 entity instruction_memory is
    port (
-      i_rst             : in std_logic;
-      i_clk             : in std_logic;
+      i_rst                : in std_logic;
+      i_clk                : in std_logic;
       i_instruction_addr   : in std_logic_vector(31 downto 0);
-      o_instruction     : out std_logic_vector(31 downto 0)
+      o_instruction        : out std_logic_vector(31 downto 0)
    );
 end entity instruction_memory;
 
 architecture rtl of instruction_memory is
 
-   signal rom : t_rom;
-
 begin
 
    p_instruction_memory : process(all)
-    variable v_asdf : std_logic;
    begin
       if (i_rst = '1') then
          o_instruction  <= (others => '0');
-         v_asdf := '0';
-      elsif (i_clk = '1' and i_clk'event) then -- get rid of the ROM, read instructions directly from CONSTANT.
-     --    if (v_asdf = '1') then
-          o_instruction  <= C_CODE(to_integer(unsigned(i_instruction_addr(11 downto 2))));
-    --     else
-    --      v_asdf := '1';
-    --      o_instruction  <= (others => '0');
-    --     end if;
+      elsif (i_clk = '1' and i_clk'event) then
+         o_instruction  <= C_CODE(to_integer(unsigned(i_instruction_addr(11 downto 2))));
       end if;
    end process p_instruction_memory;
 
