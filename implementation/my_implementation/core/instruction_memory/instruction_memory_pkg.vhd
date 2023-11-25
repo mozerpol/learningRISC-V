3bb3ed664e08f687f1c6858e9bc3d47ac64671fa
mozerpol <krzmozer@student.pg.edu.pl>
2023-11-25 13:02:58 +0100

    Change type names

--- implementation/my_implementation/core/instruction_memory/instruction_memory_pkg.vhd
+++ implementation/my_implementation/core/instruction_memory/instruction_memory_pkg.vhd
@@ -5,33 +5,28 @@ library ieee;
 
 package instruction_memory_pkg is
 
-   --constant C_INSTRUCTIONS_NUMBER : integer := 18;
-   --type t_instructions  is array (0 to C_INSTRUCTIONS_NUMBER-1) of std_logic_vector(31 downto 0);
-   --type t_rom           is array (0 to C_INSTRUCTIONS_NUMBER*4-1) of std_logic_vector(7 downto 0);
-   constant C_INSTRUCTIONS_NUMBER : integer := 18;
-   type t_instructions  is array (0 to 1024-1) of std_logic_vector(31 downto 0);
-   type t_rom           is array (0 to 1024*4-1) of std_logic_vector(7 downto 0);
 
-   constant C_CODE : t_instructions := (
-x"00100093",
-x"00200113",
-x"ffc00193",
-x"67830313",
-x"abcde3b7",
-x"0ff38393",
-x"00600023",
-x"006000a3",
-x"006080a3",
-x"006100a3",
-x"00610123",
-x"007184a3",
-x"fe728d23",
-x"00601423",
-x"00611423",
-x"fe721d23",
-x"00719923",
-others => x"00000000"
---x"00000000"
+   type t_rom  is array (0 to 1023) of std_logic_vector(31 downto 0);
+
+   constant C_CODE : t_rom := (
+    x"00100093",
+    x"00200113",
+    x"ffc00193",
+    x"67830313",
+    x"abcde3b7",
+    x"0ff38393",
+    x"00600023",
+    x"006000a3",
+    x"006080a3",
+    x"006100a3",
+    x"00610123",
+    x"007184a3",
+    x"fe728d23",
+    x"00601423",
+    x"00611423",
+    x"fe721d23",
+    x"00719923",
+    others => x"00000000"
                               );
 end;
 
