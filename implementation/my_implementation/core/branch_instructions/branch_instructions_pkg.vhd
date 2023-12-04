9f96f856c1945c73e7f8887595d3759a5c5513b1
mozerpol <krzmozer@student.pg.edu.pl>
2023-12-04 13:02:33 +0100

    Add a new consts for branch

--- implementation/my_implementation/core/branch_instructions/branch_instructions_pkg.vhd
+++ implementation/my_implementation/core/branch_instructions/branch_instructions_pkg.vhd
@@ -5,7 +5,8 @@ library ieee;
 
 package branch_instructions_pkg is
 
-   -- constant C_NAME : std_logic_vector(N downto M) := "X";
+   constant C_TAKEN : std_logic := '1';
+   constant C_NOT_TAKEN : std_logic := '0';
    constant C_BEQ    : std_logic_vector(2 downto 0) := "000";
    constant C_BNE    : std_logic_vector(2 downto 0) := "001";
    constant C_BLT    : std_logic_vector(2 downto 0) := "010";
