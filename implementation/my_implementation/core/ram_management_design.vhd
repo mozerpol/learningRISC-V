cd310f357a185280ad1a4aadd840ca303229e952
mozerpol <krzmozer@student.pg.edu.pl>
2023-12-14 19:04:53 +0100

    Remove latch from o_data, o_raddr and o_waddr signals

--- implementation/my_implementation/core/ram_management_design.vhd
+++ implementation/my_implementation/core/ram_management_design.vhd
@@ -46,45 +46,71 @@ begin
          o_data            <= (others => '0');
          v_ram_address     := (others => '0');
       else
-         v_ram_address     := (i_rs1_data(31 downto 0) + i_imm(31 downto 0));
+         v_ram_address     := i_rs1_data(31 downto 0) + i_imm(31 downto 0);
          case i_ram_management_ctrl is
             when C_SW   =>
                o_write_enable <= C_WRITE_ENABLE;
                o_byte_enable  <= "1111";
                o_waddr        <= to_integer(unsigned(v_ram_address(31 downto 2)));
                o_data         <= i_rs2_data;
+               o_raddr           <= 0;
             when C_SH   =>
                o_write_enable <= C_WRITE_ENABLE;
                o_waddr        <= to_integer(unsigned(v_ram_address(31 downto 2)));
+               o_raddr           <= 0;
                if (v_ram_address(1 downto 0) = "00") then
                   o_byte_enable        <= "0011";
                   o_data(15 downto 0)  <= i_rs2_data(15 downto 0);
+                  o_data(31 downto 16) <= (others => '0');
                elsif (v_ram_address(1 downto 0) = "10") then
                   o_byte_enable        <= "1100";
+                   o_data(15 downto 0) <= (others => '0');
                   o_data(31 downto 16) <= i_rs2_data(15 downto 0);
+               else
+                  o_byte_enable        <= "0000";
+                  o_data               <= (others => '0');
                end if;
             when C_SB   =>
                o_write_enable <= C_WRITE_ENABLE;
                o_waddr        <= to_integer(unsigned(v_ram_address(31 downto 2)));
+               o_raddr           <= 0;
                if (v_ram_address(1 downto 0) = "00") then
                   o_data(7 downto 0)   <= i_rs2_data(7 downto 0);
+                  o_data(15 downto 8)  <= (others => '0');
+                  o_data(23 downto 16) <= (others => '0');
+                  o_data(31 downto 24) <= (others => '0');
                   o_byte_enable        <= "0001";
                elsif (v_ram_address(1 downto 0) = "01") then
+               o_data(7 downto 0)   <= (others => '0');
                   o_data(15 downto 8)  <= i_rs2_data(7 downto 0);
+                 o_data(23 downto 16) <= (others => '0');
+                  o_data(31 downto 24) <= (others => '0');
                   o_byte_enable        <= "0010";
                elsif (v_ram_address(1 downto 0) = "10") then
+               o_data(7 downto 0)   <= (others => '0');
+                o_data(15 downto 8)  <= (others => '0');
                   o_data(23 downto 16) <= i_rs2_data(7 downto 0);
+                  o_data(31 downto 24) <= (others => '0');
                   o_byte_enable        <= "0100";
                elsif (v_ram_address(1 downto 0) = "11") then
+               o_data(7 downto 0)   <= (others => '0');
+                o_data(15 downto 8)  <= (others => '0');
+                o_data(23 downto 16) <=  (others => '0');
                   o_data(31 downto 24) <= i_rs2_data(7 downto 0);
                   o_byte_enable        <= "1000";
+               else
+                  o_data               <= (others => '0');
+                  o_byte_enable        <= "0000";
                end if;
             when C_LW | C_LH | C_LHU | C_LB | C_LBU  =>
                o_write_enable <= C_READ_ENABLE;
                o_raddr        <= to_integer(unsigned(v_ram_address(31 downto 2)));
+               o_waddr        <= 0;
+               o_byte_enable  <= "0000";
+               o_data            <= (others => '0');
             when others =>
                o_write_enable <= C_READ_ENABLE;
-               o_byte_enable  <= (others => '0');
+               o_byte_enable  <= "0000";
                o_raddr        <= 0;
                o_waddr        <= 0;
                o_data         <= (others => '0');
@@ -110,16 +136,20 @@ begin
                   o_rd_data(31 downto 16) <= 16x"0000";
                end if;
                if (v_ram_address(1 downto 0) = "00") then
-                  o_rd_data(15 downto 00) <= i_data_from_ram(15 downto 0);
+                  o_rd_data(15 downto 0) <= i_data_from_ram(15 downto 0);
                elsif (v_ram_address(1 downto 0) = "10") then
-                  o_rd_data(15 downto 00) <= i_data_from_ram(31 downto 16);
+                  o_rd_data(15 downto 0) <= i_data_from_ram(31 downto 16);
+               else
+                  o_rd_data(15 downto 0) <= (others => '0');
                end if;
             when C_LHU =>
                o_rd_data(31 downto 16) <= 16x"0000";
                if (v_ram_address(1 downto 0) = "00") then
-                  o_rd_data(15 downto 00) <= i_data_from_ram(15 downto 0);
+                  o_rd_data(15 downto 0) <= i_data_from_ram(15 downto 0);
                elsif (v_ram_address(1 downto 0) = "10") then
-                  o_rd_data(15 downto 00) <= i_data_from_ram(31 downto 16);
+                  o_rd_data(15 downto 0) <= i_data_from_ram(31 downto 16);
+               else
+                  o_rd_data(15 downto 0) <= (others => '0');
                end if;
             when C_LB =>
                if (i_data_from_ram(7) = '1') then
@@ -135,6 +165,8 @@ begin
                   o_rd_data(7 downto 0)   <= i_data_from_ram(23 downto 16);
                elsif (v_ram_address(1 downto 0) = "11") then
                   o_rd_data(7 downto 0)   <= i_data_from_ram(31 downto 24);
+               else
+                  o_rd_data(7 downto 0)   <= (others => '0');
                end if;
             when C_LBU =>
                o_rd_data(31 downto 8)  <= 24x"000000";
@@ -146,6 +178,8 @@ begin
                   o_rd_data(7 downto 0)   <= i_data_from_ram(23 downto 16);
                elsif (v_ram_address(1 downto 0) = "11") then
                   o_rd_data(7 downto 0)   <= i_data_from_ram(31 downto 24);
+               else
+                  o_rd_data(7 downto 0)   <= (others => '0'); 
                end if;
             when others => o_rd_data <= (others => '0');
          end case;
