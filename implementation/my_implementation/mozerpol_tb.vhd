cb273dd6c005a836005eca243c4e1c6482c8a18f
mozerpol <krzmozer@student.pg.edu.pl>
2024-01-06 18:35:07 +0100

    Working alias to GPR

--- implementation/my_implementation/mozerpol_tb.vhd
+++ implementation/my_implementation/mozerpol_tb.vhd
@@ -20,6 +20,7 @@ end mozerpol_tb;
 
 architecture tb of mozerpol_tb is
 
+
    component mozerpol is
    port (
       i_rst       : in std_logic;
@@ -30,8 +31,10 @@ architecture tb of mozerpol_tb is
    signal rst_tb  : std_logic;
    signal clk_tb  : std_logic;
 
-   -- type t_gpr  is array(0 to 31) of std_logic_vector(31 downto 0);
-   -- alias spy_gpr is <<signal .mozerpol_tb.inst_mozerpol.inst_core.inst_reg_file.gpr: t_gpr >>;
+   type t_gpr  is array(0 to 31) of std_logic_vector(31 downto 0);
+
+
+   signal test : std_logic;
 
 begin
 
@@ -50,10 +53,27 @@ begin
    end process;
 
    p_tb : process
+      -- Read why is worknig iksde
+      -- https://stackoverflow.com/questions/48347200/vhdl-2008-cant-drive-a-signal-with-an-alias-of-an-external-name
+      alias spy_test is <<signal .mozerpol_tb.inst_mozerpol.inst_core.inst_reg_file.i_reg_file_inst_ctrl: std_logic_vector(1 downto 0) >>;
+      alias spy_gpr is <<signal .mozerpol_tb.inst_mozerpol.inst_core.inst_reg_file.gpr: t_gpr >>;
    begin
       rst_tb   <= '1';
       wait for 20 ns;
       rst_tb   <= '0';
+
+      test <= '1'; --spy_test(1);
+      wait for 1 ns;
+      report "=========REG_FILE_WR_CTRL=========: " & to_string(spy_test); -- std_logic'image(test);
+
+      wait for 2 ns;
+      report "===========GPR===========: " & to_string(spy_gpr(1));
+      
+      if (spy_test(1) = '1') then
+         report "OK";
+      else
+         report "NIE OK";
+      end if;
 
       wait for 2000 ns;
       stop(2);
