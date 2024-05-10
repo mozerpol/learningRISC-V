dd901a74ca2c7ba2ef583c366bc73610a4690541
mozerpol <krzmozer@student.pg.edu.pl>
2024-05-10 13:03:01 +0200

    Add write enable to gpio

--- implementation/my_implementation/peripherals/gpio_design.vhd
+++ implementation/my_implementation/peripherals/gpio_design.vhd
@@ -22,6 +22,7 @@ entity gpio is
       i_clk    : in std_logic;
       i_addr   : in integer range 0 to C_RAM_LENGTH-1;
       i_wdata  : in std_logic_vector(31 downto 0);
+      i_we     : in std_logic;
       o_gpio   : out std_logic_vector(7 downto 0)
 );
 end gpio;
@@ -41,6 +42,7 @@ begin
          -- sb x0, C_MMIO_ADDR_GPIO*4-1
          -- For C_MMIO_ADDR_GPIO = 64 will be:
          -- sb x0, 255(x0)
+         if (i_we = '1') then
          if (i_addr = C_MMIO_ADDR_GPIO-1) then
             -- Last 8 bits from wdata vector are mapped
             o_gpio(0) <= i_wdata(24);
@@ -51,6 +53,7 @@ begin
             o_gpio(5) <= i_wdata(29);
             o_gpio(6) <= i_wdata(30);
             o_gpio(7) <= i_wdata(31);
+         end if;
          end if;
       end if;
    end process p_gpio;
