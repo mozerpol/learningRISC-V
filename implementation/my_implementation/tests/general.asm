4ed0a3cff9f38252c96691c7fe058df414d77808
mozerpol <krzmozer@student.pg.edu.pl>
2024-05-13 13:02:22 +0200

    Check the four GPIO bits, asm code

--- implementation/my_implementation/tests/general.asm
+++ implementation/my_implementation/tests/general.asm
@@ -699,3 +699,23 @@ sb    x12, 255(x0)     # gpio = 10000001
 sb    x13, 255(x0)     # gpio = 00011000
 sb    x14, 255(x0)     # gpio = 10011001
 sb    x0,  255(x0)     # gpio = 00000000
+###################################
+##   Check the four GPIO bits    ##
+###################################
+addi  x1,  x0,   0     # The value x1 is assigned to GPIO
+addi  x2,  x0,   15    # The value x1 is compared to the value of x2. 15 = 1111
+addi  x3,  x0,   0     # The value of x3 is compared to the value of x4, this
+# works as a delay loop. The x3 is incremented if is not equal x4
+lui   x4,  244
+addi  x4,  x4,   576   # F4240 = 1000000
+loop2:
+addi  x1,  x1,   1
+sb    x1,  0(x0)       # Assign the value of x1 to GPIO
+loop1:
+addi  x3,  x3,   1     # Delay loop
+bne   x3,  x4,   loop1 # Is there enough delay?
+addi  x3,  x0,   0     # Yes, reset delay counter
+bne   x1,  x2,   loop2 # Were all the GPIOs on? If not go to loop2
+# Yes, all the GPIOs were turn on, reset all counters
+addi  x1,  x0,   0
+addi  x3,  x0,   0
