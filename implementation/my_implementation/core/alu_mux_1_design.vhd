7af9a61921e2858ae6832432c9f3cbf945580e21
mozerpol <krzmozer@student.pg.edu.pl>
2024-05-20 13:05:16 +0200

    Change alu_mux1, works as combinational circuit, remove rst

--- implementation/my_implementation/core/alu_mux_1_design.vhd
+++ implementation/my_implementation/core/alu_mux_1_design.vhd
@@ -20,7 +20,7 @@ library control_lib;
 
 entity alu_mux_1 is
    port (
-      i_rst             : in std_logic;
+
       i_alu_mux_1_ctrl  : in std_logic;
       i_rs1_data        : in std_logic_vector(31 downto 0);
       i_pc_addr         : in std_logic_vector(31 downto 0);
@@ -32,17 +32,17 @@ architecture rtl of alu_mux_1 is
 
 begin
 
-   p_alu_mux_1 : process(i_rst, i_alu_mux_1_ctrl, i_rs1_data, i_pc_addr)
+
+
+   p_alu_mux_1 : process(i_alu_mux_1_ctrl, i_rs1_data, i_pc_addr)
    begin
-      if (i_rst = '1') then
-         o_alu_operand_1      <= (others => '0');
-      else
+
          case i_alu_mux_1_ctrl is
             when C_RS1_DATA   => o_alu_operand_1 <= i_rs1_data;
             when C_PC_ADDR    => o_alu_operand_1 <= i_pc_addr;
             when others       => o_alu_operand_1 <= (others => '0');
          end case;
-      end if;
+
    end process p_alu_mux_1;
 
 end architecture rtl;
