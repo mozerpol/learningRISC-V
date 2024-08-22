0c5f7494d0ff84c68571f20add12e64dbc33a593
mozerpol <krzmozer@student.pg.edu.pl>
2024-08-22 13:03:49 +0200

    Add examples for waves

---
+++ implementation/single_stage/simulation_script/ghdl/waveforms.tcl
@@ -0,0 +1,7 @@
+gtkwave::/Edit/Insert_Comment "A"
+gtkwave::addSignalsFromList top.adder_tb.adder_0.ci
+gtkwave::/Edit/Insert_Comment "B"
+gtkwave::addSignalsFromList top.adder_tb.adder_0.co
+gtkwave::addSignalsFromList top.adder_tb.adder_0.s
+
+gtkwave::/Time/Zoom/Zoom_Full
