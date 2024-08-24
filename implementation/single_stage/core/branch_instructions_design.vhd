66610e415abf6f582e3b329a85ca82f53499c335
mozerpol <krzmozer@student.pg.edu.pl>
2024-08-24 13:04:15 +0200

    Clean packages

--- implementation/single_stage/core/branch_instructions_design.vhd
+++ implementation/single_stage/core/branch_instructions_design.vhd
@@ -13,7 +13,6 @@
 
 library ieee;
    use ieee.std_logic_1164.all;
-   use ieee.std_logic_unsigned.all;
    use ieee.numeric_std.all;
 library control_lib;
    use control_lib.all;
--- implementation/single_stage/core/control_pkg.vhd
+++ implementation/single_stage/core/control_pkg.vhd
@@ -9,7 +9,7 @@
 
 library ieee;
    use ieee.std_logic_1164.all;
-   use IEEE.std_logic_unsigned.all;
+   use ieee.numeric_std.all;
    use IEEE.math_real.all;
 
  package control_pkg is
