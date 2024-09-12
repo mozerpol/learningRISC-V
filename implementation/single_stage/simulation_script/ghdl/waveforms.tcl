43c326f9e6028fb6d997d8a4eb7484f81736fda0
mozerpol <krzmozer@student.pg.edu.pl>
2024-09-12 13:02:13 +0200

    Add waves

--- implementation/single_stage/simulation_script/ghdl/waveforms.tcl
+++ implementation/single_stage/simulation_script/ghdl/waveforms.tcl
@@ -27,26 +27,54 @@ gtkwave::/Edit/Insert_Blank
 
 gtkwave::/Edit/Insert_Comment "CORE"
 gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.i_core_data_read
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.o_core_data_write
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.o_core_write_enable
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.o_core_byte_enable
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.o_core_addr_read
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.o_core_addr_write
 gtkwave::/Edit/Insert_Blank
 
 gtkwave::/Edit/Insert_Comment "ALU"
 gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu.i_alu_operand_1
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu.i_alu_operand_2
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu.i_alu_control
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu.o_alu_result
 gtkwave::/Edit/Insert_Blank
 
 gtkwave::/Edit/Insert_Comment "ALU_1_MUX"
 gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_1.i_alu_mux_1_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_1.i_rs1_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_1.i_pc_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_1.o_alu_operand_1
 gtkwave::/Edit/Insert_Blank
 
 gtkwave::/Edit/Insert_Comment "ALU_2_MUX"
 gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_2.i_alu_mux_2_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_2.i_rs2_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_2.i_imm
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_alu_mux_2.o_alu_operand_2
 gtkwave::/Edit/Insert_Blank
 
 gtkwave::/Edit/Insert_Comment "BRANCH_INST"
 gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_branch_instructions.i_branch_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_branch_instructions.i_rs1_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_branch_instructions.i_rs2_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_branch_instructions.o_branch_result
 gtkwave::/Edit/Insert_Blank
 
 gtkwave::/Edit/Insert_Comment "CONTROL"
 gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.i_opcode
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.i_func3
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.i_func7
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.i_branch_result
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_alu_mux_1_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_alu_mux_2_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_pc_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_inst_addr_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_alu_control
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_ram_management_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_reg_file_inst_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_branch_ctrl
 gtkwave::/Edit/Insert_Blank
 
 
