b0a69eae0e83a96fbc2ec821f43f1de962239d9e
mozerpol <krzmozer@student.pg.edu.pl>
2024-09-12 13:02:09 +0200

    Finish waves

--- implementation/single_stage/simulation_script/ghdl/waveforms.tcl
+++ implementation/single_stage/simulation_script/ghdl/waveforms.tcl
@@ -77,10 +77,58 @@ gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o
 gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_control.o_branch_ctrl
 gtkwave::/Edit/Insert_Blank
 
+gtkwave::/Edit/Insert_Comment "DECODER"
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.i_instruction
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.o_rd_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.o_rs1_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.o_rs2_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.o_imm
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.o_opcode
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.o_func3
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_decoder.o_func7
+gtkwave::/Edit/Insert_Blank
 
+gtkwave::/Edit/Insert_Comment "INSTRUCTION_MEM"
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_instruction_memory.i_instruction_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_instruction_memory.o_instruction
+gtkwave::/Edit/Insert_Blank
 
+gtkwave::/Edit/Insert_Comment "PC"
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_program_counter.i_alu_result
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_program_counter.i_pc_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_program_counter.i_inst_addr_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_program_counter.o_pc_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_program_counter.o_instruction_adde
+gtkwave::/Edit/Insert_Blank
 
+gtkwave::/Edit/Insert_Comment "RAM_MNGT"
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.i_ram_management_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.i_rs1_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.i_rs2_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.i_imm
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.i_data_from_ram
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.o_rd_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.v_ram_address
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.v_reg_file_address
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.o_write_enable
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.o_byte_enable
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.o_raddr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.o_waddr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_ram_management.o_data
+gtkwave::/Edit/Insert_Blank
 
+gtkwave::/Edit/Insert_Comment "REG_FILE"
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.i_rs1_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.i_rs2_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.i_rd_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.i_reg_file_inst_ctrl
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.i_rd_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.i_alu_result
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.i_pc_addr
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.o_rs2_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.o_rs1_data
+gtkwave::addSignalsFromList top.riscpol_tb.inst_riscpol.inst_core.inst_reg_file.gpr
+gtkwave::/Edit/Insert_Blank
 
-
+# Customize view settings
 gtkwave::/Time/Zoom/Zoom_Full
