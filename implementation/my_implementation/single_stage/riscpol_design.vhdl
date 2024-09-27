9608e6f05f151384be5d6ffb6e2f210c14d12cd2
mozerpol <krzmozer@student.pg.edu.pl>
2024-09-27 13:03:12 +0200

    Add counter and bind with main design

--- implementation/my_implementation/single_stage/riscpol_design.vhdl
+++ implementation/my_implementation/single_stage/riscpol_design.vhdl
@@ -17,6 +17,7 @@ library riscpol_lib;
    use riscpol_lib.riscpol_pkg.all;
 library ram_lib;
 library gpio_lib;
+library counter8bit_lib;
 library core_lib;
    use core_lib.all;
 
@@ -26,6 +27,7 @@ entity riscpol is
       i_rst    : in std_logic;
       i_clk    : in std_logic;
       o_gpio   : out std_logic_vector(7 downto 0)
+      -- TODO: Add o_ov_t8bit to ports, as a one bit, only for overflow
    );
 end entity riscpol;
 
@@ -73,6 +75,19 @@ architecture rtl of riscpol is
          o_gpio   : out std_logic_vector(7 downto 0)
       );
    end component gpio;
+   
+   
+   component counter8 is
+      generic(
+         G_COUNTER_VALUE : positive := 255
+      ); port(
+         i_clk          : in std_logic;
+         i_rst          : in std_logic;
+         i_addr         : in integer range 0 to C_RAM_LENGTH-1;
+         i_ce           : in std_logic;
+         o_q_counter8   : out std_logic_vector(7 downto 0)
+   );
+   end component counter8;
 
    -- General
    signal rst                 : std_logic;
@@ -85,6 +100,8 @@ architecture rtl of riscpol is
    signal core_addr_write     : integer range 0 to C_RAM_LENGTH-1;
    -- RAM
    signal ram_q               : std_logic_vector(31 downto 0);
+   -- Counter
+   signal q_counter8          : std_logic_vector(7 downto 0);
 
 begin
 
@@ -119,6 +136,19 @@ begin
        i_we    => core_write_enable,
        o_gpio  => o_gpio
     );
+    
+    inst_counter8bit : component counter8
+    generic map(
+      G_COUNTER_VALUE => 255
+    )
+    port map (
+       i_clk         => clk,
+       i_rst         => rst,
+       i_addr        => core_addr_write,
+       i_ce          => core_data_write(24),
+       o_q_counter8  => open
+    );
+
 
    rst <= (i_rst);
    clk <= i_clk;
