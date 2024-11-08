0aba084fa44b43d13c75d6143958f37d623ea5f9
mozerpol <krzmozer@student.pg.edu.pl>
2024-11-08 13:07:52 +0100

    Add tests for counter8bit

--- implementation/my_implementation/single_stage/code_samples/general.asm
+++ implementation/my_implementation/single_stage/code_samples/general.asm
@@ -723,11 +723,11 @@ addi  x1,  x0,   0
 addi  x3,  x0,   0
 sb    x0,  255(x0)
 ###################################
-##       Check Timer8bit         ##
+##      Check Counter8bit        ##
 ###################################
 addi  x1,  x0,   2     # Delay purposes, short loop
 addi  x2,  x0,   0x212 # Delay purposes, long loop
-addi  x3,  x0,   1     # 1 = turn on the timer, 0 = turn off the timer
+addi  x3,  x0,   1     # 1 = turn on the timer, 0 = turn off and reset the timer
 addi  x4,  x0,   0     # The value of x4 is compared to the value of x1, this
                        # works as a delay loop.
 # Check the timer for one clock cycle
@@ -751,6 +751,13 @@ addi  x4,  x4,   1     # Long delay loop
 bne   x4,  x2,   loop27# Is there enough delay?
 sb    x0,  251(x0)     # 0 = turn off the timer
 addi  x4,  x0,   0     # Reset delay loop
+# Read the counter value and save it in the x5 register
+sb    x3,  251(x0)     # 1 = turn on the timer
+addi  x0,  x0,   0     # nop
+addi  x0,  x0,   0     # nop
+addi  x0,  x0,   0     # nop
+lw    x5,  251(x0)     # Read the counter value and save it in the x5 register
+sb    x0,  251(x0)     # 0 = turn off the timer
 
 ####################################
 ##  Check behaviour after reset   ##
