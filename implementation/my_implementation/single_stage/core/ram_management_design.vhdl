b81a6b8c4c693e61eeb5241ee0e80cb3864848ff
mozerpol <krzmozer@student.pg.edu.pl>
2024-11-10 14:52:17 +0100

    Add comment

--- implementation/my_implementation/single_stage/core/ram_management_design.vhdl
+++ implementation/my_implementation/single_stage/core/ram_management_design.vhdl
@@ -27,7 +27,8 @@ entity ram_management is
       i_rs1_data              : in std_logic_vector(31 downto 0);
       i_rs2_data              : in std_logic_vector(31 downto 0);
       i_imm                   : in std_logic_vector(31 downto 0);
-      i_data_from_ram         : in std_logic_vector(31 downto 0);
+      i_data_from_ram         : in std_logic_vector(31 downto 0); -- TODO: 
+      -- change to i_data_from_mmio or sth like this
       o_rd_data               : out std_logic_vector(31 downto 0);
       o_write_enable          : out std_logic;
       o_byte_enable           : out std_logic_vector (3 downto 0);
