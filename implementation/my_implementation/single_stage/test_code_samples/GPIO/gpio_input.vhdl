b1da50c54ed74b8111f2493ed620ebab17deff35
mozerpol <krzmozer@student.pg.edu.pl>
2025-08-16 14:10:50 +0200

    Add gpio tests

---
+++ implementation/my_implementation/single_stage/test_code_samples/GPIO/gpio_input.vhdl
@@ -0,0 +1,15 @@
+      ---------------------
+      --    GPIO input   --
+      ---------------------
+      gpio_tb        <= 8b"00001101"; -- Simulate GPIO input
+      wait until rising_edge(clk_tb); -- nop instruction
+      wait until rising_edge(clk_tb); -- nop instruction
+      wait until rising_edge(clk_tb); -- nop instruction
+      wait until rising_edge(clk_tb); -- nop instruction
+      wait until rising_edge(clk_tb); -- nop instruction
+      -- Check if value 0000000D has been loaded into register x1
+      check_gpr( instruction    => "lw    x1,  255(x0)",
+                 gpr            => spy_gpr(1),
+                 desired_value  => 32x"0000000D",
+                 test_point     => set_test_point );
+      gpio_tb        <= (others => 'Z');
--- implementation/my_implementation/single_stage/test_code_samples/GPIO/gpio_output.asm
+++ implementation/my_implementation/single_stage/test_code_samples/GPIO/gpio_output.asm
@@ -10,19 +10,19 @@ addi  x1,  x0,   0     # The value x1 is assigned to GPIO
 addi  x2,  x0,   15    # The value x1 is compared to the value of x2. 15 = 1111
 addi  x3,  x0,   0     # The value of x3 is compared to the value of x4, this
 # works as a delay loop. The x3 is incremented if is not equal x4
-lui   x4,  0x3D0
-addi  x4,  x4,   2047
-addi  x4,  x4,   257   # 3D0900 = 4000000, x4 is compared with x3. For 40 MHz it
-# is about 1 sec delay
+addi  x4,  x0,   10    # For delay purposes between changing state of GPIO
 
-loop2:
+loop27:
 addi  x1,  x1,   1     # Increment x1
 sb    x1,  255(x0)     # Assign the value of x1 to GPIO
-loop1:
+
+loop26:
 addi  x3,  x3,   1     # Increment x3
-bne   x3,  x4,   loop1 # Is there enough delay? No: go to loop1
+bne   x3,  x4,   loop26# Is there enough delay? No: go to loop1
+
 addi  x3,  x0,   0     # Yes: reset delay counter
-bne   x1,  x2,   loop2 # Were all the GPIOs on? No: go to loop2
+bne   x1,  x2,   loop27# Were all the GPIOs on? No: go to loop2
+
 # Yes: all the GPIOs were turn on, reset all counters
 addi  x1,  x0,   0
 addi  x3,  x0,   0
