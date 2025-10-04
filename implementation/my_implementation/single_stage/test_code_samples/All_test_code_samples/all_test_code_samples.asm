8496384828d8413ff033cad8c797f86910702b08
mozerpol <krzmozer@student.pg.edu.pl>
2025-10-04 14:10:30 +0200

    Add gpio output to test

--- implementation/my_implementation/single_stage/test_code_samples/All_test_code_samples/all_test_code_samples.asm
+++ implementation/my_implementation/single_stage/test_code_samples/All_test_code_samples/all_test_code_samples.asm
@@ -751,3 +751,34 @@ lbu   x3,  1(x1)       # x3  = 0x000000ff
 ########################################
 lhu   x4,  4(x0)       # x4  = 0x0000fc00
 lhu   x5,  1(x1)       # x5  = 0x0000ffff
+################################################################################
+##                                                                            ##
+##                                 GPIO OUTPUT                                ##
+##                                                                            ##
+################################################################################
+# Check operation for four GPIO ports
+# gpio[0] = LED
+# gpio[1] = LED
+# gpio[2] = LED
+# gpio[3] = LED
+
+addi  x1,  x0,   0     # The value x1 is assigned to GPIO
+addi  x2,  x0,   15    # The value x1 is compared to the value of x2. 15 = 1111
+addi  x3,  x0,   0     # The value of x3 is compared to the value of x4, this
+# works as a delay loop. The x3 is incremented if is not equal x4
+addi  x4,  x0,   10    # For delay purposes between changing state of GPIO
+
+loop27:
+addi  x1,  x1,   1     # Increment x1
+sb    x1,  255(x0)     # Assign the value of x1 to GPIO
+
+loop26:
+addi  x3,  x3,   1     # Increment x3
+bne   x3,  x4,   loop26# Is there enough delay? No: go to loop1
+
+addi  x3,  x0,   0     # Yes: reset delay counter
+bne   x1,  x2,   loop27# Were all the GPIOs on? No: go to loop2
+
+# Yes: all the GPIOs were turn on, reset all counters
+addi  x1,  x0,   0
+addi  x3,  x0,   0
--- implementation/my_implementation/single_stage/test_code_samples/All_test_code_samples/all_test_code_samples.vhdl
+++ implementation/my_implementation/single_stage/test_code_samples/All_test_code_samples/all_test_code_samples.vhdl
@@ -2333,3 +2333,78 @@
                  gpr            => spy_gpr(5),
                  desired_value  => 32x"0000ffff",
                  test_point     => set_test_point );
+      --------------------------------------------------------------------------
+      --                                                                      --
+      --                              GPIO output                             --
+      --                                                                      --
+      --------------------------------------------------------------------------
+      check_gpr( instruction    => "addi  x1,  x0,   0",
+                 gpr            => spy_gpr(1),
+                 desired_value  => 32x"00000000",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x2,  x0,   15",
+                 gpr            => spy_gpr(2),
+                 desired_value  => 32x"0000000f",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x3,  x0,   0",
+                 gpr            => spy_gpr(3),
+                 desired_value  => 32x"00000000",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x4,  x0,   10",
+                 gpr            => spy_gpr(4),
+                 desired_value  => 32x"0000000a",
+                 test_point     => set_test_point );
+      -- loop27:
+      check_gpr( instruction    => "addi  x1,  x1,   1",
+                 gpr            => spy_gpr(1),
+                 desired_value  => 32x"00000001",
+                 test_point     => set_test_point );
+      check_gpio(instruction    => "sb    x1,  255(x0)",
+                 desired_value  => 8b"00000001",
+                 test_point     => set_test_point );
+      for i in 0 to 13 loop
+         -- loop26:
+         for i in 1 to 20 loop
+            -- wait for execute loop26
+            -- addi  x3,  x3,   1
+            -- bne   x3,  x4,   loop26
+            wait until rising_edge(clk_tb);
+         end loop;
+         check_gpr( instruction    => "addi  x3,  x0,   0",
+                    gpr            => spy_gpr(3),
+                    desired_value  => 32x"00000000",
+                    test_point     => set_test_point );
+         check_branch( instruction   => "bne   x1,  x2,   loop27",
+                       branch_result => spy_branch_result,
+                       desired_value => '0',
+                       test_point    => set_test_point );
+         -- addi  x1,  x1,   1     # Increment x1
+         wait until rising_edge(clk_tb);
+         -- sb    x1,  255(x0)     # Assign the value of x1 to GPIO
+         wait until rising_edge(clk_tb);
+      end loop;
+      -- The last iteration where GPIO = 00001111. This is here (outside the
+      -- loop27) because the return value of the instruction bne x1, x2, loop27
+      --  is 1, not 0 as inside the loop.
+      for i in 1 to 20 loop
+         -- wait for execute loop26
+         -- addi  x3,  x3,   1
+         -- bne   x3,  x4,   loop26
+         wait until rising_edge(clk_tb);
+      end loop;
+      check_gpr( instruction    => "addi  x3,  x0,   0",
+                 gpr            => spy_gpr(3),
+                 desired_value  => 32x"00000000",
+                 test_point     => set_test_point );
+      check_branch( instruction   => "bne   x1,  x2,   loop27",
+                    branch_result => spy_branch_result,
+                    desired_value => '1',
+                    test_point    => set_test_point );
+      check_gpr( instruction    => "addi  x1,  x0,   0",
+                 gpr            => spy_gpr(1),
+                 desired_value  => 32x"00000000",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x3,  x0,   0",
+                 gpr            => spy_gpr(3),
+                 desired_value  => 32x"00000000",
+                 test_point     => set_test_point );
