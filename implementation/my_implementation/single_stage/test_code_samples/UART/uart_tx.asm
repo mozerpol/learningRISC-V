cbb0320fb3a39b12c98ea6c4992d84ffaad03478
mozerpol <krzmozer@student.pg.edu.pl>
2025-10-16 13:03:51 +0200

    Change test

--- implementation/my_implementation/single_stage/test_code_samples/UART/uart_tx.asm
+++ implementation/my_implementation/single_stage/test_code_samples/UART/uart_tx.asm
@@ -1,23 +1,19 @@
 ###################################
 ##         Check UART tx         ##
 ###################################
-lui   x1,  2441
-addi  x1,  x1,   1664  # Long loop purposes
+lui   x1,  1           #	
+addi  x1,  x1,   -1096 # Long loop purposes
 addi  x2,  x0,   0
-lui   x4,  0x41505     # Load upper 20 bits
-ori   x4,  x4,   0x544 # Load lower 12 bits
-addi  x5,  x0,   0xD   # New line sign
+addi  x3,  x0,   0x44  # 0x44 = D in ascii
+addi  x4,  x0,   0xD   # 0xD = new line sign in ascii
 
+sw    x3,  243(x0)     # Send sign D by UART
 loop1:
 addi  x2,  x2,   1     # Increment x2
 bne   x1,  x2,   loop1 # Is there enough delay? No: go to loop1
 addi  x2,  x0,   0     # Reset x2
-sw    x4,  247(x0)     # Send data by UART, 0xDUPA
 
-lui   x6,  0xf         # Short loop purposes
+sw    x4,  243(x0)     # Send new line sign by UART
 loop2:
-addi  x6,  x6,   -1
-bne   x6,  x0,   loop2 # Is there enough delay? No: go to loop2
-sw    x5,  247(x0)     # Send data by UART, a new line sign
-
-j loop1
+addi  x2,  x2,   1     # Increment x2
+bne   x1,  x2,   loop2 # Is there enough delay? No: go to loop1
