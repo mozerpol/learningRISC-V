ae386ab64616f646ff8c2cbdd04b06a8292c1b61
mozerpol <krzmozer@student.pg.edu.pl>
2025-11-28 13:02:31 +0100

    Add uart rx tests

--- implementation/my_implementation/single_stage/test_code_samples/UART/uart_rx.asm
+++ implementation/my_implementation/single_stage/test_code_samples/UART/uart_rx.asm
@@ -1,18 +1,29 @@
 ###################################
 ##         Check UART rx         ##
 ###################################
-lui   x1,  0x3D0
-addi  x1,  x1,   2047
-addi  x1,  x1,   257   # 3D0900 = 4000000, x4 is compared with x3. For 40 MHz it
-# is about 1 sec delay
-addi  x2,  x0,   0     # Counter
-addi  x3,  x0,   1     # For LED
+addi  x1,  x0,   0xAB  # x1 = 0x000000AB
+addi  x2,  x0,   0x12  # x2 = 0x00000012
+addi  x3,  x0,   0xCD  # x3 = 0x000000CD
+addi  x4,  x0,   0x99  # x4 = 0x00000099
+addi  x5,  x0,   0     # x5 = 0x000000AB
 
-loop1:
-addi  x2,  x2,   1     # Increment x3
-bne   x1,  x2,   loop1 # Is there enough delay? No: go to loop1
-addi  x2,  x0,   0     # Reset counter
-lw    x10, 247(x0)     # Save incoming data on UART in x10 reg
-beq   x10, x0,   loop1 # If any data was received on UART then go to the next
-# instruction, if not then go to loop1
-sb    x3,  255(x0)     # Turn on LED
+loop31:
+lw    x5,  243(x0)     # Load UART rx data register to x5 register
+bne   x1,  x5   loop31 # Check if the UART received the data
+
+loop32:
+lw    x5,  243(x0)
+bne   x2,  x5,   loop32
+
+loop33:
+lw    x5,  243(x0)
+bne   x3,  x5,   loop33
+
+loop34:
+lw    x5,  243(x0)
+bne   x4,  x5,   loop34
+
+addi  x2,  x0,   0     # x2 = 0x00000000
+addi  x2,  x2,   1     # x2 = 0x00000001
+addi  x2,  x2,   1     # x2 = 0x00000002
+addi  x2,  x2,   1     # x2 = 0x00000003
---
+++ implementation/my_implementation/single_stage/test_code_samples/UART/uart_rx.vhdl
@@ -0,0 +1,60 @@
+      --------------------------------------------------------------------------
+      --                                                                      --
+      --                                 UART                                 --
+      --                                                                      --
+      --------------------------------------------------------------------------
+      -------------------------------------
+      --             UART RX             --
+      -------------------------------------
+      check_gpr( instruction    => "addi  x1,  x0,   0xAB",
+                 gpr            => spy_gpr(1),
+                 desired_value  => 32x"000000ab",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x2,  x0,   0x12",
+                 gpr            => spy_gpr(2),
+                 desired_value  => 32x"00000012",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x3,  x0,   0xCD",
+                 gpr            => spy_gpr(3),
+                 desired_value  => 32x"000000CD",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x4,  x0,   0x99",
+                 gpr            => spy_gpr(4),
+                 desired_value  => 32x"00000099",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x5,  x0,   0",
+                 gpr            => spy_gpr(5),
+                 desired_value  => 32x"00000000",
+                 test_point     => set_test_point );
+      check_uart_rx( value_to_send     => 32x"ABEEEEEE",
+                 number_bytes_to_send  => 4,
+                 out_rx                => rx_tb,
+                 test_point            => set_test_point );
+      check_uart_rx( value_to_send     => 32x"00120000",
+                 number_bytes_to_send  => 3,
+                 out_rx                => rx_tb,
+                 test_point            => set_test_point );
+      check_uart_rx( value_to_send     => 32x"0000CDEF",
+                 number_bytes_to_send  => 2,
+                 out_rx                => rx_tb,
+                 test_point            => set_test_point );
+      check_uart_rx( value_to_send     => 32x"00000099",
+                 number_bytes_to_send  => 1,
+                 out_rx                => rx_tb,
+                 test_point            => set_test_point );   
+      check_gpr( instruction    => "addi  x2,  x0,   0",
+                 gpr            => spy_gpr(2),
+                 desired_value  => 32x"00000000",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x2,  x2,   1",
+                 gpr            => spy_gpr(2),
+                 desired_value  => 32x"00000001",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x2,  x2,   1",
+                 gpr            => spy_gpr(2),
+                 desired_value  => 32x"00000002",
+                 test_point     => set_test_point );
+      check_gpr( instruction    => "addi  x2,  x2,   1",
+                 gpr            => spy_gpr(2),
+                 desired_value  => 32x"00000003",
+                 test_point     => set_test_point );
