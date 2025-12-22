09f446f6542e7486ce28e0bef4a527613147516f
mozerpol <krzmozer@student.pg.edu.pl>
2025-12-22 13:20:17 +0100

    Update tests

--- implementation/my_implementation/single_stage/tb/test_code_samples/GPIO/gpio_input.vhdl
+++ implementation/my_implementation/single_stage/tb/test_code_samples/GPIO/gpio_input.vhdl
@@ -10,8 +10,5 @@
       wait until rising_edge(clk_tb); -- nop instruction
       wait until rising_edge(clk_tb); -- nop instruction
       -- Check if value 0000000D has been loaded into register x1
-      check_gpr( instruction    => "lw    x1,  255(x0)",
-                 gpr            => spy_gpr(1),
-                 desired_value  => 32x"0000000D",
-                 test_point     => set_test_point );
+      check_gpr("lw    x1,  255(x0)", x"0000000D", clk_tb, test_point);
       gpio_tb        <= (others => 'Z');
--- implementation/my_implementation/single_stage/tb/test_code_samples/GPIO/gpio_output.vhdl
+++ implementation/my_implementation/single_stage/tb/test_code_samples/GPIO/gpio_output.vhdl
@@ -3,30 +3,13 @@
       --                              GPIO output                             --
       --                                                                      --
       --------------------------------------------------------------------------
-      check_gpr( instruction    => "addi  x1,  x0,   0",
-                 gpr            => spy_gpr(1),
-                 desired_value  => 32x"00000000",
-                 test_point     => set_test_point );
-      check_gpr( instruction    => "addi  x2,  x0,   15",
-                 gpr            => spy_gpr(2),
-                 desired_value  => 32x"0000000f",
-                 test_point     => set_test_point );
-      check_gpr( instruction    => "addi  x3,  x0,   0",
-                 gpr            => spy_gpr(3),
-                 desired_value  => 32x"00000000",
-                 test_point     => set_test_point );
-      check_gpr( instruction    => "addi  x4,  x0,   10",
-                 gpr            => spy_gpr(4),
-                 desired_value  => 32x"0000000a",
-                 test_point     => set_test_point );
+      check_gpr("addi  x1,  x0,   0", x"00000000", clk_tb, test_point);
+      check_gpr("addi  x2,  x0,   15", x"0000000f", clk_tb, test_point);
+      check_gpr("addi  x3,  x0,   0", x"00000000", clk_tb, test_point);
+      check_gpr("addi  x4,  x0,   10", x"0000000a", clk_tb, test_point);
       -- loop27:
-      check_gpr( instruction    => "addi  x1,  x1,   1",
-                 gpr            => spy_gpr(1),
-                 desired_value  => 32x"00000001",
-                 test_point     => set_test_point );
-      check_gpio(instruction    => "sb    x1,  255(x0)",
-                 desired_value  => 8b"00000001",
-                 test_point     => set_test_point );
+      check_gpr("addi  x1,  x1,   1", x"00000001", clk_tb, test_point);
+      check_gpio("sb    x1,  255(x0)", b"00000001", clk_tb, test_point);
       for i in 0 to 13 loop
          -- loop26:
          for i in 1 to 20 loop
@@ -35,14 +18,8 @@
             -- bne   x3,  x4,   loop26
             wait until rising_edge(clk_tb);
          end loop;
-         check_gpr( instruction    => "addi  x3,  x0,   0",
-                    gpr            => spy_gpr(3),
-                    desired_value  => 32x"00000000",
-                    test_point     => set_test_point );
-         check_branch( instruction   => "bne   x1,  x2,   loop27",
-                       branch_result => spy_branch_result,
-                       desired_value => '0',
-                       test_point    => set_test_point );
+         check_gpr("addi  x3,  x0,   0", x"00000000", clk_tb, test_point);
+         check_branch("bne   x1,  x2,   loop27", '0', clk_tb, test_point);
          -- addi  x1,  x1,   1     # Increment x1
          wait until rising_edge(clk_tb);
          -- sb    x1,  255(x0)     # Assign the value of x1 to GPIO
@@ -57,19 +34,7 @@
          -- bne   x3,  x4,   loop26
          wait until rising_edge(clk_tb);
       end loop;
-      check_gpr( instruction    => "addi  x3,  x0,   0",
-                 gpr            => spy_gpr(3),
-                 desired_value  => 32x"00000000",
-                 test_point     => set_test_point );
-      check_branch( instruction   => "bne   x1,  x2,   loop27",
-                    branch_result => spy_branch_result,
-                    desired_value => '1',
-                    test_point    => set_test_point );
-      check_gpr( instruction    => "addi  x1,  x0,   0",
-                 gpr            => spy_gpr(1),
-                 desired_value  => 32x"00000000",
-                 test_point     => set_test_point );
-      check_gpr( instruction    => "addi  x3,  x0,   0",
-                 gpr            => spy_gpr(3),
-                 desired_value  => 32x"00000000",
-                 test_point     => set_test_point );
+      check_gpr("addi  x3,  x0,   0", x"00000000", clk_tb, test_point);
+      check_branch("bne   x1,  x2,   loop27", '1', clk_tb, test_point);
+      check_gpr("addi  x1,  x0,   0", x"00000000", clk_tb, test_point);
+      check_gpr("addi  x3,  x0,   0", x"00000000", clk_tb, test_point);
