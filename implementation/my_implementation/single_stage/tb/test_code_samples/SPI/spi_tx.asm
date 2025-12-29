977af80b55ae151fd40c67a1f71a1129484af1e7
mozerpol <krzmozer@student.pg.edu.pl>
2025-12-29 13:05:02 +0100

    Add test code for spi

--- implementation/my_implementation/single_stage/tb/test_code_samples/SPI/spi_tx.asm
+++ implementation/my_implementation/single_stage/tb/test_code_samples/SPI/spi_tx.asm
@@ -12,12 +12,25 @@ addi  x2,  x0,   0     # x2 = 0x00000000
 addi  x3,  x0,   0xAA  # x3 = 0x000000AA, data1 to send
 lui   x4,  703711      # x4 = 0xabcdef00, data2 to send
 addi  x4,  x4,   -238  # x4 = 0xabcdef12, data2 to send
-sw    x1,  227(x0)     # Turn on sclk and chip enable
-nop
-nop
-nop
-sw    x3,  235(x0)     # Send sign data1 by SPI
 
+sw    x1,  227(x0)     # Turn on sclk and chip enable
+
+sw    x3,  235(x0)     # Send data1 by SPI
 loop35:
 lw    x2,  231(x0)     # Load SPI status bit to x2 register
 bne   x2,  x0,   loop35# Check if uart tx is busy
+
+sw    x4,  235(x0)     # Send data2 by SPI
+loop36:
+lw    x2,  231(x0)     # Load SPI status bit to x2 register
+bne   x2,  x0,   loop36# Check if uart tx is busy
+
+sw    x0,  227(x0)     # Turn off sclk and chip enable
+addi  x1,  x0,   0
+addi  x1,  x1,   1
+addi  x1,  x1,   1
+addi  x1,  x1,   1
+addi  x1,  x1,   1
+########################################
+##                RX                  ##
+########################################
