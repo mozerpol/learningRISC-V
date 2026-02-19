94cd300d32e96e058b71dcd6277fa575a3a1a50f
mozerpol <krzmozer@student.pg.edu.pl>
2026-02-19 13:03:38 +0100

    Update code for spi tx

--- implementation/my_implementation/single_stage/tb/test_code_samples/SPI/spi_tx.asm
+++ implementation/my_implementation/single_stage/tb/test_code_samples/SPI/spi_tx.asm
@@ -6,31 +6,12 @@
 ########################################
 ##                TX                  ##
 ########################################
-addi  x1,  x0,   1     # x1 = 0x00000001, turn on sclk 
-addi  x1,  x1,   2     # x1 = 0x00000011, chip enable
-addi  x2,  x0,   0     # x2 = 0x00000000
-addi  x3,  x0,   0xAA  # x3 = 0x000000AA, data1 to send
-lui   x4,  703711      # x4 = 0xabcdef00, data2 to send
-addi  x4,  x4,   -238  # x4 = 0xabcdef12, data2 to send
-
-sw    x1,  227(x0)     # Turn on sclk and chip enable
-
-sw    x3,  235(x0)     # Send data1 by SPI
+addi  x2,  x0,   1     # x2 = 0x00000001, compare with SPI status register
+addi  x3,  x0,   0x95  # x3 = 0x00000095, data to send. 0x95 = 0b10010101
+sw    x3,  20(x0)     # Send data by SPI
 loop35:
-lw    x2,  231(x0)     # Load SPI status bit to x2 register
-bne   x2,  x0,   loop35# Check if uart tx is busy
-
-sw    x4,  235(x0)     # Send data2 by SPI
-loop36:
-lw    x2,  231(x0)     # Load SPI status bit to x2 register
-bne   x2,  x0,   loop36# Check if uart tx is busy
-
-sw    x0,  227(x0)     # Turn off sclk and chip enable
-addi  x1,  x0,   0
-addi  x1,  x1,   1
-addi  x1,  x1,   1
-addi  x1,  x1,   1
-addi  x1,  x1,   1
+lw    x2,  28(x0)     # Load SPI status bit to x2 register
+beq   x2,  x0,   loop35# Check if uart tx is busy
 ########################################
 ##                RX                  ##
 ########################################
