c4132ea53faf126df14f18f45a5727ab03a4ac41
mozerpol <krzmozer@student.pg.edu.pl>
2026-05-13 14:12:08 +0200

    Add tests for i2c tx

--- implementation/my_implementation/single_stage/tb/test_code_samples/I2C/i2c_tx.asm
+++ implementation/my_implementation/single_stage/tb/test_code_samples/I2C/i2c_tx.asm
@@ -1,8 +1,15 @@
+################################################################################
+##                                                                            ##
+##                                   I2C TX                                   ##
+##                                                                            ##
+################################################################################
 lui   x1,  0xF0E7C     # x1 = 0xF0E7C000, data to send
 addi  x1,  x1,   0x381 # x1 = 0xF0E7C381 = 11110000 11100111 11000011 10000001
 addi  x2,  x0,   0     # x2 = 0x00000000, I2C status purposes
 
-################################################# send one byte
+###################################
+##         Send one byte         ##
+###################################
 addi  x3,  x0,   0x155 # x3 = 0x00000155, where:
 # 0x55 - slave address = 0b1010101
 # 0x1 - number of bytes to send
@@ -18,8 +25,9 @@ addi  x4,  x0,   0x1   # x4 = 0x00000001
 addi  x4,  x4,   0x1   # x4 = 0x00000002
 addi  x4,  x4,   0x1   # x4 = 0x00000003
 
-################################################# send two bytes
-
+###################################
+##         Send two bytes        ##
+###################################
 addi  x3,  x0,   0x278 # x3 = 0x00000278, where:
 # 0x78 - slave address = 0b1111000
 # 0x2 - number of bytes to send
@@ -36,10 +44,11 @@ addi  x4,  x4,   0x1   # x4 = 0x00000004
 addi  x4,  x4,   0x1   # x4 = 0x00000005
 addi  x4,  x4,   0x1   # x4 = 0x00000006
 
-################################################# send three bytes
-
+###################################
+##        Send three bytes       ##
+###################################
 addi  x3,  x0,   0x30F # x3 = 0x0000030F, where:
-# 0x0F - slave address
+# 0x0F - slave address = 0b00001111
 # 0x3 - number of bytes to send
 # 0x0 - R/W bit - write to slave
 
@@ -54,10 +63,11 @@ addi  x4,  x4,   0x1   # x4 = 0x00000007
 addi  x4,  x4,   0x1   # x4 = 0x00000008
 addi  x4,  x4,   0x1   # x4 = 0x00000009
 
-################################################# send four bytes
-
-addi  x3,  x0,   0x4FF # x1 = 0x000004FF, where:
-# 0xFF - slave address
+###################################
+##        Send four bytes        ##
+###################################
+addi  x3,  x0,   0x47F # x1 = 0x0000047F, where:
+# 0x7F - slave address = 0b01111111
 # 0x4 - number of bytes to send
 # 0x0 - R/W bit - write to slave
 
