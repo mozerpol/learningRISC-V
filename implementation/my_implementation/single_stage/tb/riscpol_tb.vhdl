6a40de9ba94195746aee831e5a14f05e37300676
mozerpol <krzmozer@student.pg.edu.pl>
2026-05-21 14:10:19 +0200

    Add test

--- implementation/my_implementation/single_stage/tb/riscpol_tb.vhdl
+++ implementation/my_implementation/single_stage/tb/riscpol_tb.vhdl
@@ -129,9 +129,10 @@ begin
       wait until rising_edge(clk_tb);
 
 
-      check_gpr("lui   x1,  0xF0E7C", x"F0E7C000", clk_tb, test_point);
-      check_gpr("addi  x1,  x1,   0x381", x"F0E7C381", clk_tb, test_point);
+
       check_gpr("addi  x2,  x0,   0", x"00000000", clk_tb, test_point);
+      check_gpr("lui   x3,  1", x"00001000", clk_tb, test_point);
+      check_gpr("addi  x3,  x3,   -1707", x"00000955", clk_tb, test_point);
       -- Read one byte
       check_i2c_rx(7x"55", x"F0E7C381", 1, s_i2c_sda_tb, clk_tb, test_point);
 
