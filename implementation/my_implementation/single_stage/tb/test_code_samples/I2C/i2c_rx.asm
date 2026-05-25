df78d20f76b7a296eb33061b9dd91cd51f3444fe
mozerpol <krzmozer@student.pg.edu.pl>
2026-05-25 14:10:16 +0200

    Update asm

--- implementation/my_implementation/single_stage/tb/test_code_samples/I2C/i2c_rx.asm
+++ implementation/my_implementation/single_stage/tb/test_code_samples/I2C/i2c_rx.asm
@@ -4,7 +4,6 @@
 ##                                                                            ##
 ################################################################################
 addi  x2,  x0,   0     # x2 = 0x00000000, I2C status purposes
-
 ###################################
 ##         Read one byte         ##
 ###################################
@@ -14,11 +13,12 @@ addi  x3,  x3,   -1707 # x3 = 0x00000955, where:
 # 0x1 - number of bytes to send
 # 0x1 - R/W bit - read from slave
 sw    x3,  32(x0)      # Send address, number of bytes to send and R/W bit to I2C controller
-sw    x1,  40(x0)      # Start reading data by I2C
 loop41:
 lw    x2,  44(x0)      # Load I2C status
 andi  x2,  x2    0x1   # Check the oldest bit (no. 0)
 bne   x2,  x0,   loop41# Check if I2C rx is busy, when = 1, then jump to loop37
+
+lw    x1,  40(x0)      # Load received data to x1 register
 
 addi  x4,  x0,   0x1   # x4 = 0x00000001
 addi  x4,  x4,   0x1   # x4 = 0x00000002
